849c2cb049b4275c47d793d2ef3a6b9c12199e51
Andste <andste.com@gmail.com>
2017-10-12 14:41:56 +0800

    管理界面重新整理完成

---
+++ pc/store/src/views/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="not-found-container"></div>
+</template>
+
+<script>
+  export default {
+    name: 'PageNotFound'
+  }
+</script>
+
+<style scoped>
+  .not-found-container {
+    width: 100%;
+    height: 100%;
+    background: #ffffff url("../assets/bg-404.png") no-repeat center;
+  }
+</style>
