11ff9b37d3286d26a456fa19adf7a622149bcb86
Andste <andste.com@gmail.com>
2017-10-15 13:55:05 +0800

    移除store and admin

--- store/src/views/doc/Index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <iframe src="http://element.eleme.io/2.0/#/zh-CN/component/button" style="width: 100%; height: 100%; border: none; background: #fff"></iframe>
-</template>
-
-<script>
-  export default {
-    name: 'Doc'
-  }
-</script>
