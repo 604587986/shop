61f73dd82d322cd35737c3561117b88a69b7602e
赖云川 <andste.cc@gmail.com>
2017-11-09 13:44:53 +0800

    :bug: 修复DataModel不能接收数组的问题

--- manager/src/framework/DataModel.js
+++ manager/src/framework/DataModel.js
@@ -5,26 +5,37 @@ export default class DataModel {
     if (!json) return this
     const alias = this.constructor.__serverName__
     const checkers = this.constructor.__checkers__
-    for (let i in this) {
-      if (!this.hasOwnProperty(i)) return
-      let realValue = this[i]
-      // 首先检查别名数据，并做映射
-      if (alias && typeof alias[i] !== 'undefined') {
-        const value = json[alias[i]]
-        if (value !== undefined) realValue = json[alias[i]]
-      } else if (json[i] !== undefined && json[i] !== null) {
-        realValue = json[i]
+    const _isArray = Array.isArray(json)
+
+    // 如果json不是数组 将它封装为数组
+    json = !_isArray ? [json] : json
+    json.map(item => {
+      for (let i in this) {
+        if (!this.hasOwnProperty(i)) return item
+        let realValue = this[i]
+        // 首先检查别名数据，并做映射
+        const _alias = alias[i]
+        if (alias && typeof _alias !== 'undefined') {
+          const value = item[_alias]
+          if (value !== undefined) {
+            realValue = item[_alias]
+            delete item[_alias]
+          }
+        } else if (item[i] !== undefined && item[i] !== null) {
+          realValue = item[i]
+        }
+        // 然后针对数据检查类型
+        if (checkers && checkers[i]) {
+          const value = realValue
+          const type = checkers[i]
+          const valueType = typeof (value)
+          if (!checkFun(type, value)) throw new Error(`要检查的数据类型${type}未通过！`)
+        }
+        // 赋值
+        item[i] = realValue
       }
-      // 然后针对数据检查类型
-      if (checkers && checkers[i]) {
-        const value = realValue
-        const type = checkers[i]
-        const valueType = typeof (value)
-        if (!checkFun(type, value)) throw new Error(`要检查的数据类型${type}未通过！`)
-      }
-      // 赋值
-      this[i] = realValue
-    }
-    return this
+      return item
+    })
+    return _isArray ? json : json[0]
   }
 }
