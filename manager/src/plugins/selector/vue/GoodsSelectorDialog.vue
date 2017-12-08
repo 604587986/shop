7af1fbc05dc9b4f51c187846a280d6740cbe7663
shuiRong <shuirong1997@icloud.com>
2017-12-08 16:07:24 +0800

    更新-插件迁移：完成GoodsSelector的迁移（不带面板）

---
+++ manager/src/plugins/selector/vue/GoodsSelectorDialog.vue
@@ -0,0 +1,54 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import $ from 'jquery'
+import '@/plugins/selector/css/jquery.goodsSelector.css'
+import '@/plugins/selector/js/jquery.goodsSelector'
+export default {
+  name: 'GoodsSelectorDialog',
+  props: ['defaultData', 'maxLength', 'showDialog', 'mode'],
+  methods: {
+    callAdminDialog() {
+      $.GoodsAdminSelector({
+        defaultData: this.defaultData,
+        maxLength: this.maxLength,
+        confirm: data => {
+          // 触发回调
+          this.$emit('refreshFunc', data)
+        }
+      })
+    },
+    callSellerDialog() {
+      $.GoodsAdminSelector({
+        defaultData: this.defaultData,
+        maxLength: this.maxLength,
+        confirm: data => {
+          // 触发回调
+          this.$emit('refreshFunc', data)
+        }
+      })
+    }
+  },
+  mounted() {
+    if (this.showDialog) {
+      this.mode === 'admin' ? this.callAdminDialog() : this.callSellerDialog()
+    }
+  },
+  watch: {
+    showDialog(val) {
+      if (!val) {
+        // 关闭Dialog
+        $('#__GSR__ .cancel').click()
+      } else {
+        this.mode === 'admin' ? this.callAdminDialog() : this.callSellerDialog()
+      }
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
