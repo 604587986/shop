99f62a62730b4640e7858c1399b3c2d9199b61df
赖云川 <andste.cc@gmail.com>
2018-01-19 18:09:37 +0800

    会员模型和商品模型调整

--- manager-admin/src/models/MemberModel.js
+++ manager-admin/src/models/MemberModel.js
@@ -12,32 +12,103 @@ import {
 } from '@/framework'
 
 export default class MemberModel extends DataModel {
-  /** 会员名称 */
-  @ServerName('username')
-  username
+  /** 头像 */
+  @ServerName('face')
+  avatar
 
-  /** 会员ID */
-  @ServerName('member_id')
-  member_id
+  /** 生日 */
+  @ServerName('birthday')
+  birthday
 
-  /** 性别 */
-  @ServerName('sex')
-  sex = 1
-
-  /** 手机号 */
-  @Check(CheckTypes.mobile)
-  @ServerName('mobile')
-  mobile
+  /** 当前会员是否已被禁用 */
+  @ServerName('disabled')
+  disabled
 
   /** 电子邮箱 */
   @ServerName('email')
   email
 
-  /** 注册时间 */
-  @ServerName('create_time')
-  create_time
+  /** 会员名称 */
+  @ServerName('uname')
+  username
 
-  /** 头像 */
-  @ServerName('avatar')
-  avatar
+  /** 是否是卖家 */
+  @ServerName('is_store')
+  is_seller
+
+  /** 最后登录时间 */
+  @ServerName('lastlogin')
+  last_login_time
+
+  /** 登录次数 */
+  @ServerName('logincount')
+  login_count
+
+  /** 会员等级ID */
+  @ServerName('lv_id')
+  lv_id
+
+  /** 会员等级 */
+  @ServerName('lv_name')
+  lv_name
+
+  /** 会员ID */
+  @ServerName('member_id')
+  member_id
+
+  /** 手机号 */
+  @ServerName('mobile')
+  mobile
+
+  /** 账户名 */
+  @ServerName('name')
+  name
+
+  /** 昵称 */
+  @ServerName('nickname')
+  nick_name
+
+  /** 积分 */
+  @ServerName('point')
+  point
+
+  /** QQ */
+  @ServerName('qq')
+  qq
+
+  /** QQ开放平台openid */
+  @ServerName('qq_id')
+  qq_id
+
+  /** 注册时间 */
+  @ServerName('regtime')
+  register_time
+
+  /** 会员备注 */
+  @ServerName('remark')
+  remark
+
+  /** 性别 */
+  @ServerName('sex')
+  sex = 1
+
+  /** 店铺ID [如果没有店铺 为0] */
+  @ServerName('store_id')
+  seller_id
+
+  /** 联系方式：固话 */
+  @ServerName('tel')
+  tel
+
+  /** 微信开放平台openid */
+  @ServerName('wechat_id')
+  wechat_id
+
+  /** 微博开放平台openid */
+  @ServerName('weibo_id')
+  weibo_id
+
+  /** 邮编地址 */
+  @ServerName('zip')
+  zip
 }
