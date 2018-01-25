643620a1dc3cf7f84a5d754488506a896a0b8779
赖云川 <andste.cc@gmail.com>
2018-01-25 13:40:01 +0800

    咨询列表

--- manager-admin/src/api/comment.js
+++ manager-admin/src/api/comment.js
@@ -9,7 +9,7 @@ import CommentModel from '@/models/CommentModel'
 export function getCommentList(params) {
   return new Promise((resolve, reject) => {
     request({
-      url: 'shop/admin/comments/list-json.do',
+      url: 'shop/admin/comments/list-json.do?type=1',
       method: 'get',
       loading: false,
       params
@@ -51,5 +51,60 @@ export function deleteComment(ids) {
       method: 'post',
       data: _formData
     }).then(response => resolve(response)).catch(error => reject(error))
+  })
+}
+
+/***** ASK *****/
+
+/**
+ * 获取咨询列表
+ * @param params
+ * @returns {Promise<any>}
+ */
+export function getAskList(params) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: 'shop/admin/comments/list-json.do?type=2',
+      method: 'get',
+      loading: false,
+      params
+    }).then(response => {
+      const _resposne = response
+      _resposne.data = new CommentModel().map(response.data)
+      resolve(_resposne)
+    }).catch(error => reject(error))
+  })
+}
+
+/**
+ * 获取咨询详情
+ * @param id
+ * @returns {Promist}
+ */
+export function getAskDetail(id) {
+  return new Promist((resolve, reject) => {
+    request({
+      url: '',
+      method: 'get',
+      params: { commentId: id }
+    }).then(response => resolve(response)).catch(error => reject(error))
+  })
+}
+
+/**
+ * 删除咨询
+ * @param ids
+ * @returns {Promise<any>}
+ */
+export function deleteAsk(ids) {
+  if (!Array.isArray(ids)) ids = [ids]
+  const _formData = new FormData()
+  ids.forEach(item => _formData.append('comment_id', item))
+  return new Promise((resolve, reject) => {
+    request({
+      url: 'shop/admin/comments/delete.do',
+      method: 'post',
+      data: _formData
+    })
   })
 }
