9b4ba75f3450145fbae523d5ec9adc8a345ccc6d
赖云川 <andste.cc@gmail.com>
2018-02-02 17:54:41 +0800

    修复页面没有被缓存的问题

--- manager-admin/src/views/layout/components/AppMain.vue
+++ manager-admin/src/views/layout/components/AppMain.vue
@@ -2,7 +2,7 @@
   <section class="app-main" style="min-height: 100%; padding: 10px">
     <transition name="fade" mode="out-in">
       <keep-alive :include="cachedViews">
-        <router-view :key="key"></router-view>
+        <router-view></router-view>
       </keep-alive>
     </transition>
   </section>
@@ -14,9 +14,6 @@ export default {
   computed: {
     cachedViews() {
       return this.$store.state.tagsView.cachedViews
-    },
-    key() {
-      return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route + +new Date()
     }
   }
 }
