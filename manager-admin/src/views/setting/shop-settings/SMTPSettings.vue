92824643c856e06d81970da070217df380cd7ef6
赖云川 <andste.cc@gmail.com>
2018-02-13 18:15:05 +0800

    Smtp设置

--- manager-admin/src/views/setting/shop-settings/SMTPSettings.vue
+++ manager-admin/src/views/setting/shop-settings/SMTPSettings.vue
@@ -1,10 +1,107 @@
 <template>
-  <div>SMTPSetting</div>
+  <div>
+    <en-tabel-layout
+      toolbar
+      pagination
+      :tableData="tableData"
+      :loading="loading"
+    >
+      <div slot="toolbar" class="inner-toolbar">
+        <div class="toolbar-btns">
+        </div>
+      </div>
+      <template slot="table-columns">
+        <el-table-column prop="host" label="HOST"/>
+        <el-table-column prop="username" label="用户名"/>
+        <el-table-column prop="send_count" label="今日发信数"/>
+        <el-table-column prop="max_count" label="最大发信数"/>
+        <el-table-column prop="mail_from" label="Form"/>
+        <el-table-column label="操作" width="150">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              type="primary"
+              @click="handleEditSmtp(scope.$index, scope.row)">修改</el-button>
+          </template>
+        </el-table-column>
+      </template>
+      <el-pagination
+        slot="pagination"
+        v-if="pageData"
+        @size-change="handlePageSizeChange"
+        @current-change="handlePageCurrentChange"
+        :current-page="pageData.page_no"
+        :page-sizes="[10, 20, 50, 100]"
+        :page-size="pageData.page_size"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="pageData.data_total">
+      </el-pagination>
+    </en-tabel-layout>
+  </div>
 </template>
 
 <script>
+  import * as API_Smtp from '@/api/smtp'
+  import { TableLayout } from '@/components'
   export default {
-    name: 'SMTPSettings'
+    name: 'SMTPSettings',
+    components: {
+      [TableLayout.name]: TableLayout
+    },
+    data() {
+      return {
+        /** 列表loading状态 */
+        loading: false,
+
+        /** 列表参数 */
+        params: {
+          page_no: 1,
+          page_size: 10
+        },
+
+        /** 列表数据 */
+        tableData: null,
+
+        /** 列表分页数据 */
+        pageData: null
+      }
+    },
+    mounted() {
+      this.GET_SmtpList()
+    },
+    methods: {
+      /** 分页大小发生改变 */
+      handlePageSizeChange(size) {
+        this.params.page_size = size
+        this.GET_SmtpList()
+      },
+
+      /** 分页页数发生改变 */
+      handlePageCurrentChange(page) {
+        this.params.page_no = page
+        this.GET_SmtpList()
+      },
+
+      /** 修改Smtp */
+      handleEditSmtp(index, row) {},
+
+      /** 获取SMTP列表 */
+      GET_SmtpList() {
+        this.loading = true
+        API_Smtp.getSmtpList(this.params).then(response => {
+          this.loading = false
+          this.tableData = response.data
+          this.pageData = {
+            page_no: response.draw,
+            page_size: 10,
+            data_total: response.recordsTotal
+          }
+        }).catch(error => {
+          this.loading = false
+          console.log(error)
+        })
+      }
+    }
   }
 </script>
 
