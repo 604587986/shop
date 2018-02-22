6fd1b4bf0cd8edb67a455c28551a2f8479feafcf
赖云川 <andste.cc@gmail.com>
2018-02-22 11:29:31 +0800

    静态页生成

--- manager-admin/src/views/setting/shop-settings/staticPage.vue
+++ manager-admin/src/views/setting/shop-settings/staticPage.vue
@@ -1,13 +1,114 @@
 <template>
-  <div>staticPage</div>
+  <div class="static-page-container">
+    <el-form ref="form" label-width="100px" style="width: 500px;">
+      <el-form-item label="静态页地址">
+        <el-input v-model="address">
+          <el-button slot="append" icon="el-icon-check" @click="handleSaveAddress"></el-button>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="要生成的页面">
+        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+        <el-checkbox-group v-model="checkedPages" @change="handleCheckedCitiesChange">
+          <el-checkbox v-for="page in pages" :label="page" :key="page.value">{{ page.text }}</el-checkbox>
+        </el-checkbox-group>
+        <el-button
+          type="primary"
+          :disabled="status === 'doing'"
+          :loading="status === 'doing'"
+          @click="handleCreateStaticPage"
+        >{{ status === 'doing' ? '生成中' : '生成' }}</el-button>
+      </el-form-item>
+    </el-form>
+    <div class="progress-box">
+      <el-progress :text-inside="true" :stroke-width="18" :percentage="percentage" :status="status"/>
+      <p>{{ status_text }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+  import * as API_StaticPage from '@/api/staticPage'
+  import * as API_Progress from '@/api/progress'
+  const pageOptions = [
+    { text: '首页', value: 'index' },
+    { text: '商品页', value: 'goods' },
+    { text: '帮助中心', value: 'help' }
+  ]
   export default {
-    name: 'staticPage'
+    name: 'staticPage',
+    data() {
+      return {
+        percentage: 0,
+        status: '',
+        status_text: '',
+        address: 'http://localhost:8080/javashop',
+        checkAll: true,
+        checkedPages: pageOptions,
+        pages: pageOptions,
+        isIndeterminate: true
+      }
+    },
+    created() {
+      /** 检查是否有静态页生成任务 */
+      API_Progress.hasSameTask('page_create').then(response => {
+        if (response.data.has_task === 1) {
+          this.status = 'doing'
+          this.GET_Progress()
+        }
+      }).catch(error => console.log(error))
+    },
+    methods: {
+      /** 保存静态页地址 */
+      handleSaveAddress() {
+        API_StaticPage.saveStaticPageAddress(this.address).then(response => {
+          this.$message.success('保存成功！')
+        }).catch(error => console.log(error))
+      },
+      handleCheckAllChange(val) {
+        this.checkedPages = val ? pageOptions : []
+        this.isIndeterminate = false
+      },
+      handleCheckedCitiesChange(value) {
+        let checkedCount = value.length
+        this.checkAll = checkedCount === this.pages.length
+        this.isIndeterminate = checkedCount > 0 && checkedCount < this.pages.length
+      },
+      /** 生成静态页 */
+      handleCreateStaticPage() {
+        this.$confirm('确定要生成静态页吗？', '提示', { type: 'warning' }).then(() => {
+          const params = this.checkedPages.map(item => item.value)
+          API_StaticPage.createStaticPage(params).then(response => {
+            this.GET_Progress()
+          }).catch(error => console.log(error))
+        }).catch(() => {})
+      },
+      /** 获取生成进度 */
+      GET_Progress() {
+        API_Progress.getProgressById('page_create').then(response => {
+          const { percentage, status, status_text } = response.data
+          this.percentage = percentage
+          this.status = status
+          this.status_text = status_text
+          if (response.data.status !== 'success') {
+            setTimeout(this.GET_Progress, 1000)
+          }
+        }).catch(error => console.log(error))
+      }
+    }
   }
 </script>
 
 <style type="text/scss" lang="scss" scoped>
-
+  .static-page-container {
+    background-color: #fff;
+    padding: 10px;
+  }
+  .progress-box {
+    width: 500px;
+    margin-top: 20px;
+    p {
+      margin-top: 10px;
+      font-size: 14px;
+    }
+  }
 </style>
