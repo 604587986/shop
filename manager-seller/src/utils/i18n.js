7556e1f8749e253ed82dbbbfda00ab61e9a4b094
赖云川 <andste.cc@gmail.com>
2018-03-05 10:27:25 +0800

    移除manager-seller

--- manager-seller/src/utils/i18n.js
+++
@@ -1,4 +0,0 @@
-// translate router.meta.title , be used in breadcrumb sidebar tagsview
-export function generateTitle(title) {
-  return this.$t('route.' + title) // $t :this method from vue-i18n ,inject in @/lang/index.js
-}
