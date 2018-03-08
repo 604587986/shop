e889418200e561f0910578e995953ec9e1b06992
赖云川 <andste.cc@gmail.com>
2018-03-08 12:59:59 +0800

    引入vuex

---
+++ buyer/wap/themes/default/src/store/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Andste on 2018/3/8.
+ */
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import cart from './modules/cart'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    cart
+  }
+})
+
+export default store
