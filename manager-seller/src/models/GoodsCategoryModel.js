a94d6a44c4285c970ef79d85d6544805fc2b282a
yangpei <1208812362@qq.com>
2018-03-14 17:26:14 +0800

    商品分类模型

---
+++ manager-seller/src/models/GoodsCategoryModel.js
@@ -0,0 +1,30 @@
+/**
+ * 商品分类模型
+ */
+import {
+  DataModel,
+  Check,
+  CheckTypes,
+  ServerName,
+  Unit,
+  UnitTypes
+} from '@/framework'
+
+export default class GoodsCategoryModel extends DataModel {
+  /** 商品分类D */
+  @ServerName('category_id')
+  category_id
+
+  /** 商品分类名称 */
+  @ServerName('category_name')
+  category_name
+
+  /** 商品分类顺序 */
+  @ServerName('sort')
+  category_sort
+
+  /** 商品分类显示 */
+  @ServerName('is_show')
+  category_show
+}
+
