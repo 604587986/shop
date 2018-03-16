bfa8c9cf9f1ec723bdc2593d7ec1076d6e444c95
赖云川 <andste.cc@gmail.com>
2018-03-16 12:33:50 +0800

    订单管理增加页面

---
+++ manager-seller/src/views/order/commentsManage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    commentsManage
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'commentsManage',
+    data() {
+      return {}
+    },
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
+
---
+++ manager-seller/src/views/order/settlementManage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    settlementManage
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'settlementManage',
+    data() {
+      return {}
+    },
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
+
