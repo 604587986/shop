e06d76ede54df8c182b10edee469758f2cec8297
Andste <andste.com@gmail.com>
2018-03-17 17:22:08 +0800

    分类组件

--- buyer/pc/themes/b2b2c_v5/components/Category.vue
+++ buyer/pc/themes/b2b2c_v5/components/Category.vue
@@ -1,13 +1,60 @@
 <template>
-  <div id="category"></div>
+  <div
+    id="category"
+    @mouseover="handleCategoryMouseover"
+    @mouseout="handleCategoryMouseout"
+  >
+    <nuxt-link to="/goods-list" class="category-title">全部商品分类</nuxt-link>
+    <div v-show="unfold" class="category-layer">
+
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'EnCategory'
+    name: 'EnCategory',
+    data() {
+      return {
+        unfold: this.$route.path === '/'
+      }
+    },
+    computed: {
+      always_show() {
+        return this.$route.path === '/'
+      }
+    },
+    methods: {
+      handleCategoryMouseover() {
+        if (this.always_show) return
+        this.unfold = true
+      },
+      handleCategoryMouseout() {
+        if (this.always_show) return
+        this.unfold = false
+      }
+    }
   }
 </script>
 
 <style type="text/scss" lang="scss" scoped>
-
+  #category {
+    width: 240px;
+    height: 40px;
+    background-color: #ff2828;
+    flex-shrink: 0;
+    z-index: 10;
+    .category-title {
+      display: block;
+      font-size: 18px;
+      font-weight: bold;
+      color: #fff;
+      text-align: center;
+      line-height: 40px;
+    }
+    .category-layer {
+      height: 500px;
+      background-color: rgba(0, 0, 0, .6);
+    }
+  }
 </style>
