d559c4dd2b5648840eaa24fc4da25a9d6f205e4e
赖云川 <andste.cc@gmail.com>
2018-03-18 15:04:10 +0800

    增加分类相关vuex

--- buyer/pc/themes/b2b2c_v5/store/mutation-types.js
+++ buyer/pc/themes/b2b2c_v5/store/mutation-types.js
@@ -1,5 +1,8 @@
 /** 导航栏相关 */
 export const SET_NAV_DATA = 'SET_NAV_DATA'
 
+/** 分类相关 */
+export const SET_CATEGORY_DATA = 'SET_CATEGORY_DATA'
+
 /** 购物车相关 */
 export const SET_CART_DATA = 'SET_CART_DATA'
