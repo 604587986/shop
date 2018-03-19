6b1a3ce8cb788a5ab7db79c3cbe9a80072922664
赖云川 <andste.cc@gmail.com>
2018-03-19 16:20:58 +0800

    更新字体图标css链接

--- buyer/pc/themes/b2b2c_v5/nuxt.config.js
+++ buyer/pc/themes/b2b2c_v5/nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_461357_xir5egvl2xaxxbt9.css' }
+      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_461357_yl8o9pi4h8y3nmi.css' }
     ],
     script: [
       { type: 'text/javascript', src: 'https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js' },
