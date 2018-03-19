5ef9ff7357537b31e63f281d87567d2842ea3c0c
赖云川 <andste.cc@gmail.com>
2018-03-19 16:21:52 +0800

    焦点图轮播、首页信息卡片

--- buyer/pc/themes/b2b2c_v5/pages/index.vue
+++ buyer/pc/themes/b2b2c_v5/pages/index.vue
@@ -1,21 +1,354 @@
 <template>
   <div class="container">
     <en-header/>
-    <nuxt-link to="/goods">去商品页</nuxt-link>
+    <div class="focus-container">
+      <div class="w">
+        <div class="swiper-container" id="swiper-container-index">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <a href="#">
+                <img src="http://javashop-statics.oss-cn-beijing.aliyuncs.com/demo/BEC6DE293CEB40769B43984B0C094906.jpg" alt="" class="focus-img">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#">
+                <img src="http://javashop-statics.oss-cn-beijing.aliyuncs.com/demo/068EFE2D3E294D55990FA58B1F66A340.jpg" alt="" class="focus-img">
+              </a>
+            </div>
+          </div>
+          <div class="swiper-pagination swiper-pagination-index"></div>
+        </div>
+        <div class="index-card">
+          <div class="card-user">
+            <div class="inner-card-user">
+              <div class="card-user-avatar">
+                <a href="#">
+                  <img src="//storage.jd.com/i.imageUpload/6c616979756e636875616e31343834333537343637373532_mid.jpg">
+                </a>
+              </div>
+              <div class="card-user-show">
+                <p>Hi,<a href="#">&ensp;Andste</a></p>
+                <p class="btns">
+                  <nuxt-link to="/login" class="please-login">请登录</nuxt-link>
+                  <a href="#" class="apply-shop">申请开店</a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="card-news">
+            <div class="card-news-tab">
+              <a href="#" @mouseenter="card_news_tab_x = 0">商城公告</a>
+              <a href="#" @mouseenter="card_news_tab_x = 70">联系方式</a>
+              <div class="card-news-tab-active" :style="{transform: 'translateX('+ card_news_tab_x +'px)'}"></div>
+              <a href="#" class="card-news-more">更多</a>
+            </div>
+            <div class="card-news-content">
+              <div class="card-news-con-item" v-show="card_news_tab_x === 0">
+                <ul>
+                  <li class="news-item"><a href="">路由秒杀风暴，抢99减50神券</a></li>
+                  <li class="news-item"><a href="">轮胎3送1每满500减50</a></li>
+                  <li class="news-item"><a href="">春分保养领券最高减100</a></li>
+                  <li class="news-item"><a href="">爆款手机立省800元</a></li>
+                </ul>
+              </div>
+              <div class="card-news-con-item" v-show="card_news_tab_x === 70">
+                <ul>
+                  <li class="news-item"><a href="">京东启用全新客服电话“950618”</a></li>
+                  <li class="news-item"><a href="">大家电订单超期自动取消公告</a></li>
+                  <li class="news-item"><a href="">关于召回普利司通（天津）轮胎有限公司2个规格乘用车轮胎的公告</a></li>
+                  <li class="news-item"><a href="">京东物流推出配送员统一外呼电话"95056”</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="card-service">
+            <a href="" class="service-item">
+              <i class="iconfont ea-icon-point"></i>
+              <span>我的积分</span>
+            </a>
+            <a href="" class="service-item">
+              <i class="iconfont ea-icon-score"></i>
+              <span>我的评价</span>
+            </a>
+            <a href="" class="service-item">
+              <i class="iconfont ea-icon-order"></i>
+              <span>我的订单</span>
+            </a>
+            <a href="" class="service-item">
+              <i class="iconfont ea-icon-star"></i>
+              <span>我的收藏</span>
+            </a>
+            <a href="" class="service-item">
+              <i class="iconfont ea-icon-security"></i>
+              <span>账号安全</span>
+            </a>
+            <a href="" class="service-item">
+              <i class="iconfont ea-icon-shop"></i>
+              <span>商家入驻</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import * as API_Common from '@/api/common'
+  import Swiper from 'swiper'
   export default {
     name: 'index',
+    data() {
+      return {
+        /** 首页卡片tab x坐标 */
+        card_news_tab_x: 0
+      }
+    },
     created() {
     },
     mounted() {
+      this.$nextTick(() => {
+        this.initFocusSwiper()
+      })
+    },
+    methods: {
+      initFocusSwiper() {
+        this.swiper = new Swiper('#swiper-container-index', {
+          autoplay: 2000,
+          speed: 800,
+          pagination: {
+            el: '.swiper-pagination-index',
+            type: 'custom',
+            renderCustom: function(swiper, current, total) {
+              var _btns = ''
+              for (var i = 0; i < total; i++) {
+                i === total - 1
+                  ? (_btns += '<i class="custom-pagination-btn __last__' + (current === i + 1 ? ' __active__' : '') + '"></i>')
+                  : (_btns += '<i class="custom-pagination-btn' + (current === i + 1 ? ' __active__' : '') + '"></i>')
+              }
+              //  修正pagination偏移量
+              return ('<div class="custom-pagination-inner" style="margin-left: -' + (total * 22 + 6) / 2 + 'px">' + _btns + '</div>')
+            }
+          },
+          autoplayDisableOnInteraction: false,
+          effect: 'fade'
+        })
+        const _swiper = this.swiper
+        const $swiperContainerIndex = $('#swiper-container-index')
+        $('.swiper-pagination-index').on('mouseenter', 'i', function() {
+          var $this = $(this), index = $this.index();
+          _swiper.slideTo(index, 500, false);
+        });
+      }
     }
   }
 </script>
 
-<style>
-
+<style type="text/scss" lang="scss" scoped>
+  @import "../node_modules/swiper/dist/css/swiper.min.css";
+  .focus-container {
+    height: 500px;
+    background-color: rgb(241, 241, 241);
+    .w {
+      position: relative;
+    }
+    .swiper-container {
+      height: 100%;
+    }
+    /deep/ .swiper-pagination-index {
+      position: relative;
+      .custom-pagination-inner {
+        display: block;
+        position: absolute;
+        z-index: 1;
+        left: 50%;
+        bottom: 20px;
+        font-size: 0;
+        padding: 4px 8px;
+        border-radius: 12px;
+        background-color: hsla(0, 0%, 100%, 0.4);
+        .custom-pagination-btn {
+          display: inline-block;
+          margin-right: 10px;
+          width: 12px;
+          height: 12px;
+          border-radius: 100%;
+          background-color: #fff;
+          cursor: pointer;
+        }
+        .custom-pagination-btn.__active__ {
+          background-color: #f42424;
+        }
+        .custom-pagination-btn.__last__ {
+          margin-right: 0;
+        }
+      }
+    }
+    .index-card {
+      position: absolute;
+      top: 25px;
+      right: 0;
+      width: 190px;
+      height: 450px;
+      background-color: #fff;
+      z-index: 2;
+      .card-user {
+        height: 140px;
+        text-align: center;
+        background-color: #fff;
+        .inner-card-user {
+          position: relative;
+          padding-top: 62px;
+          height: 78px;
+        }
+        .card-user-avatar {
+          position: absolute;
+          left: 50%;
+          top: -10px;
+          margin-left: -34px;
+          width: 65px;
+          height: 65px;
+          a {
+            display: block;
+            width: 55px;
+            height: 55px;
+            border: 5px solid #e3e1df;
+            -moz-border-radius: 50%;
+            border-radius: 50%;
+            overflow: hidden;
+            box-shadow: 3px 6px 25px #c3c3c3;
+          }
+          img {
+            display: block;
+            width: 55px;
+            height: 55px;
+          }
+        }
+        .card-user-show {
+          padding: 0 10px;
+          a {
+            color: #666;
+            &:hover {
+              color: #f42424;
+            }
+          }
+          .btns {
+            display: flex;
+            justify-content: space-around;
+            margin-top: 10px;
+            a {
+              display: block;
+              float: left;
+              line-height: 22px;
+              width: 68px;
+              height: 22px;
+              border: 1px solid #333;
+              text-align: center;
+            }
+            .apply-shop {
+              border-color: #f42424;
+              color: #f42424;
+            }
+          }
+        }
+      }
+      .card-news {
+        overflow: hidden;
+        height: 130px;
+        background: #fff;
+        .card-news-tab {
+          padding: 8px 0 0;
+          position: relative;
+          line-height: 13px;
+          font-size: 0;
+          a {
+            font-size: 12px;
+            color: #333;
+            padding: 0 10px;
+            border-right: 1px solid #dfe0e1;
+            &:last-child {
+              border-right: none;
+            }
+          }
+        }
+        .card-news-tab-active {
+          position: absolute;
+          bottom: -1px;
+          left: 0;
+          margin-left: 11px;
+          width: 45px;
+          height: 2px;
+          background: #db192b;
+          transform: translateX(0);
+          transition: transform .3s ease;
+        }
+        .card-news-content {
+          position: relative;
+          padding-top: 8px;
+          margin: 0 15px;
+          height: 88px;
+        }
+        .card-news-con-item {
+          height: 100%;
+          overflow: hidden;
+        }
+        .news-item {
+          line-height: 22px;
+          max-width: 160px;
+          _width: 160px;
+          overflow: hidden;
+          text-overflow:ellipsis;
+          white-space: nowrap;
+          color: #999;
+          a {
+            color: #666;
+            &:hover {
+              color: #f42424;
+            }
+          }
+        }
+        .card-news-more {
+          position: absolute;
+          right: 5px;
+          top: 8px;
+          font-size: 12px;
+          &:hover {
+            color: #f42424;
+          }
+        }
+      }
+      .card-service {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+        overflow: hidden;
+        position: relative;
+        height: 160px;
+        transition: all .2s ease;
+        padding: 10px;
+        &::after {
+          content: "";
+          position: absolute;
+          top: 0;
+          display: block;
+          width: 160px;
+          left: 15px;
+          height: 1px;
+          background: #ebeaea;
+          z-index: 1;
+        }
+        .service-item {
+          display: block;
+          .iconfont {
+            display: block;
+            font-size: 30px;
+            line-height: 30px;
+            margin: 5px auto;
+            text-align: center;
+          }
+        }
+      }
+    }
+  }
 </style>
