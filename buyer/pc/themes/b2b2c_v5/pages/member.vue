f0b21acb58aa40f3875769eaa2de042a2974e2c0
赖云川 <andste.cc@gmail.com>
2018-03-21 18:03:46 +0800

    增加个人中心

---
+++ buyer/pc/themes/b2b2c_v5/pages/member.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="member">
+    <en-header/>
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'member'
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
