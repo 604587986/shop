fd20c2985aaf618d4f9491d3a875e03119f79549
赖云川 <andste.cc@gmail.com>
2018-03-23 18:35:36 +0800

    vuex-user

--- buyer/pc/themes/b2b2c_v5/store/mutation-types.js
+++ buyer/pc/themes/b2b2c_v5/store/mutation-types.js
@@ -4,6 +4,10 @@ export const SET_NAV_DATA = 'SET_NAV_DATA'
 /** 分类相关 */
 export const SET_CATEGORY_DATA = 'SET_CATEGORY_DATA'
 
+/** 用户信息相关 */
+// 设置用户信息
+export const SET_USER_DATA = 'SET_USER_DATA'
+
 /** 购物车相关 */
 // 设置购物车数据
 export const SET_CART_DATA = 'SET_CART_DATA'
