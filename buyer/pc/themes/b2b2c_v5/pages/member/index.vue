f014f3a04aac1c94101e1f8bfd2b6b942e669c83
赖云川 <andste.cc@gmail.com>
2018-03-25 11:29:07 +0800

    代码优化

--- buyer/pc/themes/b2b2c_v5/pages/member/index.vue
+++ buyer/pc/themes/b2b2c_v5/pages/member/index.vue
@@ -202,7 +202,7 @@
             spaceBetween: 10,
             navigation: {
               nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
+              prevEl: '.swiper-button-prev'
             }
           })
         })
