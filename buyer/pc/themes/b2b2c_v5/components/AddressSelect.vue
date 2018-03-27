cb2e685a2eb30c70d2507ad490fd9ca913357b5f
赖云川 <andste.cc@gmail.com>
2018-03-27 18:06:17 +0800

    修复bug

--- buyer/pc/themes/b2b2c_v5/components/AddressSelect.vue
+++ buyer/pc/themes/b2b2c_v5/components/AddressSelect.vue
@@ -6,24 +6,33 @@
   export default {
     name: 'EnAddressSelect',
     props: {
-      changed: {
-        type: Function,
-        default: () => {}
-      },
       default: {
         type: Array,
         default: null
       }
     },
+    watch: {
+      default() {
+        this.initAddressSelect()
+      }
+    },
     mounted() {
-      this.$nextTick(() => {
-        $(this.$el).addressSelect({
-          host: 'http://localhost:3001/javashop/',
-          appApi: 'api/base/region/get-children.do?regionid=@id',
-          callback: this.changed,
-          deData: this.default
+      this.initAddressSelect()
+    },
+    methods: {
+      callback(object) {
+        this.$emit('changed', object)
+      },
+      initAddressSelect() {
+        this.$nextTick(() => {
+          $(this.$el).addressSelect({
+            host: 'http://localhost:3001/javashop/',
+            appApi: 'api/base/region/get-children.do?regionid=@id',
+            callback: this.callback,
+            deData: this.default
+          })
         })
-      })
+      }
     }
   }
 </script>
