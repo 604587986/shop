84dcabbf430a4e323f63cc25df2e841aecc9191c
赖云川 <andste.cc@gmail.com>
2018-03-27 14:59:14 +0800

    地区选择插件

---
+++ buyer/pc/themes/b2b2c_v5/components/AddressSelect/main.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+  // import './jquery.addressSelect'
+  export default {
+    name: 'EnAddressSelect',
+    mounted() {
+      console.log($)
+    }
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
