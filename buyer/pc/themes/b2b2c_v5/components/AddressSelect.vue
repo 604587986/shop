d5475329b6f8d2b9a14f88978c82c2cb8db56cde
赖云川 <andste.cc@gmail.com>
2018-03-27 16:53:53 +0800

    地区选择插件增加参数传递

--- buyer/pc/themes/b2b2c_v5/components/AddressSelect.vue
+++ buyer/pc/themes/b2b2c_v5/components/AddressSelect.vue
@@ -6,13 +6,22 @@
   export default {
     name: 'EnAddressSelect',
     props: {
-
+      changed: {
+        type: Function,
+        default: () => {}
+      },
+      default: {
+        type: Array,
+        default: null
+      }
     },
     mounted() {
       this.$nextTick(() => {
         $(this.$el).addressSelect({
           host: 'http://localhost:3001/javashop/',
-          appApi: 'api/base/region/get-children.do?regionid=@id'
+          appApi: 'api/base/region/get-children.do?regionid=@id',
+          callback: this.changed,
+          deData: this.default
         })
       })
     }
