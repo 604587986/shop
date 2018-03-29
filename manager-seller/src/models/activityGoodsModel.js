9f4355d1a77820ed21063fb380bbb1a87a0724e2
yangpei <1208812362@qq.com>
2018-03-29 17:43:55 +0800

    促销管理 活动商品名列表 增加字段

--- manager-seller/src/models/activityGoodsModel.js
+++ manager-seller/src/models/activityGoodsModel.js
@@ -1,5 +1,5 @@
 /**
- * 活动相关模型
+ * 活动商品相关模型
  */
 
 import {
@@ -13,9 +13,17 @@ import {
 
 /** 活动商品模型 */
 export class ActivityGoods extends DataModel {
+  /** 活动名称 */
+  @ServerName('activity_name')
+  activity_name
+
   /** 商品名称 */
   @ServerName('goods_name')
   goods_name
+
+  /** 商品图片 */
+  @ServerName('goods_image')
+  goods_image
 
   /** 审核状态 */
   @ServerName('examine_status')
@@ -26,6 +34,10 @@ export class ActivityGoods extends DataModel {
   start_time
 
   /** 驳回原因 */
+  @ServerName('reject_reason')
+  reject_reason
+
+  /** 活动备注信息 */
   @ServerName('reject_reason')
   reject_reason
 }
