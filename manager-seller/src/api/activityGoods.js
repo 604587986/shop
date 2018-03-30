53ae51b4db85ed46b7a3b78d906729e59d62156a
yangpei <1208812362@qq.com>
2018-03-30 11:35:28 +0800

    团购管理列表 （删除 搜索 筛选）

--- manager-seller/src/api/activityGoods.js
+++ manager-seller/src/api/activityGoods.js
@@ -24,3 +24,24 @@ export function getActivityGoddsList(params) {
     }).catch(error => reject(error))
   })
 }
+
+/**
+ * 删除团购商品
+ * @param ids
+ * @param params
+ * @returns {Promise<any>}
+ */
+export function delActivityGroupGoods(ids, params) {
+  const _formdata = new FormData()
+  Object.keys(params).forEach(key => _formdata.append(key, params[key]))
+  return new Promise((resolve, reject) => {
+    request({
+      url: `http://www.andste.cc/mock/5aa72c080d9d060b4b99b45b/seller/activity/group/goods/${ids}`,
+      method: 'delete',
+      loading: false,
+      data: _formdata
+    }).then(response => {
+      resolve(response)
+    }).catch(error => reject(error))
+  })
+}
