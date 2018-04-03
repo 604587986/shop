3f06cab4d066053383f287f95c5ee084e699bbac
赖云川 <andste.cc@gmail.com>
2018-04-03 11:17:14 +0800

    滚动到顶部mixin增加top传值

--- buyer/pc/themes/b2b2c_v5/plugins/vue-mixin.js
+++ buyer/pc/themes/b2b2c_v5/plugins/vue-mixin.js
@@ -3,8 +3,8 @@ import Vue from 'vue'
 Vue.mixin({
   methods: {
     /** 滚动到顶部【动画】 */
-    MixinScrollToTop() {
-      $("html,body").animate({ scrollTop: 0 }, 300)
+    MixinScrollToTop(top) {
+      $("html,body").animate({ scrollTop: top || 0 }, 300)
     }
   }
 })
