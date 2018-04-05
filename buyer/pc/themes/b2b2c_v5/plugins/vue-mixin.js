44052c0db6703080bb147a356ec1c8803fa6fc63
赖云川 <andste.cc@gmail.com>
2018-04-05 15:37:32 +0800

    将获取图片验证码URL方法加入全局mixin

--- buyer/pc/themes/b2b2c_v5/plugins/vue-mixin.js
+++ buyer/pc/themes/b2b2c_v5/plugins/vue-mixin.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 
+import * as API_Common from '@/api/common'
+
 Vue.mixin({
   methods: {
     /** 滚动到顶部【动画】 */
     MixinScrollToTop(top) {
       $("html,body").animate({ scrollTop: top || 0 }, 300)
+    },
+    /** 获取验证图片URL */
+    MixinGetValidateCodeUrl(type) {
+      return API_Common.getValidateCodeUrl(type)
     }
   }
 })
