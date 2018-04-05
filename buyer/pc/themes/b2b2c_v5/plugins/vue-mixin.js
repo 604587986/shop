ee916d9ee17f8c661bfa8a49a6ddb37551a75b51
赖云川 <andste.cc@gmail.com>
2018-04-05 17:14:17 +0800

    移除获取验证图片URLmixin

--- buyer/pc/themes/b2b2c_v5/plugins/vue-mixin.js
+++ buyer/pc/themes/b2b2c_v5/plugins/vue-mixin.js
@@ -7,10 +7,6 @@ Vue.mixin({
     /** 滚动到顶部【动画】 */
     MixinScrollToTop(top) {
       $("html,body").animate({ scrollTop: top || 0 }, 300)
-    },
-    /** 获取验证图片URL */
-    MixinGetValidateCodeUrl(type) {
-      return API_Common.getValidateCodeUrl(type)
     }
   }
 })
