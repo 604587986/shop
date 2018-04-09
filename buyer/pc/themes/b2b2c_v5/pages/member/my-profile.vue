2107ab88c2e007cf5b128a24c0de55492e21327a
赖云川 <andste.cc@gmail.com>
2018-04-09 20:02:21 +0800

    增加头像修改

--- buyer/pc/themes/b2b2c_v5/pages/member/my-profile.vue
+++ buyer/pc/themes/b2b2c_v5/pages/member/my-profile.vue
@@ -6,13 +6,29 @@
       </ul>
     </div>
     <div class="profile-container">
+      <div class="avatar-box">
+        <el-upload
+          :action="upload_api"
+          class="avatar-uploader"
+          :show-file-list="false"
+          :on-success="handleAvararUploadSuccess"
+        >
+          <img v-if="profileForm.avatar" :src="profileForm.avatar" class="avatar">
+          <img v-else src="https://misc.360buyimg.com/mtd/pc/common/img/no_login.jpg" title="求真相" class="avatar">
+          <div class="eidt-mask">
+            <i class="el-icon-edit-outline"></i>
+            <p>修改头像</p>
+          </div>
+        </el-upload>
+        <p>头像修改在保存后生效</p>
+      </div>
       <el-form :model="profileForm" :rules="profileRules" ref="profileForm" label-width="100px" style="width:350px">
-        <el-form-item label="账户名称">
-          <span>{{ profileForm.username }}</span>
+        <el-form-item label="账户名称" prop="username">
+          <el-input v-model="profileForm.username" size="small" clearable></el-input>
         </el-form-item>
-        <el-form-item label="真实姓名" prop="truename">
-          <el-input v-model="profileForm.truename" size="small" clearable></el-input>
-        </el-form-item>
+        <!--<el-form-item label="真实姓名" prop="truename">-->
+          <!--<el-input v-model="profileForm.truename" size="small" clearable></el-input>-->
+        <!--</el-form-item>-->
         <el-form-item label="昵称" prop="nickname">
           <el-input v-model="profileForm.nickname" size="small" clearable></el-input>
         </el-form-item>
@@ -46,12 +62,14 @@
           <el-button @click="submitProfile">保存资料</el-button>
         </el-form-item>
       </el-form>
+      <span class="clr"></span>
     </div>
   </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
+  import { mapGetters, mapActions } from 'vuex'
+  import * as API_Common from '@/api/common'
   import axios from 'axios'
   import { AddressSelect } from '@/components'
   export default {
@@ -61,14 +79,18 @@
     },
     data() {
       return {
-        /** 修改头像模态框显示状态 */
-        crop_show: false,
+        /** 修改头像，上传API */
+        upload_api: API_Common.uploadApi,
         /** 地区 */
         regions: {},
         /** 个人资料 表单 */
         profileForm: JSON.parse(JSON.stringify(this.$store.state.user.user)) || {},
         /** 个人资料 表单规则 */
         profileRules: {
+          username: [
+            { required: true, message: '请输入真实姓名', trigger: 'blur' },
+            { min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }
+          ],
           truename: [
             { required: true, message: '请输入真实姓名', trigger: 'blur' },
             { min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
@@ -114,21 +136,29 @@
     methods: {
       /** 地区选择器值发生改变 */
       handleAddressSelectChanged(object) {
-        console.log(object)
+        const { regions } = object
+        Object.keys(regions).forEach(key => this.profileForm[key] = regions[key] || '')
       },
-      handleCropSuccess() {},
-      handleCropUploadSuccess() {},
+      /** 头像上传成功 */
+      handleAvararUploadSuccess(res) {
+        this.profileForm.avatar = res
+      },
       /** 保存资料提交表单 */
       submitProfile() {
         this.$refs['profileForm'].validate((valid) => {
           if (valid) {
-            console.log(this.profileForm)
+            this.saveUserInfo(this.profileForm).then(() => {
+              this.$message.success('修改成功！')
+            })
           } else {
             this.$message.error('表单填写有误，请检查！')
             return false
           }
         })
-      }
+      },
+      ...mapActions({
+        saveUserInfo: 'user/saveUserInfoAction'
+      })
     }
   }
 </script>
@@ -136,6 +166,56 @@
 <style type="text/scss" lang="scss" scoped>
   .profile-container {
     padding-top: 15px;
+    .avatar-box {
+      float: left;
+      position: relative;
+      margin-left: 20px;
+      margin-right: 50px;
+      p { text-align: center }
+      .eidt-mask {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 20;
+        background-color: rgba(0,0,0,.3);
+        opacity: 0;
+        transition: opacity .2s ease-in;
+        color: #f5f7fa;
+        i { font-size: 24px }
+        p { font-size: 14px }
+      }
+      &:hover { .eidt-mask { opacity: 1 } }
+      .avatar-uploader {
+        /deep/ .el-upload {
+          border: 1px dashed #d9d9d9;
+          cursor: pointer;
+          position: relative;
+          overflow: hidden;
+          border-radius: 100%;
+          :hover { border-color: #409EFF }
+        }
+      }
+      .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 135px;
+        height: 135px;
+        line-height: 135px;
+        text-align: center;
+      }
+      .avatar {
+        width: 135px;
+        height: 135px;
+        display: block;
+      }
+    }
+    /deep/ .el-form { float: left }
     /deep/ .el-date-editor.el-input { width: 100% }
     /deep/ .app-address { margin-top: 7px }
   }
