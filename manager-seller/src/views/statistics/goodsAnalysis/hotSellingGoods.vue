444578344c70e0022196b1e51750a50fbed0fd1d
yangpei <1208812362@qq.com>
2018-04-11 16:06:48 +0800

    统计分析 商品分析 运营报告 相关路由和语言包 初始化修正 补充提交

---
+++ manager-seller/src/views/statistics/goodsAnalysis/hotSellingGoods.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'hotSellingGoods'
+  }
+</script>
+
+<style lang="scss" type="scss" scoped>
+
+</style>
---
+++ manager-seller/src/views/statistics/operateReport/buyAnalysis.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'buyAnalysis'
+  }
+</script>
+
+<style lang="scss" type="scss" scoped>
+
+</style>
