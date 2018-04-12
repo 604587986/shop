dfd2e4c2805837199749f446b7a2fb3c38c904cf
赖云川 <andste.cc@gmail.com>
2018-04-12 16:53:21 +0800

    抽离skuList组件

--- buyer/pc/themes/b2b2c_v5/pages/member/my-order/_id.vue
+++ buyer/pc/themes/b2b2c_v5/pages/member/my-order/_id.vue
@@ -42,33 +42,19 @@
       </div>
     </div>
     <div v-if="order" class="goods-list">
-      <el-table :data="order.skuList" style="width: 100%">
-        <el-table-column label="商品名称" class-name="goods-name-img">
-          <template slot-scope="scope">
-            <nuxt-link :to="'/goods/' + scope.row.goods_id" target="_blank">
-              <img :src="scope.row.goods_image">
-            </nuxt-link>
-            <nuxt-link :to="'/goods/' + scope.row.goods_id" target="_blank" class="goods-name">{{ scope.row.goods_name }}</nuxt-link>
-          </template>
-        </el-table-column>
-        <el-table-column label="商品单价" width="120">
-          <template slot-scope="scope">{{ scope.row.goods_price | unitPrice('￥') }}</template>
-        </el-table-column>
-        <el-table-column label="数量" width="90">
-          <template slot-scope="scope">{{ scope.row.num }}</template>
-        </el-table-column>
-        <el-table-column label="商品小计" width="120">
-          <template slot-scope="scope">{{ scope.row.goods_total | unitPrice('￥') }}</template>
-        </el-table-column>
-      </el-table>
+      <sku-list :skuList="order.skuList"></sku-list>
     </div>
   </div>
 </template>
 
 <script>
   import * as API_Order from '@/api/order'
+  import SkuList from '../__skuList'
   export default {
     name: 'order-detail',
+    components: {
+      SkuList
+    },
     data() {
       return {
         order: '',
@@ -168,28 +154,5 @@
   }
   .goods-list {
     margin-top: 30px;
-    /deep/ .el-table {
-      border: 1px solid #ebeef5;
-      border-bottom: none;
-      font-size: 12px;
-    }
-    /deep/ .el-table__header .cell { text-align: center }
-    /deep/ .el-table__body .cell { text-align: center }
-    /deep/ .el-table__body .goods-name-img .cell {
-      display: flex;
-      align-items: center;
-      text-align: left;
-      .goods-name {
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 1;
-        overflow: hidden;
-      }
-      img {
-        width: 50px;
-        height: 50px;
-        margin-right: 10px;
-      }
-    }
   }
 </style>
