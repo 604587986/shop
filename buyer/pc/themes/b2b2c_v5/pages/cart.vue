4681b033d448164ad5d6ff91bad062ff1e408067
赖云川 <andste.cc@gmail.com>
2018-04-14 16:09:41 +0800

    购物车样式调整

--- buyer/pc/themes/b2b2c_v5/pages/cart.vue
+++ buyer/pc/themes/b2b2c_v5/pages/cart.vue
@@ -267,5 +267,352 @@
 </script>
 
 <style type="text/scss" lang="scss" scoped>
-  @import "../assets/styles/cart";
+  .cart-header {
+    width: 100%;
+    background: #fff;
+    .w {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      height: 100px;
+      .logo {
+        width: 260px;
+        height: 60px;
+      }
+    }
+  }
+  .cart-content {
+    position: relative;
+    width: 100%;
+    margin-top: 20px;
+    background: #f5f5f5;
+    font-size: 12px;
+    overflow: hidden;
+    padding-bottom: 20px;
+    .list-header {
+      margin: 10px auto;
+      padding: 0 30px;
+      height: 40px;
+      background: #fff;
+      color: #666;
+      font-size: 12px;
+      line-height: 40px;
+      border: 1px solid #eee;
+      border-radius: 3px;
+      span {
+        display: inline-block;
+        color: #333;
+      }
+      .checkbox {
+        width: 125px;
+        cursor: pointer;
+      }
+      .title { width: 280px }
+      .price { width: 165px; text-align: right; font-size: 12px; font-weight: normal }
+      .num { width: 280px; text-align: center }
+      .weight { width: 90px; text-align: center }
+      .total { width: 108px; text-align: center }
+      .action { width: 80px; text-align: center }
+    }
+    .list-content {
+      .empty-cart {
+        height: 280px;
+        margin: 0 auto;
+        overflow: hidden;
+        position: relative;
+        width: 380px;
+        z-index: 1;
+        padding: 50px 0 20px 280px;
+        background: url(../assets/images/icon-empty-cart.jpg) no-repeat left 40px;
+        h1 {
+          color: #333;
+          font: lighter 22px/30px "microsoft yahei";
+          width: 380px;
+          line-height: 30px;
+          margin: 55px 0 10px 0;
+        }
+        a {
+          width: 100px;
+          height: 25px;
+          line-height: 25px;
+          float: left;
+          text-align: center;
+          margin-right: 10px;
+          border: 1px solid #dcdcdc;
+          background: #f5f5f5;
+          font-size: 12px;
+          color: #666;
+          &:hover { color: #f42424 }
+        }
+      }
+    }
+    .shop-item {
+      background-color: #fff;
+      margin-top: 20px;
+      color: #333333;
+      font-size: 12px;
+      border: 1px solid #dcdcdc;
+      border-radius: 3px;
+      .item {
+        display: flex;
+        align-items: center;
+        position: relative;
+        .check {
+          position: absolute;
+          left: 0;
+          top: 50%;
+          margin-top: -7px;
+          z-index: 1;
+        }
+        .sku-pic {
+          position: relative;
+          float: left;
+          width: 75px;
+          height: 75px;
+          img {
+            width: 100%;
+            height: 100%;
+          }
+        }
+        .sku-name {
+          float: left;
+          width: 275px;
+          padding-left: 20px;
+          overflow: hidden;
+          color: #333333;
+          font-size: 12px;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+          &:hover { color: #f42424 }
+        }
+        .sku-price {
+          position: relative;
+          float: left;
+          width: 165px;
+          text-align: right;
+          font-size: 12px;
+        }
+        .sku-num {
+          display: flex;
+          justify-content: center;
+          position: relative;
+          float: left;
+          width: 280px;
+        }
+        .num-action {
+          padding: 3px 0;
+          width: 80px;
+          height: 18px;
+          background-color: #fff;
+          overflow: hidden;
+          border: 1px solid #dfdfdf;
+          border-radius: 2px;
+          .oper {
+            float: left;
+            width: 20px;
+            text-align: center;
+            color: #333333;
+            font-family: "Tahoma";
+            font-size: 14px;
+            overflow: hidden;
+            user-select: none;
+            &.unable {
+              color: #dfdfdf;
+              cursor: default;
+            }
+          }
+          .input {
+            float: left;
+            width: 38px;
+            height: 18px;
+            border: none;
+            border-left: 1px solid #dfdfdf;
+            border-right: 1px solid #dfdfdf;
+            text-align: center;
+            line-height: 18px;
+            outline: none;
+          }
+        }
+        .sku-weight {
+          width: 90px;
+          text-align: center;
+          font-size: 12px;
+        }
+        .sku-total {
+          width: 108px;
+          text-align: center;
+          font-size: 12px;
+          color: #ff5e5e;
+        }
+        .sku-action {
+          width: 80px;
+          text-align: center;
+          i {
+            font-size: 18px;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+    .shop-header {
+      display: flex;
+      align-items: center;
+      padding: 15px 30px;
+      height: 25px;
+      line-height: 25px;
+      border-bottom: 1px solid #dcdcdc;
+      .shop-name {
+        font-size: 14px;
+        color: #333;
+        font-weight: 600;
+      }
+    }
+    .shop-body {
+      .sku-item {
+        padding: 0 30px;
+        background-color: #fff;
+        transition: background-color 0.2s ease-in-out;
+        &:hover {
+          background-color: #fff7f7;
+        }
+        .item {
+          position: relative;
+          padding: 20px 0 20px 35px;
+        }
+      }
+    }
+    .shop-footer {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      height: 50px;
+      padding-right: 25px;
+      border-top: 1px solid #dcdcdc;
+      border-bottom-left-radius: 5px;
+      border-bottom-right-radius: 5px;
+      background-color: #fefcfc;
+      .shop-footer-item {
+        padding-left: 20px;
+        &.price {
+          font-size: 12px;
+          font-weight: normal;
+          color: #ff5e5e;
+          span {
+            font-size: 14px;
+            font-weight: bold;
+          }
+        }
+      }
+    }
+  }
+  .cart-checkbar {
+    display: block;
+    width: 100%;
+    height: 60px;
+    &.fiexd-top {
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 1110;
+      box-shadow: 0 0 10px #ccc;
+    }
+    &.fiexd-bottom {
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      z-index: 1110;
+      min-width: 1190px;
+      box-shadow: 0 0 10px #ccc;
+    }
+    .check-bar {
+      position: relative;
+      z-index: 10;
+      width: 100%;
+      height: 60px;
+      background-color: #fff;
+      .w {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+    .check-bar-left {
+      display: flex;
+      align-items: center;
+      height: 60px;
+      color: #dedede;
+      overflow: hidden;
+      .select-all {
+        float: left;
+        margin: 0 18px 0 30px;
+        cursor: pointer;
+        span { color: #333333; font-size: 13px }
+        b { font-style: normal }
+      }
+      em { font-size: 16px }
+      .check-tool-a {
+        margin: 0 18px;
+        font-size: 12px;
+        color: #999;
+      }
+    }
+    .check-bar-right {
+      display: flex;
+      align-items: center;
+      height: 60px;
+      .check-btn {
+        display: inline-block;
+        width: 130px;
+        height: 60px;
+        line-height: 60px;
+        text-align: center;
+        background-color: #ff5e5e;
+        font-weight: bold;
+        font-size: 17px;
+        color: #fff;
+        cursor: pointer;
+        user-select: none;
+        transition: background-color 0.2s ease-in-out;
+        &.disabled {
+          pointer-events: none;
+          background-color: #ccc;
+          cursor: default;
+        }
+      }
+      em { font-size: 16px; color: #dedede }
+      span { margin: 0 20px }
+      .price i { font-size: 20px }
+    }
+  }
+  .check {
+    display: inline-block;
+    position: relative;
+    top: -1px;
+    width: 13px;
+    height: 13px;
+    line-height: normal;
+    margin-right: 20px;
+    background-color: #fff;
+    border: 1px solid #dcdcdc;
+    border-radius: 3px;
+    transition: background-color 0.2s ease-in-out;
+    vertical-align: middle;
+    &.checked {
+      background-color: #ff5e5e;
+      border-color: #ff5e5e;
+      i { opacity: 1 }
+    }
+    i {
+      opacity: 0;
+      font-size: 14px !important;
+      color: #fff;
+      user-select: none;
+      transition: opacity 0.2s ease-in-out;
+    }
+  }
+  .price {
+    font-size: 16px;
+    color: #ff5e5e;
+    font-weight: bold;
+  }
 </style>
