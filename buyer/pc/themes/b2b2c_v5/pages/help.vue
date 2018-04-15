25e01ba91f349882692f4f1f9284d44ba5baf1ec
赖云川 <andste.cc@gmail.com>
2018-04-15 15:05:33 +0800

    增加帮助中心

---
+++ buyer/pc/themes/b2b2c_v5/pages/help.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="help-container w">
+    <div class="help-menus">
+      menus
+    </div>
+    <nuxt-child class="help-content"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'help',
+    layout: 'full'
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+  .help-container {
+    display: flex;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+  .help-menus {
+    width: 208px;
+  }
+  .help-content {
+    width: 980px;
+    min-height: 300px;
+    margin-left: 10px;
+    padding: 20px;
+    border: 1px solid #e6e6e6;
+  }
+</style>
---
+++ buyer/pc/themes/b2b2c_v5/pages/help/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="help"></div>
+</template>
+
+<script>
+	export default {
+		name: 'help-index'
+	}
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
