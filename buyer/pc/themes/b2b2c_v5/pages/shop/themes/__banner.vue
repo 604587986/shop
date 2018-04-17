8a0c8f8c41fe88df7fd2c9577e74d5af1750afd9
赖云川 <andste.cc@gmail.com>
2018-04-17 15:14:35 +0800

    店铺横幅组件抽离

---
+++ buyer/pc/themes/b2b2c_v5/pages/shop/themes/__banner.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-carousel height="500px" class="banner-container">
+    <el-carousel-item
+      v-for="banner in banners"
+      :key="banner.id"
+    >
+      <nuxt-link :to="banner.link">
+        <img :src="banner.image" class="banner-img">
+      </nuxt-link>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import { Carousel, CarouselItem } from 'element-ui'
+  Vue.use(Carousel)
+  Vue.use(CarouselItem)
+  export default {
+    name: 'shop-banner',
+    props: ['banners']
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+  .banner-container {
+    /deep/ .el-carousel__arrow--left {
+      left: 100px;
+    }
+    /deep/ .el-carousel__arrow--right {
+      right: 100px;
+    }
+    .banner-img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+</style>
