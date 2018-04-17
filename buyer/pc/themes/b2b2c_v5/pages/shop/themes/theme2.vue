c9c4fb68061783ae3cc245ede41f028f5174b191
赖云川 <andste.cc@gmail.com>
2018-04-17 15:18:23 +0800

    theme1,theme2,theme3

---
+++ buyer/pc/themes/b2b2c_v5/pages/shop/themes/theme2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="shop-theme-2">
+    <en-banner :banners="shop.shop_banner"/>
+  </div>
+</template>
+
+<script>
+  import mixin from './themeMixin'
+  export default {
+    name: 'shop-theme-2',
+    mixins: [mixin]
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
