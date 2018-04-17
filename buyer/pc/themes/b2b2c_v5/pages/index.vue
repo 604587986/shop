307ebcd1fca189d8da29732ecad490d53ca58ffc
赖云川 <andste.cc@gmail.com>
2018-04-17 11:51:16 +0800

    首页js大小优化

--- buyer/pc/themes/b2b2c_v5/pages/index.vue
+++ buyer/pc/themes/b2b2c_v5/pages/index.vue
@@ -83,6 +83,10 @@
 </template>
 
 <script>
+  import Vue from 'vue'
+  import { Carousel, CarouselItem } from 'element-ui'
+  Vue.use(Carousel)
+  Vue.use(CarouselItem)
   import * as API_Common from '@/api/common'
   export default {
     name: 'index',
