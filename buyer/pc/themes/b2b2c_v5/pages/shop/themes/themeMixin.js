0bdeb8ce42a729f6e2832759fc2c739904f5442b
赖云川 <andste.cc@gmail.com>
2018-04-17 15:14:18 +0800

    店铺主题mixin

---
+++ buyer/pc/themes/b2b2c_v5/pages/shop/themes/themeMixin.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import * as API_Shop from '@/api/shop'
+import banner from './__banner'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  props: ['shop'],
+  components: {
+    'en-banner': banner
+  },
+  mounted() {
+  
+  },
+  methods: {
+  
+  }
+}
