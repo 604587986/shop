b58819914b413805663f80ec84ab09a223e349c7
赖云川 <andste.cc@gmail.com>
2018-04-17 18:56:04 +0800

    店铺主题1，热卖商品

--- buyer/pc/themes/b2b2c_v5/pages/shop/themes/themeMixin.js
+++ buyer/pc/themes/b2b2c_v5/pages/shop/themes/themeMixin.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import * as API_Shop from '@/api/shop'
+import * as API_Collection from '@/api/collection'
 import banner from './__banner'
 
 export default {
   data() {
     return {
+      shop_id: this.shop.shop_id
     }
   },
   props: ['shop'],
@@ -15,6 +17,10 @@ export default {
   
   },
   methods: {
-  
+    collectionShop() {
+      API_Collection.collectionShop(this.shop_id).then(() => {
+        this.$message.success('收藏成功！')
+      })
+    }
   }
 }
