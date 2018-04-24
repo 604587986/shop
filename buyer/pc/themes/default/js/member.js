56a97f96f5ec2c2ee270b3bad10644e93dda52aa
赖云川 <andste.cc@gmail.com>
2018-04-24 18:21:31 +0800

    移除无用文件

--- buyer/pc/themes/default/js/member.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-	/** 渲染个人概览 */
-	var userInfo = JSON.parse(window.localStorage.getItem('userInfo'));
-	$.laytpl($('#overview-member-tpl'), $('#overview-member-view')).render(userInfo);
-})
