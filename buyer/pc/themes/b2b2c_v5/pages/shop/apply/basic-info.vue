6b2d312a6c5281d91ccf0d2252bfc11f19415548
赖云川 <andste.cc@gmail.com>
2018-04-24 16:03:20 +0800

    开店申请增加 基础信息页面

---
+++ buyer/pc/themes/b2b2c_v5/pages/shop/apply/basic-info.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="basic-info">
+    basic-info
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "basic-info"
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
