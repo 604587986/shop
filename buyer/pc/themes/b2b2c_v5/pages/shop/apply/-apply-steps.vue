2d8d298c8352216b5f6f6ba734895beb692493a4
赖云川 <andste.cc@gmail.com>
2018-04-24 15:14:27 +0800

    店铺申请步骤条

--- buyer/pc/themes/b2b2c_v5/pages/shop/apply/-apply-steps.vue
+++ buyer/pc/themes/b2b2c_v5/pages/shop/apply/-apply-steps.vue
@@ -1,15 +1,60 @@
 <template>
-  <div id="apply-steps">
-    apply-steps
+  <div id="apply-steps" class="steps-container">
+    <ul>
+      <li class="step1" :class="[step === 1 && 'current']"><span>入驻协议</span></li>
+      <li class="step2" :class="[step === 2 && 'current']"><span>基本信息</span></li>
+      <li class="step3" :class="[step === 3 && 'current']"><span>认证信息</span></li>
+      <li class="step4" :class="[step === 4 && 'current']"><span>财务资质信息</span></li>
+      <li class="step5" :class="[step === 5 && 'current']"><span>店铺信息</span></li>
+    </ul>
   </div>
 </template>
 
 <script>
 	export default {
-		name: 'apply-steps'
+		name: 'apply-steps',
+    props: {
+		  step: {
+		    type: Number,
+        default: 1
+      }
+    }
 	}
 </script>
 
 <style type="text/scss" lang="scss" scoped>
-
+  .steps-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 99px;
+    border-bottom: 1px dashed #D7D7D7;
+    li {
+      position: relative;
+      font-size: 12px;
+      background: url(~/assets/images/icons-apply-shop.png) no-repeat 0 -130px;
+      vertical-align: top;
+      letter-spacing: normal;
+      word-spacing: normal;
+      display: inline-block;
+      width: 120px;
+      height: 16px;
+      z-index: 1;
+      span {
+        color: #999;
+        text-align: center;
+        width: 120px;
+        height: 20px;
+        margin-left: -60px;
+        position: absolute;
+        z-index: 1;
+        bottom: -30px;
+        left: 50%;
+      }
+      &.step1, &.step5 { width: 68px }
+      &.current {
+        background-position: -172px -130px;
+      }
+    }
+  }
 </style>
