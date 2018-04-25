7442840473d560f8d60246a766a08bb5a282b805
yangpei <1208812362@qq.com>
2018-04-25 19:33:33 +0800

    重新编写商品规格选择器 部分提交 我怕丢了

--- manager-seller/src/components/SkuSelector/src/SkuItem.vue
+++ manager-seller/src/components/SkuSelector/src/SkuItem.vue
@@ -1,151 +1,272 @@
 <template>
   <div class="sku-item-content">
-    <div v-for="(item, $index) in skuInfo" :key="$index">
-      <div class="sku-item-first">
-        <!--规格项列表select-->
-        <el-select
-          v-model="currentSkuItem"
-          filterable
-          allow-create
-          default-first-option
-          @change="handleChangeSkuItem">
-          <el-option
-            v-for="(elem, index) in skuData" :key="index"
-            :label="elem.spec_name"
-            :value="elem.spec_memo">
-          </el-option>
-        </el-select>
-        <!--添加规格图片-->
-        <el-checkbox v-if="$index === 0 " v-model="checkedImage" @change="handleChangeImage">添加规格图片</el-checkbox>
-        <i class="el-icon-error close-sku-item" @click="handleCloseSku($index)" ></i>
-      </div>
-      <div class="sku-value-list">
-        <!--规格值文本列表-->
-        <div v-for="(spec, index) in specList" :key="index" >
-          <el-tag  closable @close="delTag(spec)">
-            {{spec.spec_value}}
-          </el-tag>
-          <!--规格值图片 上传列表-->
-          <div v-show="index === 0 && checkedImage">
-            sdfioada
+    <el-form  :model="skuForm" >
+      <div v-for="(item, $index) in skuInfo" :key="$index">
+        <el-form-item label="规格名：" prop="spec_name">
+          <el-autocomplete
+            style="width: 170px;"
+            class="inline-input"
+            v-model="item.spec_name"
+            value-key="spec_name"
+            :fetch-suggestions="querySearchSkuItem"
+            placeholder="请输入规格项名称"
+            @select="handleSelectSkuItem(item, $index)">
+            <!--@blur.naitve="editSkuItem(item, $index)"-->
+          </el-autocomplete>
+          <el-checkbox v-if="$index === 0 " v-model="checkedImage" @change="handleChangeImage">添加规格图片</el-checkbox>
+          <div class="empty"></div>
+          <el-button type="danger"   size="mini" @click="handleCloseSkuItem($index)" icon="el-icon-delete"></el-button>
+        </el-form-item>
+        <el-form-item label="规格值：" prop="spec_value">
+          <!--规格值文本列表-->
+          <div  v-for="(val, index) in item.value_list" :key="index" style="padding: 10px 10px 10px 0;">
+            <el-autocomplete
+              class="inline-input"
+              style="width: 170px;"
+              v-model="val.spec_value"
+              :key="index"
+              value-key="spec_value"
+              :fetch-suggestions="querySearchSkuValue"
+              placeholder="请输入规格值名称"
+              @select="handleSelectSkuValue(val, $index, index)">
+              <!--@blur.naitve="editSkuIValue(val, $index, index)"-->
+              <template slot="append">
+                <el-button type="danger" size="mini" @click="handleCloseSkuValue($index, index)" icon="el-icon-delete"></el-button>
+              </template>
+            </el-autocomplete>
+            <!--规格值图片 上传列表-->
+            <div v-show="$index === 0 && checkedImage">
+              我是上传组件
+            </div>
           </div>
-        </div>
-        <el-button type="text" plain @click="addSpec">+添加</el-button>
+          <el-button type="text" plain size="mini" style="margin-left: 10px;"  class="add-btn-skuval" @click="addSpec($index)">添加规格值</el-button>
+        </el-form-item>
       </div>
-      <!--规格值列表 select-->
-      <div v-show="isShowSkuValue">
-        <el-select
-          v-model="currentSkuValue"
-          multiple
-          filterable
-          allow-create
-          default-first-option
-          @change="handleChangeSkuValue">
-          <el-option
-            v-for="(spec, _index) in specList"
-            :key="_index"
-            :label="spec.spec_value"
-            :value="spec.spec_value_id">
-          </el-option>
-        </el-select>
-        <el-button type="primary" @click="confirmSku" size="mini">确认</el-button>
-        <el-button type="primary" @click="cancelSku" size="mini">取消</el-button>
-      </div>
-    </div>
+    </el-form>
+    <el-button type="primary" size="mini" @click="addSkuItem">添加规格项目</el-button>
   </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
   export default {
     name: 'SkuItem',
-    computed: {
-      ...mapGetters([
-        'skuData', 'skuInfo'
-      ])
+    props: {
+      /** 请求数据 */
+      skuData: {
+        type: Array,
+        default: []
+      },
+
+      /** 商品id */
+      goodsId: {
+        type: String,
+        default: ''
+      },
+
+      /** 分类id*/
+      categoryId: {
+        type: String,
+        default: ''
+      }
     },
     data() {
       return {
+        /** 表单数据 */
+        skuForm: {},
 
-        /** 当前规格项*/
-        currentSkuItem: '',
+        /** 要提交的规格数据*/
+        skuInfo: [
+          {
+            /** 规格描述 */
+            spec_memo: '',
+
+            /** 规格名称 */
+            spec_name: '',
+
+            /** 规格值列表 */
+            value_list: [
+              {
+                /** 规格项id */
+                spec_id: '',
+
+                /** 规格值名字 */
+                spec_value: '',
+
+                /** 规格值id */
+                spec_value_id: ''
+              }
+            ]
+          }
+        ],
 
         /** 当前规格值 */
         currentSkuValue: '',
 
-        /** 是否可添加规格图片 取决条件：是否是第一项 */
-        isShowImgage: false,
-
         /** 当前规格项下的规格值列表*/
         specList: [
-          { spec_value_id: '0', spec_value: '你好大蛇阿比' }
+          { spec_value_id: '0', spec_value: '' }
         ],
 
         /** 是否选中 添加规格图片*/
         checkedImage: false,
 
-        /** 是否显示规格值选择select */
+        /** 是否显示规格值 */
         isShowSkuValue: false,
 
         /** 当前正在操作的规格值列表 */
         operaSpecsList: []
       }
     },
-    mounted() {
-      console.log(this.skuData, 666)
-    },
     methods: {
-      /** 移除规格值标签 同步数据*/
-      delTag(target) {
-        console.log(target)
-        this.skuData.forEach((elem) => {
-          if (elem.spec_list[0].spec_id === target.spec_id) {
-            elem.spec_list.forEach((key, index) => {
-              if (key.spec_value_id === target.spec_value_id) {
-                elem.spec_list.splice(index, 1)
-              }
-            })
-          }
+
+      /** 添加规格项 */
+      addSkuItem() {
+        this.skuInfo.push({})
+      },
+
+      /** 移除当前规格项 进行数据变化*/
+      handleCloseSkuItem($index) {
+        this.skuInfo.splice($index, 1)
+        this.$emit('updateSkuInfo', this.skuInfo)
+      },
+
+      /** 点击查询输入规格项建议*/
+      querySearchSkuItem(queryString, cb) {
+        const restaurants = this.skuData.map((key) => { return key })
+        const results = queryString ? restaurants.filter(this.createFilterSkuItem(queryString)) : restaurants
+        cb(results)
+      },
+
+      /** 筛选符合输入信息的规格项 */
+      createFilterSkuItem(queryString) {
+        return (restaurant) => {
+          return (restaurant.spec_name.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+        }
+      },
+
+      /** 选择规格项时触发  */
+      handleSelectSkuItem(item, $index) {
+        /** 检测是否已存在*/
+        const exited = this.skuInfo.some((key) => {
+          return key.spec_name === item.spec_name
         })
-        this.$store.dispatch('updateSkuData', this.skuData)
+        const _skuInfo = this.skuInfo.filter((key) => {
+          return key.spec_name === item.spec_name
+        })
+        if (exited && _skuInfo.length > 1) {
+          this.$message.error('当前项已存在，请重新选择或者编辑！')
+          this.$set(this.skuInfo[$index], 'spec_name', '')
+          return
+        }
+        /** 更新skuInfo数据 */
+        this.$set(this.skuInfo[$index], 'spec_memo', item.spec_memo || '')
+        this.$set(this.skuInfo[$index], 'spec_name', item.spec_name || '')
+        this.$set(this.skuInfo[$index], 'value_list', item.value_list || [])
+
+        /** 设置当前规格值列表 */
+        this.specList = this.skuData[$index].spec_list
       },
 
-      /** 当前规格项发生改变时触发*/
-      handleChangeSkuItem(val) {
-        console.log(val, 5896)
-        this.specList = val.value_list
+      /** 编辑规格项结束时触发添加事件 blur */
+      editSkuItem(item, $index) {
+        /** 检测是否已存在*/
+        // const exited = this.skuInfo.some((key) => {
+        //   return key.spec_name === item.spec_name
+        // })
+        // if (exited) {
+        //   this.$message.error('当前项已存在，请重新选择或者编辑！')
+        //   return
+        // }
+        /** 更新提交数据  按道理应该等请求数据回来之后再进行赋值 此处先这么用着等组件大致完结的时候再修正*/
+        this.skuInfo[$index] = {
+          spec_memo: item.spec_memo || '',
+          spec_name: item.spec_name || '',
+          value_list: [
+            {
+              spec_id: '',
+
+              spec_value: '',
+
+              spec_value_id: ''
+            }
+          ]
+        }
+
+        /** 更新下拉列表规格项数据 */
+        this.$emit('updateSkuItem', item.value)
       },
 
-      /** 当前规格值发生改变时触发 */
-      handleChangeSkuValue(val) {
-        console.log(val, 36363)
-        this.operaSpecsList.push(val)
-      },
-
-      /** 添加规格项的值*/
-      addSpec() {
-        this.isShowSkuValue = true
-      },
-
-      /** 选中/不选中 添加规格图片*/
+      /** 选中/不选中 添加规格图片 是否显示上传组件*/
       handleChangeImage(val) {
         this.checkedImage = val
       },
 
-      /** 添加规格值确认 执行异步操作 提交当前添加的当前规格项下的规格值 并根据返回值进行数据修补*/
-      confirmSku() {
-        this.isShowSkuValue = false
+      /** 规格值 */
+      /** 添加当前规格项的规格值*/
+      addSpec($index) {
+        if (!this.skuInfo[$index] || !this.skuInfo[$index].value_list) {
+          this.$message.warning('请选择规格项')
+          return
+        }
+        this.skuInfo[$index].value_list.push({
+          /** 规格项id */
+          spec_id: '',
+
+          /** 规格值名字 */
+          spec_value: '',
+
+          /** 规格值id */
+          spec_value_id: ''
+        })
       },
 
-      /** 取消*/
-      cancelSku() {
-        this.isShowSkuValue = false
+      /** 点击查询输入规格值建议*/
+      querySearchSkuValue(queryString, cb) {
+        const restaurants = this.specList.map((key) => { return key })
+        const results = queryString ? restaurants.filter(this.createFilterSkuVal(queryString)) : restaurants
+        cb(results)
       },
 
-      /** 移除当前规格项 同步数据*/
-      handleCloseSku(index) {
-        this.skuData.splice(index, 1)
-        // this.$store.dispatch('updateSkuData', this.skuData)
+      /** 筛选符合输入信息的规格值 */
+      createFilterSkuVal(queryString) {
+        return (restaurant) => {
+          return (restaurant.spec_value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+        }
+      },
+
+      /** 移除当前规格值 */
+      handleCloseSkuValue($index, index) {
+        this.skuInfo[$index].value_list.splice(index, 1)
+        this.$emit('updateSkuInfo', this.skuInfo)
+      },
+
+      /** 选择规格值时触发 */
+      handleSelectSkuValue(val, $index, index) {
+        /** 检测是否已存在*/
+        const exited = this.skuInfo[$index].value_list.some((key) => {
+          return key.spec_value === val.spec_value
+        })
+        const _value_list = this.skuInfo[$index].value_list.filter((key) => {
+          return key.spec_value === val.spec_value
+        })
+        if (exited && _value_list.length > 1) {
+          this.$message.error('当前项已存在，请重新选择或者编辑！')
+          this.$set(this.skuInfo[$index].value_list[index], 'spec_value', '')
+          return
+        }
+        /** 更新skuInfo数据 */
+        this.skuInfo[$index].value_list[index] = val
+        this.$emit('updateSkuInfo', this.skuInfo)
+      },
+
+      /** 编辑规格值时触发 */
+      editSkuIValue(val, $index, index) {
+        /** 更新下拉列表规格项数据 */
+        // this.$emit('updateSkuItem', val)
+
+        /** 更新skuInfo数据 */
+        this.skuInfo[$index].value_list[index] = val
+        console.log(this.skuInfo, 56)
       }
     }
   }
@@ -153,7 +274,7 @@
 <style lang="scss" type="scss" scoped>
   .sku-item-content {
     div {
-      margin: 10px;
+      margin: 0px;
     }
   }
 
@@ -168,12 +289,63 @@
   .close-sku-item {
     cursor: pointer;
   }
-
-  /** 第一行 */
-  .sku-item-first {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: flex-start;
+  /*表单结构*/
+  .el-form {
+    border: 1px solid #e5e5e5;
+    padding: 10px;
+    .el-form-item {
+      padding: 5px 10px;
+    }
+    /** 规格项 */
+    .el-form-item:first-child {
+      background-color: #f8f8f8;
+      cursor: pointer;
+      /deep/ .el-form-item__label + div {
+        width: 90%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: flex-start;
+        align-items: center;
+        div {
+          flex-grow: 1;
+        }
+        label {
+          flex-grow: 1;
+          display: inline-block;
+          margin-left: 10px;
+        }
+        div.empty {
+          flex-grow: 200;
+        }
+        button {
+          flex-grow: 1;
+        }
+      }
+    }
+    /*规格值*/
+    .el-form-item:last-child {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      justify-content: flex-start;
+      align-items: center;
+      /deep/ .el-form-item__label + div {
+        width: 90%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        button.add-btn-skuval {
+          margin-left: 10px;
+          outline: none;
+        }
+      }
+    }
   }
+
+
+
+
+
 </style>
--- manager-seller/src/components/SkuSelector/src/SkuTable.vue
+++ manager-seller/src/components/SkuSelector/src/SkuTable.vue
@@ -1,10 +1,112 @@
 <template>
-
+  <div>
+    {{ skuInfo }}
+    <el-table
+      :data="skuInfo"
+      border
+      :span-method="arraySpanMethod"
+      style="width: 100%">
+      <el-table-column
+        v-for="item in tablehead"
+        :prop="item"
+        :key="item"
+        :label="labeltxt(item)"
+        align="center"
+        contenteditable="true"
+      />
+    </el-table>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'skuTable'
+    name: 'skuTable',
+    props: {
+      skuInfo: {
+        type: Array,
+        default: [{
+          sn: '65656262',
+          weight: 45,
+          stock: 45,
+          cost: '25.25',
+          price: '56.32'
+        }]
+      },
+      tablehead: {
+        type: Array,
+        default: []
+      }
+    },
+    watch: {
+      skuInfo: function() { // 此方法可能中毒了 不能使用箭头函数 原因不明
+        this.concactArray = []
+        this.skuInfo.forEach((key, index) => {
+          this.concactArrayCom(index, key)
+          console.log(this.concactArray, 'bjhb ')
+        })
+      }
+    },
+    mounted() {
+      this.skuInfo.forEach((key, index) => {
+        this.concactArrayCom(index, key)
+      })
+    },
+    data() {
+      return {
+        b: 5,
+        tableData: this.skuInfo,
+        concactArray: []
+      }
+    },
+    methods: {
+      /** 表头文本格式化 */
+      labeltxt(item) {
+        let _output = ''
+        switch (item) {
+          case 'sn': _output = '货号'; break
+          case 'weight': _output = '重量(kg)'; break
+          case 'stock': _output = '库存'; break
+          case 'cost': _output = '成本价'; break
+          case 'price': _output = '价格(元)'; break
+          default: _output = item
+        }
+        return _output
+      },
+
+      /** 合并数据相同的单元格 */
+      arraySpanMethod({ row, column, rowIndex, columnIndex }) {
+        if (columnIndex < this.tablehead.length - 5) {
+          const _row = this.concactArray[rowIndex]
+          const _col = _row >= 1 ? 1 : 0
+          return {
+            rowspan: _row,
+            colspan: _col
+          }
+        }
+      },
+      concactArrayCom(index, item) {
+        console.log(index, 5555)
+        let _isMerge = false
+        if (index === 0) {
+          this.concactArray.push(1)
+        } else {
+          for (let i = 0, _len = this.tablehead.length; i < _len; i++) {
+            if (i < this.tablehead.length - 5) {
+              this.concactArray.push(1)
+              console.log(item[this.tablehead[i]], this.skuInfo[index - 1][this.tablehead[i]])
+              if (item[this.tablehead[i]] !== this.skuInfo[index - 1][this.tablehead[i]]) {
+                _isMerge = true
+              } else {
+                console.log('weishenme')
+                if (!_isMerge) {
+                  this.concactArray[index] += 1
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 </script>
 
