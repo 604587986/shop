e3308740bea8632530a9eef8a27c285c12c51e6a
赖云川 <andste.cc@gmail.com>
2018-05-03 17:13:59 +0800

    store调整

--- buyer/pc/themes/b2b2c_v5/store/index.js
+++ buyer/pc/themes/b2b2c_v5/store/index.js
@@ -1,12 +1,17 @@
 import * as types from './mutation-types'
 import * as API_Common from '@/api/common'
+import uuidv1 from 'uuid/v1'
+import Cookie from 'cookie'
+import Storage from '@/utils/storage'
 
 /** state */
 export const state = () => ({
   // 导航栏
   navList: [],
   // 分类
-  categoryList: []
+  categoryList: [],
+  // uuid
+  uuid: ''
 })
 
 /** mutations */
@@ -18,14 +23,36 @@ export const mutations = {
   /** 设置分类数据 */
   [types.SET_CATEGORY_DATA](state, data) {
     state.categoryList = data
+  },
+  /** 设置UUID */
+  [types.SET_UUID](state, uuid) {
+    state.uuid = uuid
+    process.client && Storage.setItem('uuid', uuid)
   }
 }
 
 /** actions */
 export const actions = {
   /** nuxt服务初始化时调用 */
-  nuxtServerInit ({ commit }, { req }) {
-  
+  nuxtServerInit ({ commit }, { req, res }) {
+    let _uuid = ''
+    if (req.headers.cookie) {
+      const cookies = Cookie.parse(req.headers.cookie)
+      commit('user/SET_USER_INFO', global.JSON.parse(cookies.user || "{}"))
+      commit('user/SET_ACCESS_TOKEN', cookies.accessToken)
+      commit('user/SET_REFRESH_TOKEN', cookies.refreshToken)
+      _uuid = cookies.uuid
+    }
+    /***
+     * 如果客户端请求携带的cookie中没有uuid
+     * 则创建一个uuid
+     * 并在res中设置cookie
+     */
+    if (!_uuid) {
+      _uuid = uuidv1()
+      res.setHeader('Set-Cookie', [`uuid=${_uuid}`])
+    }
+    commit('SET_UUID', _uuid)
   },
   /** 获取导航栏数据 */
   getNavData: ({ commit }) => {
@@ -62,5 +89,23 @@ export const getters = {
    * @param state
    * @returns {getters.uuid|(function(*))|*}
    */
-  uuid: state => state.uuid
+  uuid: state => state.uuid,
+  /**
+   * 获取访问令牌
+   * @param state
+   * @returns {getters.accessToken|(function(*))|string|*}
+   */
+  accessToken: state => state.user.accessToken,
+  /**
+   * 获取刷新访问令牌
+   * @param state
+   * @returns {getters.refreshToken|(function(*))|string|*}
+   */
+  refreshToken: state => state.user.refreshToken,
+  /**
+   * 获取用户信息
+   * @param state
+   * @returns {*}
+   */
+  user: state => state.user.user
 }
