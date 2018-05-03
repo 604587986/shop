f5d15ed79b30a32524574a556de5076723c4955f
yangpei <1208812362@qq.com>
2018-05-03 19:01:21 +0800

    店铺设置 修改预警货品数量 接口调试完成

--- manager-seller/src/api/stockWarningNum.js
+++ manager-seller/src/api/stockWarningNum.js
@@ -5,36 +5,16 @@
 import request from '@/utils/request'
 
 /**
- * 获取库存预警数量
- * @param params
- * @returns {Promise<any>}
- */
-export function getStockWarningNum(params) {
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'http://www.andste.cc/mock/5aa72c080d9d060b4b99b45b/seller/settings/stock-number',
-      method: 'get',
-      loading: false,
-      params
-    }).then(response => {
-      resolve(response)
-    }).catch(error => reject(error))
-  })
-}
-
-/**
- * 保存库存预警数量
+ * 修改库存预警数量
  * @param params
  * @returns {Promise<any>}
  */
 export function saveStockWarningNum(params) {
-  const _formData = new FormData()
-  Object.keys(params).forEach(key => _formData.append(key, params[key]))
   return new Promise((resolve, reject) => {
     request({
-      url: 'http://www.andste.cc/mock/5aa72c080d9d060b4b99b45b/seller/settings/reserve/stock-number',
-      method: 'post',
-      data: _formData
+      url: '/shops/warning-counts',
+      method: 'put',
+      params
     }).then(response => resolve(response)).catch(error => reject(error))
   })
 }
