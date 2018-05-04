7840c2c11786c6e7e1318e6872c6fce2448bc0ba
yangpei <1208812362@qq.com>
2018-05-04 16:23:03 +0800

    物流公司接口对接完成

--- manager-seller/src/api/expressCompany.js
+++ manager-seller/src/api/expressCompany.js
@@ -13,7 +13,7 @@ import ExpressCompanyModel from '@/models/ExpressCompanyModel'
 export function getExpressCompanyList(params) {
   return new Promise((resolve, reject) => {
     request({
-      url: 'http://www.andste.cc/mock/5aa72c080d9d060b4b99b45b/seller/logistics/company/list',
+      url: '/shops/logi-companies',
       method: 'get',
       loading: false,
       params
@@ -26,20 +26,37 @@ export function getExpressCompanyList(params) {
 }
 
 /**
- * 开启/关闭 物流公司
+ * 开启 物流公司
  * @param ids
  * @param params
  * @returns {Promise<any>}
  */
-export function switchExpressPower(ids, params) {
-  const _formData = new FormData()
-  Object.keys(params).forEach(key => _formData.append(key, params[key]))
+export function openExpressPower(ids, params) {
   return new Promise((resolve, reject) => {
     request({
-      url: `http://www.andste.cc/mock/5aa72c080d9d060b4b99b45b/seller/logistics/company/${ids}`,
+      url: `/shops/logi-companies/${ids}`,
       method: 'post',
       loading: false,
-      _formData
+      params
+    }).then(response => {
+      resolve(response)
+    }).catch(error => reject(error))
+  })
+}
+
+/**
+ * 关闭 物流公司
+ * @param ids
+ * @param params
+ * @returns {Promise<any>}
+ */
+export function closeExpressPower(ids, params) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: `/shops/logi-companies/${ids}`,
+      method: 'delete',
+      loading: false,
+      params
     }).then(response => {
       resolve(response)
     }).catch(error => reject(error))
