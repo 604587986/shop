be47e730086e5b7049a5beea044546b863721f45
赖云川 <andste.cc@gmail.com>
2018-05-04 13:29:59 +0800

    请求超时时间调整为8秒

--- buyer/pc/themes/b2b2c_v5/utils/request.js
+++ buyer/pc/themes/b2b2c_v5/utils/request.js
@@ -9,7 +9,7 @@ import GetFullUrl from '@/utils/urls'
 // 创建axios实例
 const service = axios.create({
   // baseURL: process.env.BASE_API, // api的base_url
-  timeout: 5000 // 请求超时时间
+  timeout: 8000 // 请求超时时间
 })
 
 // request拦截器
