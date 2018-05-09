9250ff33db7519caeb57743a23e7bfe61594ec19
赖云川 <andste.cc@gmail.com>
2018-05-09 10:55:56 +0800

    API调整

--- manager-admin/src/api/category.js
+++ manager-admin/src/api/category.js
@@ -6,20 +6,18 @@ import request from '@/utils/request'
 /**
  * 添加分类
  * @param params
- * @returns {Promise<any>}
  */
 export function addCategory(params) {
-  const _formData = new FormData()
-  _formData.append('name', params.category_name)
-  _formData.append('parent_id', params.parent_id)
-  _formData.append('category_order', params.category_order)
-  _formData.append('image', params.category_image)
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'goods/categories',
-      method: 'post',
-      data: _formData
-    }).then(response => resolve(response)).catch(error => reject(error))
+  const _params = {
+    name: params.category_name,
+    parent_id: params.parent_id,
+    category_order: params.category_order,
+    image: params.category_image
+  }
+  return request({
+    url: 'goods/categories',
+    method: 'post',
+    data: _params
   })
 }
 
@@ -27,7 +25,6 @@ export function addCategory(params) {
  * 编辑分类
  * @param id
  * @param params
- * @returns {Promise<any>}
  */
 export function editCategory(id, params) {
   const _params = {
@@ -36,12 +33,10 @@ export function editCategory(id, params) {
     category_order: params.category_order,
     image: params.category_image
   }
-  return new Promise((resolve, reject) => {
-    request({
-      url: `goods/categories/${id}`,
-      method: 'put',
-      data: _params
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: `goods/categories/${id}`,
+    method: 'put',
+    data: _params
   })
 }
 
@@ -51,11 +46,9 @@ export function editCategory(id, params) {
  * @returns {Promise<any>}
  */
 export function getBrandByCategoryId(category_id) {
-  return new Promise((resolve, reject) => {
-    request({
-      url: `goods/categories/${category_id}/brands`,
-      method: 'get'
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: `goods/categories/${category_id}/brands`,
+    method: 'get'
   })
 }
 
