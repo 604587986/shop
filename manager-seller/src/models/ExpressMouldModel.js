acf14887b792cfb7ec128b431bd884c874bb18b7
yangpei <1208812362@qq.com>
2018-05-10 19:09:58 +0800

    model文件字段调整

--- manager-seller/src/models/ExpressMouldModel.js
+++ manager-seller/src/models/ExpressMouldModel.js
@@ -13,11 +13,11 @@ import {
 
 export default class ExpressMouldModel extends DataModel {
   /** 快递模板ID */
-  @ServerName('tpl_id')
+  @ServerName('template_id')
   tpl_id
 
   /** 快递模板名称 */
-  @ServerName('tpl_name')
+  @ServerName('name')
   tpl_name
 
   /** 首重 */
@@ -37,6 +37,18 @@ export default class ExpressMouldModel extends DataModel {
   continued_price
 
   /** 模板类型 */
-  @ServerName('tpl_type')
+  @ServerName('type')
   tpl_type
+
+  /** 地区 */
+  @ServerName('area')
+  tpl_area
+
+  /** 地区ID */
+  @ServerName('area_id')
+  tpl_area_id
+
+  /** 地区json */
+  @ServerName('area_json')
+  tpl_area_json
 }
