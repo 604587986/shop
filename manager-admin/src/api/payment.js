19f428b5e5501939c478654669d89ced8fae4ef1
赖云川 <andste.cc@gmail.com>
2018-05-10 17:10:41 +0800

    支付方式相关API调整

--- manager-admin/src/api/payment.js
+++ manager-admin/src/api/payment.js
@@ -13,14 +13,56 @@ import PaymentModel from '@/models/PaymentModel'
 export function getPaymentList(params) {
   return new Promise((resolve, reject) => {
     request({
-      url: 'order-pay/admin/payment/list-json.do',
+      url: 'payment/payment-methods',
       method: 'get',
       loading: false,
       params
     }).then(response => {
-      const _response = response
-      _response.data = new PaymentModel().map(response.data)
-      resolve(_response)
+      response.data = new PaymentModel().map(response.data)
+      resolve(response)
     }).catch(error => reject(error))
+  })
+}
+
+/**
+ * 修改支付方式
+ * @param payment_plugin_id 支付方式code
+ * @param payment_method 支付方式详细配置
+ * payment_method:
+ * {
+ * "enable_client": [
+ *   {
+ *     "config_list": [
+ *       {
+ *         "name": "string",
+ *         "value": "string"
+ *       }
+ *     ],
+ *     "is_open": 0,
+ *     "key": "string",
+ *     "name": "string"
+ *   }
+ * ],
+ * "image": "string",
+ * "is_retrace": 0
+ * }
+ */
+export function editPayment(payment_plugin_id, payment_method) {
+  return request({
+    url: `payment/payment-methods/${payment_plugin_id}`,
+    method: 'put',
+    headers: { 'Content-Type': 'appliaction/json' },
+    data: { payment_method }
+  })
+}
+
+/**
+ * 获取支付方式详情
+ * @param payment_plugin_id
+ */
+export function getPaymentDetail(payment_plugin_id) {
+  return request({
+    url: `payment/payment-methods/${payment_plugin_id}`,
+    method: 'get'
   })
 }
