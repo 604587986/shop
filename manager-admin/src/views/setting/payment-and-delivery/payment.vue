2e9c9afc2ddc0c3a3e2a3dfc335a02b2bbbc403f
赖云川 <andste.cc@gmail.com>
2018-05-10 16:52:16 +0800

    支付方式页面调整

--- manager-admin/src/views/setting/payment-and-delivery/payment.vue
+++ manager-admin/src/views/setting/payment-and-delivery/payment.vue
@@ -2,8 +2,7 @@
   <div>
     <en-tabel-layout
       :toolbar="false"
-      pagination
-      :tableData="tableData"
+      :tableData="tableData.data"
       :loading="loading"
     >
       <template slot="table-columns">
@@ -27,15 +26,15 @@
         </el-table-column>
       </template>
       <el-pagination
+        v-if="tableData"
         slot="pagination"
-        v-if="pageData"
         @size-change="handlePageSizeChange"
         @current-change="handlePageCurrentChange"
-        :current-page="pageData.page_no"
+        :current-page="params.page_no"
         :page-sizes="[10, 20, 50, 100]"
-        :page-size="pageData.page_size"
+        :page-size="params.page_size"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="pageData.data_total">
+        :total="tableData.data_total">
       </el-pagination>
     </en-tabel-layout>
   </div>
@@ -53,18 +52,13 @@
       return {
         /** 列表loading状态 */
         loading: false,
-
         /** 列表参数 */
         params: {
           page_no: 1,
           page_size: 10
         },
-
         /** 列表数据 */
-        tableData: null,
-
-        /** 列表分页数据 */
-        pageData: null
+        tableData: null
       }
     },
     mounted() {
@@ -91,12 +85,7 @@
         this.loading = true
         API_Payment.getPaymentList(this.params).then(response => {
           this.loading = false
-          this.tableData = response.data
-          this.pageData = {
-            page_no: response.draw,
-            page_size: 10,
-            data_total: response.recordsTotal
-          }
+          this.tableData = response
         }).catch(error => {
           this.loading = false
           console.log(error)
