de96903fda6a4d21fc22f79b97678275d880c6e7
赖云川 <andste.cc@gmail.com>
2018-05-14 19:16:05 +0800

    物流公司相关API调整

--- manager-admin/src/api/expressCompany.js
+++ manager-admin/src/api/expressCompany.js
@@ -4,6 +4,7 @@
 
 import request from '@/utils/request'
 import ExpressCompanyModel from '@/models/ExpressCompanyModel'
+const expressCompanyModel = new ExpressCompanyModel()
 
 /**
  * 获取物流公司列表
@@ -13,14 +14,60 @@ import ExpressCompanyModel from '@/models/ExpressCompanyModel'
 export function getExpressCompanyList(params) {
   return new Promise((resolve, reject) => {
     request({
-      url: 'shop/admin/logi/list-logi-json.do',
+      url: 'shops/logi-companies',
       method: 'get',
       loading: false,
       params
     }).then(response => {
-      const _response = response
-      _response.data = new ExpressCompanyModel().map(response.data)
-      resolve(_response)
+      response.data = expressCompanyModel.map(response.data)
+      resolve(response)
     }).catch(error => reject(error))
+  })
+}
+
+/**
+ * 添加物流公司
+ * @param params
+ */
+export function addExpressCompany(params) {
+  return request({
+    url: 'shops/logi-companies',
+    method: 'post',
+    data: params
+  })
+}
+
+/**
+ * 删除物流公司
+ * @param id
+ */
+export function deleteExpressCompany(id) {
+  return request({
+    url: 'shops/logi-companies',
+    method: 'delete'
+  })
+}
+
+/**
+ * 获取物流公司详情
+ * @param id
+ */
+export function getExpressCompanyDetail(id) {
+  return request({
+    url: `shops/logi-companies/${id}`,
+    method: 'get'
+  })
+}
+
+/**
+ * 修改物流公司
+ * @param id
+ * @param params
+ */
+export function editExpressCompany(id, params) {
+  return request({
+    url: `shops/logi-companies/${id}`,
+    method: 'put',
+    data: params
   })
 }
