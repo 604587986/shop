fe0d38e3ab67ba6b7c1476703d4b3567f22f11b2
赖云川 <andste.cc@gmail.com>
2018-05-15 13:20:48 +0800

    会员恢复

--- manager-admin/src/views/member/member-manage/memberRecycle.vue
+++ manager-admin/src/views/member/member-manage/memberRecycle.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
     <en-tabel-layout
-      :toolbar="false"
-      :pagination="true"
-      :tableData="tableData"
+      :tableData="tableData.data"
       :loading="loading"
-      :selection-change="handleSelectionChange"
     >
       <template slot="table-columns">
-        <el-table-column type="selection" width="100"/>
         <el-table-column prop="username" label="用户名"/>
         <el-table-column prop="mobile" label="手机号"/>
         <el-table-column prop="email" label="电子邮箱"/>
@@ -18,7 +14,7 @@
         <el-table-column label="上次登录时间">
           <template slot-scope="scope">{{ scope.row.last_login_time | unixToDate }}</template>
         </el-table-column>
-        <el-table-column prop="login_count" label="本月登录次数"/>
+        <el-table-column prop="login_count" label="登录次数"/>
         <el-table-column prop="sex" label="性别" :formatter="formatterSex"/>
         <el-table-column label="操作">
           <template slot-scope="scope">
@@ -30,19 +26,16 @@
         </el-table-column>
       </template>
 
-      <template slot="pagination-toolbar">
-        <el-button type="primary" size="mini" @click="recoverMembers">恢复选中</el-button>
-      </template>
       <el-pagination
+        v-if="tableData"
         slot="pagination"
-        v-if="pageData"
         @size-change="handlePageSizeChange"
         @current-change="handlePageCurrentChange"
-        :current-page="pageData.page_no"
+        :current-page="tableData.page_no"
         :page-sizes="[10, 20, 50, 100]"
-        :page-size="pageData.page_size"
+        :page-size="tableData.page_size"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="pageData.data_total">
+        :total="tableData.data_total">
       </el-pagination>
     </en-tabel-layout>
   </div>
@@ -68,13 +61,7 @@
         },
 
         /** 列表数据 */
-        tableData: null,
-
-        /** 列表分页数据 */
-        pageData: null,
-
-        /** 被选数据 */
-        selectedData: []
+        tableData: ''
       }
     },
     mounted() {
@@ -93,27 +80,14 @@
         this.GET_RecycleMemberList()
       },
 
-      /** 当选择项发生变化 */
-      handleSelectionChange(val) {
-        this.selectedData = val.map(item => item.id)
-      },
-
       /** 恢复会员操作 */
       handleRecoverMember(index, row) {
         this.$confirm('确定要恢复这个会员吗？', '提示', { type: 'warning' }).then(() => {
-          this.POST_RecycleMember(row.id)
+          API_Member.recycleMember(row.id).then(() => {
+            this.$message.success('会员恢复成功！')
+            this.GET_RecycleMemberList()
+          })
         }).catch(() => {})
-      },
-
-      /** 批量恢复会员操作 */
-      recoverMembers() {
-        if (this.selectedData.length < 1) {
-          this.$message.error('您未选中任何会员！')
-        } else {
-          this.$confirm('确定要恢复这些会员吗？', '提示', { type: 'warning' }).then(() => {
-            this.POST_RecycleMember(this.selectedData)
-          }).catch(() => {})
-        }
       },
 
       /** 性别格式化 */
@@ -126,24 +100,10 @@
         this.loading = true
         API_Member.getRecycleMemberList(this.params).then(response => {
           this.loading = false
-          this.tableData = response.data
-          this.pageData = {
-            page_no: response.draw,
-            page_size: 10,
-            data_total: response.recordsTotal
-          }
-        }).catch(error => {
+          this.tableData = response
+        }).catch(() => {
           this.loading = false
-          console.log(error)
         })
-      },
-
-      /** 恢复会员 */
-      POST_RecycleMember(ids) {
-        API_Member.recycleMember(ids).then(response => {
-          this.$message.success('会员恢复成功！')
-          this.GET_RecycleMemberList()
-        }).catch(error => console.log(error))
       }
     }
   }
