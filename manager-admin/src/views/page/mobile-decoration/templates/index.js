c427f0128b3c359c58f93845750f1ca366c82fbb
赖云川 <andste.cc@gmail.com>
2018-05-18 19:23:23 +0800

    refactor(floor): 楼层模板入口调整

--- manager-admin/src/views/page/mobile-decoration/templates/index.js
+++ manager-admin/src/views/page/mobile-decoration/templates/index.js
@@ -17,11 +17,9 @@ Object.keys(common).forEach(key => {
   if (common[key].dataTpl) templateArray.push(common[key].dataTpl)
 })
 
-// Object.keys(customs).forEach(key => {
-//   templateArray.push({
-//     key
-//   })
-// })
+Object.keys(customs).forEach(key => {
+  if (customs[key].dataTpl) templateArray.push(customs[key].dataTpl)
+})
 
 export { templateArray }
 export default templates
