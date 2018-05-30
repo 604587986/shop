fd946a7fde18714602a21478d89698d90caf5057
赖云川 <andste.cc@gmail.com>
2018-05-30 17:00:43 +0800

    chore(statistics): 价格销量统计

--- manager-admin/src/api/statistics.js
+++ manager-admin/src/api/statistics.js
@@ -2,7 +2,7 @@
  * 统计相关API
  */
 
-import request from '@/utils/request'
+import request from '@/utils/request';
 
 /** 会员分析 */
 
@@ -84,20 +84,13 @@ export function addMemberStatistics(params) {
 /**
  * 价格销量
  * @param params
- * @returns {Promise<any>}
  */
 export function getPriceSales(params) {
-  const _formData = new FormData()
-  Object.keys(params).forEach(key => _formData.append(key, params[key]))
-  _formData.append('store_id', params.shop_id)
-  _formData.append('cycle_type', params.type)
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'b2b2c/admin/goodsStatis/get-price-sales-json.do',
-      method: 'post',
-      loading: false,
-      data: _formData
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: 'statistics/goods/price/sales',
+    method: 'get',
+    loading: false,
+    params
   })
 }
 
