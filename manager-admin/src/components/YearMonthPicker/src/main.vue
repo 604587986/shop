75b90634dce06baff06347385e320b346449e0e6
赖云川 <andste.cc@gmail.com>
2018-05-30 18:15:44 +0800

    style(components): 年月选择组件优化

--- manager-admin/src/components/YearMonthPicker/src/main.vue
+++ manager-admin/src/components/YearMonthPicker/src/main.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="display: inline-block">
     <el-select v-if="optional" v-model="date_type" placeholder="请选择" style="width: 100px">
-      <el-option label="按月" value="MONTH"/>
-      <el-option label="按年" value="YEAR"/>
+      <el-option label="按月" value="month"/>
+      <el-option label="按年" value="year"/>
     </el-select>
     <el-date-picker
       style="width: 130px"
@@ -11,8 +11,8 @@
       :editable="editable"
       :clearable="clearable"
       :type="date_type"
-      :value-format="date_type === 'MONTH' ? 'yyyy-MM' : 'yyyy'"
-      :placeholder="date_type === 'MONTH' ? '选择月份' : '选择年份'"
+      :value-format="date_type === 'month' ? 'yyyy-MM' : 'yyyy'"
+      :placeholder="date_type === 'month' ? '选择月份' : '选择年份'"
       :picker-options="{disabledDate(time) { return time.getTime() > Date.now() }}">
     </el-date-picker>
   </div>
@@ -37,17 +37,17 @@
     },
     data() {
       return {
-        date_type: 'MONTH',
+        date_type: 'month',
         date_val: this.getCurrentYearMonth().o,
         l_date_val: this.getCurrentYearMonth().l_o
       }
     },
     watch: {
       date_type(newVal) {
-        this.date_val = newVal === 'MONTH'
+        this.date_val = newVal === 'month'
           ? this.getCurrentYearMonth().o
           : this.getCurrentYear().o
-        this.l_date_val = newVal === 'MONTH'
+        this.l_date_val = newVal === 'month'
           ? this.getCurrentYearMonth().l_o
           : this.getCurrentYear().l_o
         this.handleDateChange()
@@ -83,7 +83,7 @@
       },
       /** 日期发生改变 */
       handleDateChange() {
-        const is_year = this.date_type === 'YEAR'
+        const is_year = this.date_type === 'year'
         const val = this.date_val
         const l_val = this.l_date_val
         const _y = val.substr(0, 4)
@@ -91,7 +91,7 @@
         const _m = val.substr(5, 2)
         const l_m = l_val.substr(5, 2)
         const obj = {
-          type: this.date_type,
+          type: this.date_type.toLocaleUpperCase(),
           year: _y,
           last_year: l_y,
           month: _m,
