16888f5730cb02d080e48b42f6af1f50009970fc
赖云川 <andste.cc@gmail.com>
2018-06-01 17:38:39 +0800

    增加ui-domain配置

--- buyer/pc/themes/b2b2c_v5/utils/urls.js
+++ buyer/pc/themes/b2b2c_v5/utils/urls.js
@@ -1,32 +1,8 @@
 /**
  * Created by andste.cc@gmail.com on 2018/5/4.
- * 获取不同服务下的正确的URL。
- * dev_domain 开发模式下的域名
- * pro_domain 生产模式下的域名
- * domain.base 基础服务
- * domain.passport 认证服务
- * domain.main 业务逻辑服务
  */
 
-/**
- * 开发模式域名
- * @type {{base : string, passport : string, main : string}}
- */
-const dev_domain = {
-  base: 'http://yiqisi.s1.natapp.cc/base-api/',
-  passport: 'http://yiqisi.s1.natapp.cc/passport/',
-  main: 'http://yiqisi.s1.natapp.cc/buyer-api/'
-}
-
-/**
- * 生产模式域名
- * @type {{base : string, passport : string, main : string}}
- */
-const pro_domain = {
-  base: 'http://192.168.2.5:7000/',
-  passport: 'http://192.168.2.5:7001/',
-  main: 'http://192.168.2.5:7002/'
-}
+const domain = require('../ui-domain')
 
 /**
  * 返回完整URL
@@ -35,9 +11,9 @@ const pro_domain = {
  */
 export default function getFullUrl(url) {
   const _domain = process.env.NODE_ENV === 'production'
-    ? pro_domain
-    : dev_domain
-  let _url = _domain.main + url
+    ? domain.pro
+    : domain.dev
+  let _url = _domain.buyer + url
   if (/^\/?passport($|\/)/.test(url)) _url = _domain.passport + url
   if (/^\/?captchas($|\/)|^\/?uploaders($|\/)/.test(url)) _url = _domain.base + url
   return _url.replace(/([^:])\/\//g, '$1/')
