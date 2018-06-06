9c6710e03f72a18a300d0f3c7cb01ce2faca0464
赖云川 <andste.cc@gmail.com>
2018-06-06 22:15:36 +0800

    style(api)：申请售后API调整

--- buyer/pc/themes/b2b2c_v5/api/after-sale.js
+++ buyer/pc/themes/b2b2c_v5/api/after-sale.js
@@ -11,7 +11,7 @@ import request from '@/utils/request'
  */
 export function getAfterSale(params) {
   return request({
-    url: 'http://www.andste.cc/mock/5aab2c100d9d060b4b99b47f/buyer/after-sale',
+    url: 'after-sales/refunds',
     method: 'get',
     params
   })
