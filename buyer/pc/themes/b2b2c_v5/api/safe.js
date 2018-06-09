902fa0cb48ed35aad435851e3c270e2f7e9fd36d
赖云川 <andste.cc@gmail.com>
2018-06-09 20:33:24 +0800

    style(member)：更改密码

--- buyer/pc/themes/b2b2c_v5/api/safe.js
+++ buyer/pc/themes/b2b2c_v5/api/safe.js
@@ -5,76 +5,6 @@
 import request from '@/utils/request'
 
 /**
- * 校验短信验证码
- * @param type 验证类型
- * @param mobile 手机号
- * @param mobile_code 手机验证码
- * @param img_code 图片验证码
- * @returns {AxiosPromise}
- */
-export function validMobileSms(type, mobile, mobile_code, img_code) {
-  return request({
-    url: `${process.env.VALIDATE_CODE_API}/api/shop/member/member-mobile-validate.do`,
-    method: 'get',
-    needToken: true,
-    params: {
-      validcode: img_code,
-      mobileCode: mobile_code,
-      mobile,
-      key: type
-    }
-  })
-}
-
-/**
- * 修改密码
- * @param new_password 新密码
- * @param rep_password 确认新密码
- * @param img_code 图片验证码
- * @returns {AxiosPromise}
- */
-export function changePassword(new_password, rep_password, img_code) {
-  const _formData = new FormData()
-  _formData.append('newpassword', new_password)
-  _formData.append('re_passwd', rep_password)
-  _formData.append('authCode', img_code)
-  return request({
-    url: `${process.env.VALIDATE_CODE_API}/api/shop/member/update-password.do`,
-    method: 'post',
-    needToken: true,
-    data: _formData
-  })
-}
-
-/**
- * 发送更换密码的手机验证码
- * @param mobile
- * @param params
- */
-export function sendUpdatePasswordMobileCode(mobile, params) {
-  return request({
-    url: 'members/security/send-val-code',
-    method: 'post',
-    needToken: true,
-    data: params
-  })
-}
-
-/**
- * 修改密码
- * @param params
- */
-export function updatePassword(params) {
-  return request({
-    url: 'members/security/update-password',
-    method: 'post',
-    needToken: true,
-    data: params
-  })
-}
-
-/////////////////////////////////////////////////////////////////////////////////////
-/**
  * 发送绑定手机验证码
  * @param mobile
  * @param captcha
@@ -145,5 +75,35 @@ export function changeMobile(mobile, sms_code) {
     url: `members/security/exchange-bind/${mobile}`,
     method: 'put',
     data: { sms_code }
+  })
+}
+
+/**
+ * 验证更改密码手机短信
+ * @param sms_code
+ */
+export function validChangePasswordSms(sms_code) {
+  return request({
+    url: 'members/security/password',
+    method: 'get',
+    params: { sms_code }
+  })
+}
+
+/**
+ * 更改密码
+ * @param uuid
+ * @param captcha
+ * @param password
+ */
+export function changePassword(uuid, captcha, password) {
+  return request({
+    url: 'members/security/password',
+    method: 'put',
+    data: {
+      uuid,
+      captcha,
+      password
+    }
   })
 }
