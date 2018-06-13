e61515b4518aee9d47a32f72ac8408784fdfa628
赖云川 <andste.cc@gmail.com>
2018-06-13 21:29:17 +0800

    style(dev): 移除商城公告Model

--- manager-admin/src/api/mallAnnouncement.js
+++ manager-admin/src/api/mallAnnouncement.js
@@ -3,24 +3,16 @@
  */
 
 import request from '@/utils/request'
-import MallAnnouncementModel from '@/models/MallAnnouncementModel'
 
 /**
  * 获取商城公告列表
  * @param params
- * @returns {Promise<any>}
  */
 export function getMallAnnounList(params) {
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'cms/admin/data/list-json.do?catid=26',
-      method: 'get',
-      loading: false,
-      params
-    }).then(response => {
-      const _response = response
-      _response.data = new MallAnnouncementModel().map(response.data)
-      resolve(_response)
-    }).catch(error => reject(error))
+  return request({
+    url: 'cms/admin/data/list-json.do?catid=26',
+    method: 'get',
+    loading: false,
+    params
   })
 }
