82081a8c9c88d7e334aa2920e46a457ff01da6a8
赖云川 <andste.cc@gmail.com>
2018-06-13 14:50:07 +0800

    style(checkout)：地址相关改造完成

---
+++ buyer/pc/themes/b2b2c_v5/pages/member/addressMixin.js
@@ -0,0 +1,130 @@
+/**
+ * Created by andste.cc@gmail.com on 2018/6/13.
+ * 地址相关mixin
+ * 个人中心和结算页复用
+ */
+
+import { mapGetters, mapActions } from 'vuex'
+import { RegExp } from '~/ui-utils'
+export default {
+  data() {
+    return {
+      /** 添加、编辑地址 表单 */
+      addressForm: {},
+      /** 添加、编辑地址 表单规则 */
+      addressRules: {
+        name: [
+          this.MixinRequired('请输入收货人姓名！'),
+          { min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }
+        ],
+        mobile: [
+          this.MixinRequired('请输入联系方式！'),
+          { validator: (rule, value, callback) => {
+              if (!RegExp.mobile.test(value)) {
+                callback(new Error('手机格式有误！'))
+              } else {
+                callback()
+              }
+            }, trigger: 'blur' }
+        ],
+        region: [this.MixinRequired('请选择地址地区！')],
+        addr: [
+          this.MixinRequired('请输入详细地址！'),
+          { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' }
+        ]
+      },
+      /** 编辑地址时，暂存操作的地址 */
+      regions: ''
+    }
+  },
+  computed: {
+    ...mapGetters(['address'])
+  },
+  mounted() {
+    this.address.length === 0 && this.getAddress()
+  },
+  methods: {
+    areaFormatter(row) {
+      return row.province + row.city + row.county + row.town
+    },
+    /** 添加地址 */
+    handleAddAddress() {
+      this.addressForm = {
+        def_addr: 0,
+        ship_address_name: ''
+      }
+      this.regions = ''
+      this.openLayer({
+        title: '添加地址',
+        yes: index => {
+          this.submitAddressForm('add', index)
+        }
+      })
+    },
+    /** 编辑地址 */
+    handleEaitAddress(row) {
+      this.addressForm = JSON.parse(JSON.stringify(row))
+      this.regions = [row.province_id, row.city_id, row.county_id || -1, row.town_id || -1]
+      this.openLayer({
+        yes: index => {
+          this.submitAddressForm('edit', index)
+        }
+      })
+    },
+    /** 删除地址 */
+    handleDeleteAddress(row) {
+      this.$confirm('确认要删除这个地址吗？', () => {
+        this.deleteAddress(row.addr_id).then(() => {
+          this.$message.success('删除成功！')
+          this.getAddress()
+        })
+      })
+    },
+    /** 设置默认地址 */
+    handleSetDefaultAddress(item) {
+      this.setDefaultAddress(item.addr_id).then(() => {
+        this.$message.success('设置成功！')
+      })
+    },
+    /** 提交地址表单 */
+    submitAddressForm(type, index) {
+      this.$refs['addressForm'].validate((valid) => {
+        if (valid) {
+          this.$layer.close(index)
+          const { addr_id } = this.addressForm
+          if (!addr_id) {
+            this.addAddress(this.addressForm).then(() => {
+              this.$message.success('保存成功！')
+            })
+          } else {
+            this.editAddress(this.addressForm).then(() => {
+              this.$message.success('保存成功！')
+            })
+          }
+        } else {
+          this.$message.error('表单填写有误，请检查！')
+          return false
+        }
+      })
+    },
+    /** 打开layer弹窗 */
+    openLayer(params) {
+      this.$layer.open({
+        type: 1,
+        title: '编辑地址',
+        area: '500px',
+        scrollbar: false,
+        content: $('#addressForm'),
+        btn: ['确认', '取消'],
+        ...params
+      })
+    },
+    ...mapActions({
+      getAddress: 'address/getAddressAction',
+      deleteAddress: 'address/deleteAddressAction',
+      addAddress: 'address/addAddressAction',
+      editAddress: 'address/editAddressAction',
+      setDefaultAddress: 'address/setDefaultAddressAction'
+    })
+  }
+}
