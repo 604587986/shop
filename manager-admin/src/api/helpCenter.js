fffdc56f9be458acf62c4e372e9a8076962ab4df
赖云川 <andste.cc@gmail.com>
2018-06-13 21:28:45 +0800

    style(dev): 移除帮助中心相关Model

--- manager-admin/src/api/helpCenter.js
+++ manager-admin/src/api/helpCenter.js
@@ -3,24 +3,15 @@
  */
 
 import request from '@/utils/request'
-import HelpCenterCategoryModel from '@/models/HelpCenterCategoryModel'
-import HelpArticleModel from '@/models/HelpArticleModel'
 
 /**
  * 获取帮助中心分类
- * @returns {Promise<any>}
  */
 export function getHelpCenterCategory() {
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'cms/admin/cat/list-json-help.do',
-      method: 'get',
-      loading: false
-    }).then(response => {
-      const _response = response
-      _response.data = new HelpCenterCategoryModel().map(response)
-      resolve(_response)
-    }).catch(error => reject(error))
+  return request({
+    url: 'cms/admin/cat/list-json-help.do',
+    method: 'get',
+    loading: false
   })
 }
 
@@ -28,21 +19,13 @@ export function getHelpCenterCategory() {
  * 获取帮助中心文章列表
  * @param id
  * @param params
- * @returns {Promise<any>}
  */
 export function getHelpArticleList(id, params) {
-  params.catid = id
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'cms/admin/data/list-json.do',
-      method: 'get',
-      loading: false,
-      params
-    }).then(response => {
-      const _response = response
-      _response.data = new HelpArticleModel().map(_response.data)
-      resolve(_response)
-    }).catch(error => reject(error))
+  return request({
+    url: 'cms/admin/data/list-json.do',
+    method: 'get',
+    loading: false,
+    params
   })
 }
 
