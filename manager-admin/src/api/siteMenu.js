8ba99352c44a535116f85842c391af2f7b556f96
赖云川 <andste.cc@gmail.com>
2018-06-13 21:24:23 +0800

    style(page): 移除导航栏Model

--- manager-admin/src/api/siteMenu.js
+++ manager-admin/src/api/siteMenu.js
@@ -3,7 +3,6 @@
  */
 
 import request from '@/utils/request'
-import SiteMenuModel from '@/models/SiteMenuModel'
 
 /**
  * 获取导航栏 - PC
@@ -28,40 +27,23 @@ export function getSiteMenuByMobile(params) {
  * @returns {Promise<any>}
  */
 function getSiteMenu(params, type) {
-  params.clientType = type
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'core/admin/site-menu.do',
-      method: 'get',
-      loading: false,
-      params
-    }).then(response => {
-      const _response = response
-      _response.data = new SiteMenuModel().map(response.data)
-      resolve(_response)
-    }).catch(error => reject(error))
+  return request({
+    url: 'core/admin/site-menu.do',
+    method: 'get',
+    loading: false,
+    params
   })
 }
 
 /**
  * 添加导航菜单 - PC
  * @param params
- * @returns {Promise<any>}
  */
 export function addPCSiteMenu(params) {
-  const _params = {
-    name: params.name,
-    url: params.url,
-    client_type: 'PC'
-  }
-  const _formData = new FormData()
-  Object.keys(_params).forEach(key => _formData.append(key, _params[key]))
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'core/admin/site-menu.do',
-      method: 'post',
-      data: _formData
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: 'core/admin/site-menu.do',
+    method: 'post',
+    data: params
   })
 }
 
@@ -69,61 +51,35 @@ export function addPCSiteMenu(params) {
  * 编辑导航菜单 - PC
  * @param id
  * @param params
- * @returns {Promise<any>}
  */
 export function editPCSiteMenu(id, params) {
-  const _params = {
-    name: params.name,
-    url: params.name,
-    client_type: 'PC',
-    sort: params.order,
-    menu_id: id
-  }
-  const _formData = new FormData()
-  Object.keys(_params).forEach(key => _formData.append(key, _params[key]))
-  return new Promise((resolve, reject) => {
-    request({
-      url: `core/admin/site-menu/${id}.do`,
-      method: 'post',
-      data: _formData
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: `core/admin/site-menu/${id}.do`,
+    method: 'post',
+    data: params
   })
 }
 
 /**
  * 删除导航菜单 - PC
  * @param id
- * @returns {Promise<any>}
  */
 export function deletePCSiteMenu(id) {
-  return new Promise((resolve, reject) => {
-    request({
-      url: `core/admin/site-menu/${id}.do`,
-      method: 'delete'
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: `core/admin/site-menu/${id}.do`,
+    method: 'delete'
   })
 }
 
 /**
  * 添加导航菜单 - Mobile
  * @param params
- * @returns {Promise<any>}
  */
 export function addMobileSiteMenu(params) {
-  const _params = {
-    name: params.name,
-    url: params.url,
-    client_type: 'MOBILE',
-    image_path: params.image
-  }
-  const _formData = new FormData()
-  Object.keys(_params).forEach(key => _formData.append(key, _params[key]))
-  return new Promise((resolve, reject) => {
-    request({
-      url: 'core/admin/site-menu.do',
-      method: 'post',
-      data: _formData
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: 'core/admin/site-menu.do',
+    method: 'post',
+    data: params
   })
 }
 
@@ -131,38 +87,22 @@ export function addMobileSiteMenu(params) {
  * 编辑导航菜单 - Mobile
  * @param id
  * @param params
- * @returns {Promise<any>}
  */
 export function editMobileSiteMenu(id, params) {
-  const _params = {
-    name: params.name,
-    url: params.url,
-    client_type: 'MOBILE',
-    sort: params.order,
-    menu_id: id,
-    image_path: params.image
-  }
-  const _formData = new FormData()
-  Object.keys(_params).forEach(key => _formData.append(key, _params[key]))
-  return new Promise((resolve, reject) => {
-    request({
-      url: `core/admin/site-menu/${id}.do`,
-      method: 'post',
-      data: _formData
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: `core/admin/site-menu/${id}.do`,
+    method: 'post',
+    data: params
   })
 }
 
 /**
  * 删除导航菜单 - Mobile
  * @param id
- * @returns {Promise<any>}
  */
 export function deleteMobileSiteMenu(id) {
-  return new Promise((resolve, reject) => {
-    request({
-      url: `core/admin/site-menu/${id}.do`,
-      method: 'delete'
-    }).then(response => resolve(response)).catch(error => reject(error))
+  return request({
+    url: `core/admin/site-menu/${id}.do`,
+    method: 'delete'
   })
 }
