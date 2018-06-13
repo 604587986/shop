3de0817225f5c664c89b3e566220016125c50595
赖云川 <andste.cc@gmail.com>
2018-06-13 16:31:43 +0800

    style(checkout)：购物清单适配

---
+++ buyer/pc/themes/b2b2c_v5/pages/checkout/-checkout-inventory.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="ckt-item inventory">
+    <div class="top-ckt">
+      <span class="title-top">配送清单</span>
+      <span class="other-top"><nuxt-link to="/cart" class="c-back-cart">返回购物车修改</nuxt-link></span>
+      <div class="clearfix"></div>
+    </div>
+    <div class="content-ckt-inventory inventory">
+      <div
+        v-for="shop in inventoryList"
+        :key="shop.seller_id"
+        class="item-ckt-inventory"
+      >
+        <div class="left-item-inventory" style="height: 221px;">
+          <div class="express-inventory">
+            <div class="title-item-inventory">配送方式</div>
+            <div class="content-item-inventory express">
+              <div class="ckt-checkbox express selected" title="运费&nbsp;（0&nbsp;元）">
+                <span>运费&nbsp;（0&nbsp;元）</span>
+              </div>
+            </div>
+            <div style="clear: both;"></div>
+          </div>
+          <div class="hr-inventory"></div>
+          <div class="discount-inventory">
+            <div class="title-item-inventory">优惠折扣</div>
+            <div class="content-item-inventory">
+              <p class="no-item-discount-inventory">您在该店铺还没有领到优惠劵，去&nbsp;
+                <nuxt-link :to="'/shop?shop_id=' + shop.seller_id" target="_blank" style="color: #005ea7;">店铺</nuxt-link>&nbsp;看看吧！
+              </p>
+              <div style="clear: both;"></div>
+            </div>
+          </div>
+        </div>
+        <div class="right-item-inventory">
+          <div class="gooods-inventory">
+            <div class="title-item-inventory">
+              <div class="store-name">店铺名称：
+                <nuxt-link :to="'/shop?shop_id=' + shop.seller_id" target="_blank">{{ shop.seller_name }}</nuxt-link>
+              </div>
+              <div class="store-total">
+                <div class="discount-store-total">优惠折扣： <span>-￥{{ shop.price.discount_price | unitPrice }}</span>
+                </div>
+                <div class="total-store-total">店铺合计（含运费）： <span>￥{{ shop.price.total_price | unitPrice }}</span>
+                </div>
+              </div>
+              <input type="hidden" name="storeid" value="18">
+            </div>
+            <div class="content-item-inventory goods">
+              <table class="table-gooods-inventory">
+                <tbody>
+                <tr v-for="goods in shop.sku_list" :key="goods.sku_id">
+                  <td class="img-gooods-inventory">
+                    <nuxt-link :to="'/goods-' + goods.goods_id + '.html'" target="_blank">
+                      <img :src="goods.goods_image">
+                    </nuxt-link>
+                  </td>
+                  <td class="name-gooods-inventory">
+                    <nuxt-link :to="'/goods-' + goods.goods_id + '.html'" target="_blank">
+                      {{ goods.name }}
+                    </nuxt-link>
+                    <span class="sku-spec">{{ goods | formatterSkuSpec }}</span>
+                  </td>
+                  <td class="price-gooods-inventory">￥{{ goods.purchase_price | unitPrice }}</td>
+                  <td class="num-gooods-inventory">x{{ goods.num }}</td>
+                  <td class="subtotal-gooods-inventory">￥{{ goods.subtotal | unitPrice }}</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div style="clear: both;"></div>
+        <div class="bottom-item-inventory">
+          <!--// Andste_TODO 2018/6/13: 促销相关未适配-->
+          <!-- 注释 [store.activity_id??] -->
+          <!-- 注释 [store.storeprice.gift_id != 0] -->
+          <!-- 注释 [store.storeprice.bonus_id != 0]-->
+        </div>
+        <div style="clear: both;"></div>
+      </div>
+    </div>
+    <div class="bottom-ckt-inventory">
+      订单备注：
+      <el-input v-model="iRemark" size="small" :maxlength="120">
+        <el-button slot="append" icon="el-icon-check" @click="handleSetRemark"></el-button>
+      </el-input>
+      <span class="remark-tip">*请勿填写有关支付、收货、发票方面的信息，如有特殊需要请联系客服人员。</span>
+    </div>
+    <div class="placeholder-20"></div>
+  </div>
+</template>
+
+<script>
+  /**
+   * 结算页
+   * 购物车清单组件
+   */
+  import * as API_Trade from '@/api/trade'
+  export default {
+    name: 'checkout-inventory',
+    props: ['remark'],
+    data() {
+      return {
+        inventoryList: [],
+        iRemark: this.remark
+      }
+    },
+    watch: {
+      remark(newVal) { this.iRemark = newVal }
+    },
+    mounted() {
+      API_Trade.getCarts('checked').then(response => this.inventoryList = response)
+    },
+    methods: {
+      handleSetRemark() {
+        API_Trade.setRemark(this.iRemark).then(() => {
+          this.$message.success('设置成功！')
+        })
+      }
+    }
+  }
+</script>
--- buyer/pc/themes/b2b2c_v5/pages/checkout/index.vue
+++ buyer/pc/themes/b2b2c_v5/pages/checkout/index.vue
@@ -24,176 +24,7 @@
         <!--支付方式 end-->
 
         <!--配送清单 start-->
-        <div class="ckt-item inventory" id="inventory">
-          <div class="top-ckt">
-            <span class="title-top">配送清单</span>
-            <span class="other-top"><a href="/javashop/cart.html" class="c-back-cart">返回购物车修改</a></span>
-            <div class="clearfix"></div>
-          </div>
-          <div class="content-ckt-inventory inventory">
-            <div class="item-ckt-inventory" id="store_18" storeid="18">
-              <div class="left-item-inventory" style="height: 221px;">
-                <div class="express-inventory">
-                  <div class="title-item-inventory">配送方式</div>
-                  <div class="content-item-inventory express">
-                    <div class="ckt-checkbox express selected" type_id="1" title="运费&nbsp;（0&nbsp;元）">
-                      <span>运费&nbsp;（0&nbsp;元）</span>
-                    </div>
-                  </div>
-                  <div style="clear: both;"></div>
-                </div>
-                <div class="hr-inventory"></div>
-                <div class="discount-inventory">
-                  <div class="title-item-inventory">优惠折扣</div>
-                  <div class="content-item-inventory">
-                    <p class="no-item-discount-inventory">您在该店铺还没有领到优惠劵，去&nbsp;
-                      <a target="_blank" href="/javashop/store_themes/18/index.html?store_id=0&amp;menu=store" style="color: #005ea7;">店铺</a>&nbsp;看看吧！
-                    </p>
-                    <div style="clear: both;"></div>
-                  </div>
-                </div>
-              </div>
-              <div class="right-item-inventory">
-                <div class="gooods-inventory">
-                  <div class="title-item-inventory">
-                    <div class="store-name">店铺名称：
-                      <a target="_blank" href="/javashop/store_themes/18/index.html?store_id=18&amp;menu=store" style="color: #005ea7;">数码家电</a>
-                    </div>
-                    <div class="store-total">
-                      <div class="discount-store-total">优惠折扣： <span>￥0.00</span>
-                      </div>
-                      <div class="total-store-total">店铺合计（含运费）： <span>￥11,687.00</span>
-                      </div>
-                    </div>
-                    <input type="hidden" name="storeid" value="18">
-                  </div>
-                  <div class="content-item-inventory goods">
-                    <table class="table-gooods-inventory">
-                      <tbody><tr>
-                        <td class="img-gooods-inventory">
-                          <a target="_blank" href="/javashop/goods-279.html">
-                            <img src="http://javashop-statics.oss-cn-beijing.aliyuncs.com/demo/2FA07EED46144151B7BBEF8A76839293.jpg_300x300">                                            </a>
-                          <!-- 此处应该是促销list [item.activity_id?exists && item.activity_id != 0] -->
-                        </td>
-                        <td class="name-gooods-inventory">
-                          <a target="_blank" href="/javashop/goods-279.html"> 戴尔DELL INS15-7567湛黑版15.6英寸笔记本电脑i7-7700 8G 1T+128G固态GTX1050t
-                          </a>
-                        </td>
-                        <td class="price-gooods-inventory">
-                          ￥7,288.00
-                        </td>
-                        <td class="num-gooods-inventory">x1</td>
-                        <td class="subtotal-gooods-inventory">
-                          ￥7,288.00
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="img-gooods-inventory">
-                          <a target="_blank" href="/javashop/goods-210.html">
-                            <img src="http://javashop-statics.oss-cn-beijing.aliyuncs.com/demo/7C7CB2FC5FE6480EA4EC425108A2FDC9.jpg_300x300">                                            </a>
-                          <!-- 此处应该是促销list [item.activity_id?exists && item.activity_id != 0] -->
-                        </td>
-                        <td class="name-gooods-inventory">
-                          <a target="_blank" href="/javashop/goods-210.html">长虹(CHANGHONG)60G3 60英寸25核轻薄4K
-                          </a>
-                        </td>
-                        <td class="price-gooods-inventory">
-                          ￥4,399.00
-                        </td>
-                        <td class="num-gooods-inventory">x1</td>
-                        <td class="subtotal-gooods-inventory">
-                          ￥4,399.00
-                        </td>
-                      </tr>
-                      </tbody></table>
-                  </div>
-                </div>
-              </div>
-              <div style="clear: both;"></div>
-              <div class="bottom-item-inventory">
-                <!-- 注释 [store.activity_id??] -->
-                <!-- 注释 [store.storeprice.gift_id != 0] -->
-                <!-- 注释 [store.storeprice.bonus_id != 0]-->
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-            <div class="item-ckt-inventory" id="store_1" storeid="1">
-              <div class="left-item-inventory">
-                <div class="express-inventory">
-                  <div class="title-item-inventory">配送方式</div>
-                  <div class="content-item-inventory express">
-                    <div class="ckt-checkbox express selected" type_id="1" title="运费&nbsp;（0&nbsp;元）">
-                      <span>运费&nbsp;（0&nbsp;元）</span>
-                    </div>
-                  </div>
-                  <div style="clear: both;"></div>
-                </div>
-                <div class="hr-inventory"></div>
-                <div class="discount-inventory">
-                  <div class="title-item-inventory">优惠折扣</div>
-                  <div class="content-item-inventory">
-                    <p class="no-item-discount-inventory">您在该店铺还没有领到优惠劵，去&nbsp;
-                      <a target="_blank" href="/javashop/store_themes/1/index.html?store_id=0&amp;menu=store" style="color: #005ea7;">店铺</a>&nbsp;看看吧！
-                    </p>
-                    <div style="clear: both;"></div>
-                  </div>
-                </div>
-              </div>
-              <div class="right-item-inventory" style="height: 156px;">
-                <div class="gooods-inventory">
-                  <div class="title-item-inventory">
-                    <div class="store-name">店铺名称：
-                      <a target="_blank" href="/javashop/store_themes/1/index.html?store_id=1&amp;menu=store" style="color: #005ea7;">平台自营</a>
-                    </div>
-                    <div class="store-total">
-                      <div class="discount-store-total">优惠折扣： <span>￥0.00</span>
-                      </div>
-                      <div class="total-store-total">店铺合计（含运费）： <span>￥798.00</span>
-                      </div>
-                    </div>
-                    <input type="hidden" name="storeid" value="1">
-                  </div>
-                  <div class="content-item-inventory goods">
-                    <table class="table-gooods-inventory">
-                      <tbody><tr>
-                        <td class="img-gooods-inventory">
-                          <a target="_blank" href="/javashop/goods-203.html">
-                            <img src="http://javashop-statics.oss-cn-beijing.aliyuncs.com/demo/D298849609114B168FD10106C45AEFF1.jpg_300x300">                                            </a>
-                          <!-- 此处应该是促销list [item.activity_id?exists && item.activity_id != 0] -->
-                        </td>
-                        <td class="name-gooods-inventory">
-                          <a target="_blank" href="/javashop/goods-203.html">小米（MI）米家IH电饭煲 智能电饭煲 IH电磁环绕加热 手
-                          </a>
-                        </td>
-                        <td class="price-gooods-inventory">
-                          ￥399.00
-                        </td>
-                        <td class="num-gooods-inventory">x2</td>
-                        <td class="subtotal-gooods-inventory">
-                          ￥798.00
-                        </td>
-                      </tr>
-                      </tbody></table>
-                  </div>
-                </div>
-              </div>
-              <div style="clear: both;"></div>
-              <div class="bottom-item-inventory">
-                <!-- 注释 [store.activity_id??] -->
-                <!-- 注释 [store.storeprice.gift_id != 0] -->
-                <!-- 注释 [store.storeprice.bonus_id != 0]-->
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-          </div>
-          <div class="bottom-ckt-inventory">
-            订单备注：
-            <input name="remark" id="remark" class="form-control" maxlength="120" value="">
-            <button type="button" class="btn btn-primary remark-confirm-btn" id="remark-confirm">确定</button>
-            <i>*请勿填写有关支付、收货、发票方面的信息，如有特殊需要请联系客服人员。</i>
-          </div>
-          <div class="placeholder-20"></div>
-        </div>
+        <checkout-inventory :remark="params.remark"/>
         <!--配送清单 end-->
 
         <!--送货时间 start-->
@@ -320,15 +151,21 @@
 </template>
 
 <script>
+  import Vue from 'vue'
+  import { Input,Button } from 'element-ui'
+  Vue.use(Input)
+  Vue.use(Button)
   import { mapGetters, mapActions } from 'vuex'
   import CheckoutAddress from './-checkout-address'
   import CheckoutPayment from './-checkout-payment'
+  import CheckoutInventory from './-checkout-inventory'
   import * as API_Trade from '@/api/trade'
   export default {
     name: 'checkout-index',
     components: {
       CheckoutAddress,
-      CheckoutPayment
+      CheckoutPayment,
+      CheckoutInventory
     },
     data() {
       return {
