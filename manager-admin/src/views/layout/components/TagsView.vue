0a73c1dc61165de9f1c583c2ccf503351d2a969e
赖云川 <andste.cc@gmail.com>
2018-06-14 11:40:45 +0800

    fix(tagView): Support route query in TagView

--- manager-admin/src/views/layout/components/TagsView.vue
+++ manager-admin/src/views/layout/components/TagsView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags-view-container">
     <scroll-pane class='tags-view-wrapper' ref='scrollPane'>
-      <router-link ref='tag' class="tags-view-item" :class="isActive(tag)?'active':''" v-for="tag in Array.from(visitedViews)" :to="tag.path" :key="tag.path" @contextmenu.prevent.native="openMenu(tag,$event)">
+      <router-link ref='tag' class="tags-view-item" :class="isActive(tag)?'active':''" v-for="tag in Array.from(visitedViews)" :to="tag" :key="tag.path" @contextmenu.prevent.native="openMenu(tag,$event)">
         {{generateTitle(tag.title)}}
         <span v-if="tag.name !== 'dashboard'" class='el-icon-close' @click.prevent.stop='closeSelectedTag(tag)'></span>
       </router-link>
@@ -15,10 +15,10 @@
 </template>
 
 <script>
-import ScrollPane from '@/components/ScrollPane'
-import { generateTitle } from '@/utils/i18n'
+  import ScrollPane from '@/components/ScrollPane'
+  import { generateTitle } from '@/utils/i18n'
 
-export default {
+  export default {
   components: { ScrollPane },
   data() {
     return {
