84b2c522ad9f26167e292dcb1f9c1ba2208f8363
赖云川 <andste.cc@gmail.com>
2018-06-21 21:27:41 +0800

    style(component): 增加一个楼层标题编辑组件

---
+++ manager-admin/src/components/FloorTitlePicker/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by andste.cc@gmail.com on 2018/6/21.
+ * 楼层标题编辑组件
+ * 可以编辑标题文字和标题过度颜色
+ * 此组件继承于TextPicker组件
+ */
+
+import Vue from 'vue'
+import FloorTitlePicker from './src/main'
+
+FloorTitlePicker.install = () => {
+  Vue.component(FloorTitlePicker.name, FloorTitlePicker)
+}
+
+export default FloorTitlePicker
