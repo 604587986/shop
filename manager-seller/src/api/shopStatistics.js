d54f94a4b5270190c2302cc6a363390938901470
yangpei <1208812362@qq.com>
2018-06-26 17:31:32 +0800

    商家中心 统计管理 店铺概况 & 流量统计对接完毕

--- manager-seller/src/api/shopStatistics.js
+++ manager-seller/src/api/shopStatistics.js
@@ -5,18 +5,34 @@
 import request from '@/utils/request'
 
 /**
- * 获取店铺概况
- * @param params
+ * 获取店铺30天展示数据
  * @returns {Promise<any>}
  */
 export function getShopSurvey() {
   return new Promise((resolve, reject) => {
     request({
-      url: 'http://www.andste.cc/mock/5aa72c080d9d060b4b99b45b/seller/shop/Statistics',
+      url: '/statistics/shop_profile/data',
       method: 'get',
       loading: false
     }).then(response => {
       resolve(response)
-    }).catch(error => reject(error))
+    })
+  })
+}
+
+/**
+ * 获取店铺概况
+ * @param params
+ * @returns {Promise<any>}
+ */
+export function getShopSurveyCharts() {
+  return new Promise((resolve, reject) => {
+    request({
+      url: '/statistics/shop_profile/chart',
+      method: 'get',
+      loading: false
+    }).then(response => {
+      resolve(response)
+    })
   })
 }
