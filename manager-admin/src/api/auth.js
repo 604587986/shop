cfd126a79e091809069d3e62c4e6ba97c38631ec
赖云川 <andste.cc@gmail.com>
2018-06-27 13:45:07 +0800

    style(api): 管理员相关API调整

--- manager-admin/src/api/auth.js
+++ manager-admin/src/api/auth.js
@@ -8,12 +8,59 @@ import request from '@/utils/request'
  * 获取管理员列表
  * @param params
  */
-export function getAdmainistratorList(params) {
+export function getAdministratorList(params) {
   return request({
     url: 'systems/adminuser',
     method: 'get',
     loaidng: false,
     params
+  })
+}
+
+/**
+ * 添加管理员
+ * @param params
+ */
+export function addAdministrator(params) {
+  return request({
+    url: 'systems/adminuser',
+    method: 'post',
+    data: params
+  })
+}
+
+/**
+ * 获取管理员详情
+ * @param id
+ */
+export function getAdministratorDetail(id) {
+  return request({
+    url: `systems/adminuser/${id}`,
+    method: 'get'
+  })
+}
+
+/**
+ * 修改管理员
+ * @param id
+ * @param parmas
+ */
+export function editAdministrator(id, parmas) {
+  return request({
+    url: `systems/adminuser/${id}`,
+    method: 'put',
+    data: parmas
+  })
+}
+
+/**
+ * 删除管理员
+ * @param id
+ */
+export function deleteAdministrator(id) {
+  return request({
+    url: `systems/adminuser/${id}`,
+    method: 'delete'
   })
 }
 
