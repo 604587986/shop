071d4afdb2eeb9b6abca62089049ef5a68b6cb43
赖云川 <andste.cc@gmail.com>
2018-07-03 10:49:14 +0800

    style(help)：帮助中心搭建

--- buyer/pc/themes/b2b2c_v5/pages/help.vue
+++ buyer/pc/themes/b2b2c_v5/pages/help.vue
@@ -1,13 +1,44 @@
 <template>
-  <div id="help">
+  <div style="background-color: #f9f9f9">
     <en-header-other title="帮助中心"/>
-    <div class="help-container w">
+    <div class="help-container">
       <div class="help-menus">
         <h3 class="h3-title">常见问题分类</h3>
-        <dl></dl>
+        <dl class="subside-mod on">
+          <dt class="title">
+            购物指南 <b class="icon-triangle"></b>
+          </dt>
+          <dd class="subside-cnt">
+            <ul class="subside-list">
+              <li class="list-item">
+                <a href="#">交易条款</a>
+              </li>
+              <li class="list-item">
+                <a href="#">购物流程</a>
+              </li>
+              <li class="list-item">
+                <a href="#">促销咨询</a>
+              </li>
+              <li class="list-item">
+                <a href="#">商品咨询</a>
+              </li>
+              <li class="list-item">
+                <a href="#">生活旅行</a>
+              </li>
+            </ul>
+          </dd>
+        </dl>
       </div>
-      <nuxt-child class="help-content"/>
+      <div class="help-content">
+        <div class="breadcrumb">
+          <span>订单百事通</span>
+          &gt;
+          <a href="#"><span id="sLevel2">第三方交易纠纷</span></a>
+        </div>
+        <nuxt-child class="content"/>
+      </div>
     </div>
+    <span class="clr"></span>
   </div>
 </template>
 
@@ -20,12 +51,13 @@
 
 <style type="text/scss" lang="scss" scoped>
   .help-container {
-    display: flex;
-    margin-top: 20px;
-    margin-bottom: 20px;
+    width: 1000px;
+    margin: 20px auto;
+    overflow: hidden;
   }
   .help-menus {
-    width: 208px;
+    width: 210px;
+    float: left;
     .h3-title {
       color: #fff;
       text-align: center;
@@ -35,12 +67,73 @@
       background-color: #f42424;
       font-weight: normal;
     }
+    .subside-mod {
+      .title {
+        position: relative;
+        height: 39px;
+        line-height: 39px;
+        padding-left: 45px;
+        background-color: #eaeaea;
+        border-bottom: 1px solid #fff;
+        font-size: 14px;
+        cursor: pointer;
+        &:hover {
+          background-color: #dadada;
+        }
+      }
+      &.on .icon-triangle {
+        border-width: 0 5px 5px;
+      }
+      .icon-triangle {
+        border-style: solid dashed solid dashed;
+        border-color: #999 transparent #999 transparent;
+        border-width: 5px 5px 0;
+        position: absolute;
+        top: 17px;
+        right: 20px;
+        font-size: 0;
+        line-height: 0;
+        height: 0;
+        overflow: hidden;
+      }
+    }
+    .subside-mod.on .subside-cnt {
+      height: auto;
+    }
+    .subside-cnt {
+      height: 0;
+      transition: all ease .3s;
+      background-color: #fff;
+      .list-item {
+        height: 30px;
+        line-height: 30px;
+        border-bottom: 1px solid #faf8f9;
+        a {
+          display: block;
+          padding-left: 45px;
+          color: #333;
+          &:hover {
+            color: #f42424;
+          }
+        }
+      }
+    }
   }
   .help-content {
-    width: 980px;
-    min-height: 300px;
+    float: left;
+    width: 780px;
+    min-height: 800px;
     margin-left: 10px;
-    padding: 20px;
-    border: 1px solid #e6e6e6;
+    background-color: #fff;
+    .breadcrumb {
+      height: 39px;
+      line-height: 39px;
+      font-size: 14px;
+      background: #eaeaea;
+      padding-left: 13px;
+    }
+    .content {
+      padding: 20px;
+    }
   }
 </style>
