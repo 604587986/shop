5701c3db003c9167c2d8e19983ca16d14ebbeacd
yangpei <1208812362@qq.com>
2018-07-03 19:46:11 +0800

    商家中心 修正域名配置

--- manager-seller/config/dev.env.js
+++ manager-seller/config/dev.env.js
@@ -4,5 +4,6 @@ module.exports = {
 	ENV_CONFIG: '"dev"',
 	SELLER_API: `"${domain.api_dev.seller}"`, // api接口地址
   BASE_API: `"${domain.api_dev.base}"`, // baseapi接口地址
-  BASE_IMG_URL: `"${domain.api_dev.base}/uploaders"` // 图片服务器地址
+  BASE_IMG_URL: `"${domain.api_dev.base}/uploaders"`, // 图片服务器地址
+  HTTP_URL: `"${domain.domain.buyer_pc}"`
 }
--- manager-seller/config/prod.env.js
+++ manager-seller/config/prod.env.js
@@ -4,5 +4,6 @@ module.exports = {
 	ENV_CONFIG: '"prod"',
   SELLER_API: `"${domain.api_pro.seller}"`, // api接口地址
   BASE_API: `"${domain.api_pro.base}"`, // baseapi接口地址
-  BASE_IMG_URL: `"${domain.api_pro.base}/uploaders"` // 图片服务器地址
+  BASE_IMG_URL: `"${domain.api_pro.base}/uploaders"`, // 图片服务器地址
+  HTTP_URL: `"${domain.domain.buyer_pc}"`
 }
