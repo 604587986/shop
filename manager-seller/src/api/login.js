0edff138dbe47f7fe34a6c07be028f4074e5a68e
赖云川 <andste.cc@gmail.com>
2018-07-04 20:28:33 +0800

    style(api): loginAPI调整

--- manager-seller/src/api/login.js
+++ manager-seller/src/api/login.js
@@ -1,27 +1,5 @@
 import request from '@/utils/request'
-
-/**
- * 登录
- * @param username
- * @param password
- * @param validcode
- * @returns {*}
- */
-export function loginByUsername(username, password, validcode) {
-  const _params = {
-    username,
-    password,
-    valid_code: validcode
-  }
-  const _formData = new FormData()
-  Object.keys(_params).forEach(key => _formData.append(key, _params[key]))
-  return request({
-    url: 'http://www.andste.cc/mock/5aa72c080d9d060b4b99b45b/seller/login',
-    method: 'post',
-    loading: false,
-    data: _formData
-  })
-}
+import { api } from '~/ui-domain'
 
 /**
  * 退出账户
@@ -29,21 +7,18 @@ export function loginByUsername(username, password, validcode) {
  */
 export function logout() {
   return request({
-    url: 'core/admin/admin-user/logout.do',
+    url: `${api.buyer}/members/logout`,
     method: 'post'
   })
 }
 
 /**
  * 获取用户信息
- * @param token
- * @returns {*}
  */
-export function getUserInfo(token) {
+export function getUserInfo() {
   return request({
-    url: 'http://www.andste.cc/mock/5aa72c080d9d060b4b99b45b/seller/user',
-    method: 'get',
-    params: { token }
+    url: `${api.buyer}/members`,
+    method: 'get'
   })
 }
 
