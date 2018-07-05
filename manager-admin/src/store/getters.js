2169f8ceadce0255be6e5ca6df5a53e6fea49d99
赖云川 <andste.cc@gmail.com>
2018-07-05 17:11:57 +0800

    style(layout)：Navbar优化

--- manager-admin/src/store/getters.js
+++ manager-admin/src/store/getters.js
@@ -4,9 +4,7 @@ const getters = {
   device: state => state.app.device,
   visitedViews: state => state.tagsView.visitedViews,
   cachedViews: state => state.tagsView.cachedViews,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name,
+  user: state => state.user.user,
   permission_routers: state => state.permission.routers,
   addRouters: state => state.permission.addRouters
 }
--- manager-admin/src/views/layout/components/Navbar.vue
+++ manager-admin/src/views/layout/components/Navbar.vue
@@ -17,7 +17,7 @@
 
       <el-dropdown class="avatar-container right-menu-item" trigger="click">
         <div class="avatar-wrapper">
-          <img class="user-avatar" :src="avatar">
+          <img class="user-avatar" :src="user.face">
           <i class="el-icon-caret-bottom"></i>
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -61,11 +61,7 @@
       ThemePicker
     },
     computed: {
-      ...mapGetters([
-        'sidebar',
-        'name',
-        'avatar'
-      ])
+      ...mapGetters(['sidebar', 'user'])
     },
     methods: {
       toggleSideBar() {
