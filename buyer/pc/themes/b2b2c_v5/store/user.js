567c1d40fc3ab53786ea6148d9969cd643d00776
赖云川 <andste.cc@gmail.com>
2018-07-05 09:59:55 +0800

    fix(store)：cookie domain修正

--- buyer/pc/themes/b2b2c_v5/store/user.js
+++ buyer/pc/themes/b2b2c_v5/store/user.js
@@ -5,7 +5,7 @@ import Storage from '@/utils/storage'
 import { Base64 } from 'js-base64'
 import { Foundation } from '~/ui-utils'
 import * as API_Address from "@/api/address";
-import { domain_dev, domain_pro } from '~/ui-domain'
+import { domain } from '~/ui-domain'
 
 export const state = () => ({
   user: '',
@@ -23,7 +23,7 @@ export const mutations = {
    */
   [types.SET_USER_INFO](state, data) {
     state.user = data
-    process.client && Storage.setItem('user', JSON.stringify(data))
+    process.client && Storage.setItem('user', JSON.stringify(data), { domain: domain.cookie })
   },
   /**
    * 移除用户信息
@@ -42,10 +42,9 @@ export const mutations = {
   [types.SET_ACCESS_TOKEN](state, token) {
     state.accessToken = token
     if (process.client) {
-      // Andste_TODO 2018/7/4: 需要设置path
       const access_token_time = Base64.decode(token).match(/"exp":(\d+)/)[1] * 1000
       const expires = new Date(access_token_time)
-      Storage.setItem('accessToken', token, { expires })
+      Storage.setItem('accessToken', token, { expires, domain: domain.cookie })
     }
   },
   /**
@@ -64,10 +63,9 @@ export const mutations = {
   [types.SET_REFRESH_TOKEN](state, token) {
     state.refreshToken = token
     if (process.client) {
-      // Andste_TODO 2018/7/4: 需要设置path
       const refresh_token_time = Base64.decode(token).match(/"exp":(\d+)/)[1] * 1000
       const expires = new Date(refresh_token_time)
-      Storage.setItem('refreshToken', token, { expires })
+      Storage.setItem('refreshToken', token, { expires, domain: domain.cookie })
     }
   },
   /**
