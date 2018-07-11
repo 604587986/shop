da73b0599de0179c7404361bb45c0ff5aad5e9eb
赖云川 <andste.cc@gmail.com>
2018-07-11 13:59:28 +0800

    style(component)：移除无用组件

--- buyer/wap/themes/default/components/index.js
+++ buyer/wap/themes/default/components/index.js
@@ -1,33 +1,21 @@
 import Vue from 'vue'
 import RegionPicker from './RegionPicker'
-import Logo from './Logo'
 import CountDownBtn from './CountDownBtn'
 import Face from './Face'
-import Footer from './Footer'
 import Header from './Header'
-import HeaderOther from './HeaderOther'
-import Nav from './Nav'
 import Operation from './Operation'
-import PicZoom from './PicZoom'
 import Search from './Search'
-import Shortcut from './Shortcut'
 import TabBar from './TabBar'
 import NavBar from './NavBar'
 import ShortcutBox from './ShortcutBox'
 
 export {
   RegionPicker,
-  Logo,
   CountDownBtn,
-  Footer,
   Face,
   Header,
-  HeaderOther,
-  Nav,
   Operation,
-  PicZoom,
   Search,
-  Shortcut,
   TabBar,
   NavBar,
   ShortcutBox
