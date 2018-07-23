abaeb3b01eb0db8f5b59aac780a3647359cab183
赖云川 <andste.cc@gmail.com>
2018-07-23 19:24:07 +0800

    style()：增加支付完成页面

---
+++ buyer/pc/themes/b2b2c_v5/pages/payment-complete.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="payment-complete" class="w" style="text-align: center">
+    <h1 style="margin-top: 100px">支付状态回调页面</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'payment-complete'
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
