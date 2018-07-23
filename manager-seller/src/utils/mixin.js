37464ed5b35cb84f9ea629d402f67db2cdadd5ad
赖云川 <andste.cc@gmail.com>
2018-07-23 11:02:18 +0800

    style(mixin): 全局mixin增加导出Excel方法

--- manager-seller/src/utils/mixin.js
+++ manager-seller/src/utils/mixin.js
@@ -4,7 +4,8 @@
 
 import Vue from 'vue'
 import { Foundation } from '~/ui-utils'
-import { domain, api } from '~/ui-domain'
+import { api, domain } from '~/ui-domain'
+import * as XLSX from 'xlsx'
 
 export default {
   data() {
@@ -39,6 +40,40 @@ export default {
     /** 格式化时间戳 */
     MixinUnixToDate(row, column, cellValue, index) {
       return Foundation.unixToDate(cellValue)
+    },
+    /**
+     * 导出Excel
+     * @param json   要导出的json数据
+     * @param name   要导出的文件名
+     * @param type   要导出的数据类型
+     * @constructor
+     */
+    MixinExportJosnToExcel(json, name = 'data', type = 'application/octet-stream') {
+      const wb = { SheetNames: ['Sheet1'], Sheets: {}, Props: { }}
+      wb.Sheets['Sheet1'] = XLSX.utils.json_to_sheet(json)
+      const wopts = { bookType: 'xlsx', bookSST: false, type: 'binary' }
+      let blob = new Blob([s2ab(XLSX.write(wb, wopts))], { type })
+      let link = document.createElement('a')
+      link.href = window.URL.createObjectURL(blob)
+      link.download = `${name}.xlsx`
+      link.click()
+      // 释放资源
+      setTimeout(() => {
+        URL.revokeObjectURL(link.href)
+      }, 100)
+
+      function s2ab(s) {
+        if (typeof ArrayBuffer !== 'undefined') {
+          const buf = new ArrayBuffer(s.length)
+          const view = new Uint8Array(buf)
+          for (let i = 0; i !== s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF
+          return buf
+        } else {
+          const buf = new Array(s.length)
+          for (let i = 0; i !== s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF
+          return buf
+        }
+      }
     }
   }
 }
