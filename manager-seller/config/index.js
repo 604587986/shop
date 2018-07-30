62937dc482ee1b4b01c758bb2412b129734dd34b
赖云川 <andste.cc@gmail.com>
2018-07-30 15:18:06 +0800

    style(config): port兼容

--- manager-seller/config/index.js
+++ manager-seller/config/index.js
@@ -16,7 +16,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: _domain[1], // can be overwritten by process.env.HOST
-    port: _domain[2], // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: _domain[2] || 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: false,
