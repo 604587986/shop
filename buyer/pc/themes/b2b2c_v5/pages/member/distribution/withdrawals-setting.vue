902ad8610a39e1560b4863e3c060084a083b6f70
赖云川 <andste.cc@gmail.com>
2018-08-02 10:43:19 +0800

    style(distribution)：分销相关代码提交

---
+++ buyer/pc/themes/b2b2c_v5/pages/member/distribution/withdrawals-setting.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="withdrawals-setting">
+    <div class="member-nav">
+      <ul class="member-nav-list">
+        <li><nuxt-link to="./withdrawals-setting">提现设置</nuxt-link></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'withdrawals-setting'
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
