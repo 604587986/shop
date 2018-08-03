694074d4dfb925dc57a313e952a77d5c52c010bb
赖云川 <andste.cc@gmail.com>
2018-08-03 18:20:26 +0800

    style(component)：底部TabBar组件调整

--- buyer/wap/themes/default/components/TabBar.vue
+++ buyer/wap/themes/default/components/TabBar.vue
@@ -2,8 +2,8 @@
   <van-tabbar v-model="activeIndex" style="box-shadow: 0 0 10px 0 hsla(0,6%,58%,.6);">
     <van-tabbar-item icon="wap-home" to="/">首页</van-tabbar-item>
     <van-tabbar-item icon="chat" to="/category">分类</van-tabbar-item>
-    <van-tabbar-item icon="cart" info="99+">购物车</van-tabbar-item>
-    <van-tabbar-item icon="contact">我的</van-tabbar-item>
+    <van-tabbar-item icon="cart" to="/cart" :info="cartBadge ? (cartBadge > 99 ? '99+' : cartBadge) : ''">购物车</van-tabbar-item>
+    <van-tabbar-item icon="contact" to="/member">我的</van-tabbar-item>
   </van-tabbar>
 </template>
 
@@ -15,10 +15,21 @@
       return {
         activeIndex: this.active || 0
       }
+    },
+    computed: {
+      /** 购物车徽章 */
+      cartBadge() {
+        return this.$store.getters.allCount
+      }
     }
   }
 </script>
 
 <style type="text/scss" lang="scss" scoped>
-
+  @import "../assets/styles/color";
+  /deep/ {
+    .van-tabbar-item--active {
+      color: $color-main
+    }
+  }
 </style>
