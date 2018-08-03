550fb974d82c08541afe2c59a00e03ce7ca2456b
赖云川 <andste.cc@gmail.com>
2018-08-03 18:20:59 +0800

    style(login)：登录页适配

--- buyer/wap/themes/default/pages/login.vue
+++ buyer/wap/themes/default/pages/login.vue
@@ -1,109 +1,120 @@
 <template>
   <div id="login">
-    <!-- 大图 -->
-    <div class="login-content">
-      <div class="prompt">
-        <div>
-          <i></i>
-          <p>依据《网络安全法》，为保障您的账户安全和正常使用，请尽快完成手机号验证！ 新版<a href="#">《javashop隐私政策》</a>已上线，将更有利于保护您的个人隐私。</p>
-        </div>
+    <nav-bar title="登录"></nav-bar>
+    <!--登录方式切换-->
+    <div class="login-tab">
+      <div
+        class="tab-item account"
+        :class="[login_type === 'account' && 'active']"
+        @click="login_type = 'account'"
+      >
+        <span>账号密码登录</span>
       </div>
-      <div class="login-banner">
-        <div class="bg-banner">
-          <div class="banner-img"></div>
-          <!-- 登录页 -->
-          <div class="login-box">
-            <div class="login-form">
-              <div class="tips-wapper">
-                <i></i>
-                <p>Javashop不会以任何理由要求您转账汇款，谨防诈骗。</p>
-              </div>
-              <div class="login-tab">
-                <ul>
-                  <li @click="login_type = 'quick'">
-                    <a :class="['tab-a', login_type === 'quick' ? 'active' : '']">快捷登录</a>
-                  </li>
-                  <li @click="login_type = 'account'">
-                    <a :class="[login_type === 'account' ? 'active' : '']">账号登录</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="login-interface">
-                <div v-show="login_type === 'quick'" class="login-show quick-login">
-                  <form class="quick-form">
-                    <div class="item item-form-o">
-                      <label for="mobile">
-                        <i class="iconfont ea-icon-mobile"></i>
-                      </label>
-                      <input id="mobile" v-model="quickForm.mobile" placeholder="请输入手机号" maxlength="11" autofocus>
-                    </div>
-                    <div class="item">
-                      <label for="validcode-mobile">
-                        <i class="iconfont ea-icon-safe"></i>
-                      </label>
-                      <input id="validcode-mobile" v-model="quickForm.captcha" placeholder="图片验证码" maxlength="4">
-                      <img class="validcode-img" :src="val_code_url" @click="handleChangeValUrl">
-                    </div>
-                    <div class="item item-form-t">
-                      <en-count-down-btn :start="sendValidMobileSms" class="send-sms-btn"/>
-                    </div>
-                    <div class="item item-form-p">
-                      <label for="sms-code">
-                        <i class="iconfont ea-icon-sms"></i>
-                      </label>
-                      <input id="sms-code" v-model="quickForm.sms_code" placeholder="短信验证码" maxlength="6">
-                    </div>
-                    <div class="forget">
-                      <span><nuxt-link :to="'/find-password' + MixinForward">忘记密码</nuxt-link></span>
-                    </div>
-                    <button class="form-sub" type="button" @click="handleLogin">登&nbsp;&nbsp;&nbsp;录</button>
-                  </form>
-                </div>
-                <div v-show=" login_type === 'account'" class="login-show account-login">
-                  <form class="account-form">
-                    <div class="item">
-                      <label for="username">
-                        <i class="iconfont ea-icon-persion"></i>
-                      </label>
-                      <input id="username" v-model="accountForm.username" placeholder="邮箱/用户名/已验证手机" maxlength="20">
-                    </div>
-                    <div class="item">
-                      <label for="password">
-                        <i class="iconfont ea-icon-password"></i>
-                      </label>
-                      <input id="password" v-model="accountForm.password" type="password" placeholder="请输入密码" maxlength="20">
-                    </div>
-                    <div class="item">
-                      <label for="validcode">
-                        <i class="iconfont ea-icon-safe"></i>
-                      </label>
-                      <input id="validcode" v-model="accountForm.captcha" placeholder="图片验证码" maxlength="4">
-                      <img class="validcode-img" :src="val_code_url" @click="handleChangeValUrl">
-                    </div>
-                    <div class="forget">
-                      <span><nuxt-link :to="'/find-password' + MixinForward">忘记密码</nuxt-link></span>
-                    </div>
-                    <button class="form-sub" type="button" @click="handleLogin">登&nbsp;&nbsp;&nbsp;录</button>
-                  </form>
-                </div>
-              </div>
-              <!-- 第三方登录、立即注册 -->
-              <div class="other-login">
-                <ul>
-                  <li class="other-one"><a :href="getConnectUrl('pc', 'QQ')"><span>QQ</span></a></li>
-                  <li class="other-one"><a :href="getConnectUrl('pc', 'WECHAT')"><span>微信</span></a></li>
-                  <li class="other-one"><a :href="getConnectUrl('pc', 'WEIBO')"><span>微博</span></a></li>
-                  <li><a :href="getConnectUrl('pc', 'ALIPAY')"><span>支付宝</span></a></li>
-                  <li class="other-right">
-                    <nuxt-link :to="'/register' + MixinForward" class="registered">
-                      <b></b>立即注册
-                    </nuxt-link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div
+        class="tab-item quick"
+        :class="[login_type === 'quick' && 'active']"
+        @click="login_type = 'quick'"
+      >
+        <span>短信验证码登录</span>
+      </div>
+    </div>
+    <!--账号密码登录、短信验证码登录-->
+    <div class="login-content">
+      <div v-show="login_type === 'account'" class="content-item account">
+        <no-ssr>
+          <van-cell-group :border="false">
+            <van-field
+              v-model="accountForm.username"
+              clearable
+              label="用户名"
+              placeholder="邮箱/用户名/已验证手机"
+            />
+            <van-field
+              v-model="accountForm.password"
+              type="password"
+              label="密码"
+              placeholder="请输入密码"
+            />
+            <van-field
+              v-model="accountForm.captcha"
+              center
+              clearable
+              label="图片验证码"
+              placeholder="请输入图片验证码"
+              maxlength="4"
+            >
+              <img v-if="captcha_url" slot="button" :src="captcha_url" @click="handleChangeCaptchalUrl" class="captcha-img"/>
+            </van-field>
+          </van-cell-group>
+        </no-ssr>
+        <van-button
+          size="large"
+          class="login-btn"
+          :disabled="login_disabled_account"
+          @click="handleLogin"
+        >登&nbsp;&nbsp;&nbsp;录</van-button>
+      </div>
+      <div v-show="login_type === 'quick'" class="content-item quick">
+        <no-ssr>
+          <van-cell-group :border="false">
+            <van-field
+              v-model="quickForm.captcha"
+              center
+              clearable
+              label="图片验证码"
+              placeholder="请输入图片验证码"
+              maxlength="4"
+            >
+              <img v-if="captcha_url" slot="button" :src="captcha_url" @click="handleChangeCaptchalUrl" class="captcha-img"/>
+            </van-field>
+            <van-field
+              v-model="quickForm.mobile"
+              type="tel"
+              clearable
+              label="手机号码"
+              placeholder="请输入手机号"
+              maxlength="11"
+              @focus="showKeyboard = true"
+            />
+            <van-field
+              v-model="quickForm.sms_code"
+              center
+              clearable
+              label="短信验证码"
+              placeholder="请输入短信验证码"
+            >
+              <en-count-down-btn slot="button" :start="sendValidMobileSms">发送验证码</en-count-down-btn>
+            </van-field>
+          </van-cell-group>
+        </no-ssr>
+        <van-button
+          size="large"
+          class="login-btn"
+          :disabled="login_disabled_quick"
+          @click="handleLogin"
+        >登&nbsp;&nbsp;&nbsp;录</van-button>
+      </div>
+    </div>
+    <!--忘记密码、手机注册-->
+    <div class="login-nav">
+      <nuxt-link :to="'/find-password' + MixinForward">
+        <i class="iconfont ea-icon-password"></i>忘记密码
+      </nuxt-link>
+      <nuxt-link :to="'/register' + MixinForward">
+        <i class="iconfont ea-icon-mobile"></i>手机注册
+      </nuxt-link>
+    </div>
+    <!--第三方登录-->
+    <div class="login-other">
+      <h4>其他登录方式</h4>
+      <div class="icons-login-other">
+        <a :href="getConnectUrl('pc', 'QQ')"><i class="iconfont ea-icon-qq"></i></a>
+        <a :href="getConnectUrl('pc', 'WECHAT')"><i class="iconfont ea-icon-wechat"></i></a>
+        <a :href="getConnectUrl('pc', 'WEIBO')"><i class="iconfont ea-icon-weibo"></i></a>
+        <a :href="getConnectUrl('pc', 'ALIPAY')"><i class="iconfont ea-icon-alipay"></i></a>
+      </div>
+      <div class="agreement-tips">
+        <p>登录即代表您已同意<a href="javascript:;">Javashop隐私政策</a></p>
       </div>
     </div>
   </div>
@@ -115,24 +126,47 @@
   import * as API_Common from '@/api/common'
   import * as API_Passport from '@/api/passport'
   import * as API_Connect from '@/api/connect'
+  import EnCountDownBtn from "@/components/CountDownBtn"
   export default {
     name: 'login',
+    components: {EnCountDownBtn},
     layout: 'full',
     head() {
       return {
-        title: '会员登录-Javashop多店铺示例商城'
+        title: `会员登录-${this.site.title}`
       }
     },
     data() {
       return {
-        login_type: 'quick', // 'account',
-        login_banner: 'http://data.andste.cc/developers/web/temp/images/background-banner.jpg',
+        login_type: 'account', // 'account',
         /** 图片验证码 */
-        val_code_url: API_Common.getValidateCodeUrl(this.$store.state.uuid, 'LOGIN'),
+        captcha_url: process.client ? API_Common.getValidateCodeUrl(this.$store.state.uuid, 'LOGIN') : '',
         /** 快捷登录 表单 */
-        quickForm: {},
+        quickForm: {
+          mobile: '',
+          captcha: '',
+          sms_code: ''
+        },
         /** 普通登录 表单 */
-        accountForm: {}
+        accountForm: {
+          username: '',
+          password: '',
+          captcha: ''
+        },
+        /** 显示数字键盘 */
+        showKeyboard: false
+      }
+    },
+    computed: {
+      // 普通登录按钮 是否禁用
+      login_disabled_account() {
+        const { username, password, captcha } = this.accountForm
+        return !(username && password && captcha)
+      },
+      // 短信登录按钮 是否禁用
+      login_disabled_quick() {
+        const { captcha, mobile, sms_code } = this.quickForm
+        return !(captcha && mobile && sms_code)
       }
     },
     methods: {
@@ -140,14 +174,14 @@
       sendValidMobileSms() {
         const { mobile, captcha } = this.quickForm
         return new Promise((resolve, reject) => {
-          if (!mobile) {
+          if (!captcha) {
+            this.$message.error('请输入图片验证码！')
+            reject()
+          } else if (!mobile) {
             this.$message.error('请输入手机号码！')
             reject()
           } else if (!RegExp.mobile.test(mobile)) {
             this.$message.error('手机号码格式有误！')
-            reject()
-          } else if (!captcha) {
-            this.$message.error('请输入图片验证码！')
             reject()
           } else {
             API_Passport.sendLoginSms(mobile, captcha).then(() => {
@@ -158,8 +192,8 @@
         })
       },
       /** 改变图片验证码URL */
-      handleChangeValUrl() {
-        this.val_code_url = API_Common.getValidateCodeUrl(this.$store.state.uuid, 'LOGIN')
+      handleChangeCaptchalUrl() {
+        this.captcha_url = API_Common.getValidateCodeUrl(this.$store.state.uuid, 'LOGIN')
       },
       /** 登录事件 */
       handleLogin() {
@@ -186,6 +220,7 @@
           }
         })
       },
+      /** 获取第三方登录链接 */
       getConnectUrl: API_Connect.getConnectUrl,
       ...mapActions({
         login: 'user/loginAction'
@@ -195,195 +230,84 @@
 </script>
 
 <style type="text/scss" lang="scss" scoped>
-  .login-content .prompt {
-    width: 100%;
-    text-align: center;
-    background: #fff8f0;
-    margin: 0 auto;
-    padding: 10px 0;
-    a { color: black }
-  }
-  .login-content .login-banner {
-    background: #FF5E18;
-    width: 100%;
-    .bg-banner {
-      width: 1200px;
-      margin: 0 auto;
-      position: relative;
-      .banner-img {
-        background: url(../assets/images/background-banner.jpg) no-repeat center center;
-        background-size: cover;
-        height: 560px;
-      }
-      .login-box {
-        float: right;
-        position: absolute;
-        right: 20px;
-        top: 20px;
-        width: 346px;
-        background: #ffffff;
-      }
-    }
-  }
-  .login-box .login-form .tips-wapper {
-    background: #fff8f0;
-    width: 100%;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    text-align: center;
-    p {
-      font-size: 12px;
-      color: #999;
-    }
-  }
-  .login-box .login-form .login-tab {
-    height: 60px;
-    border-bottom: 1px solid #f4f4f4;
-    ul li {
-      float: left;
-      width: 50%;
-      padding: 20px 0;
-      text-align: center;
-      font-size: 20px;
-      cursor: pointer;
-      a {
-        display: block;
-        height: 18px;
-        width: 99%;
-        color: #666;
-        &:hover, &.active { color: #f42424 }
-      }
-      .tab-a { border-right: 1px solid #f4f4f4 }
-    }
-  }
-  .login-form .login-interface .account-login { display: block }
-  .login-form .login-interface .login-show form {
-    width: 280px;
-    margin: 25px auto;
-  }
-  .active { color: #f42424 }
-  .login-interface form .item.focus {
-    border-color: #666;
-    label {
-      border-color: #666;
-      .iconfont { color: #666 }
-    }
-  }
-  .login-interface form .item-form-t {
-    width: 225px !important;
-    height: 30px;
-    border: 0 !important;
-    button {
-      width: 100%;
-      height: 100%;
-      cursor: pointer;
-      border-radius: 3px;
-    }
-  }
-  .login-interface .quick-form .item {
-    position: relative;
-    width: 100%;
-    height: 35px;
-    margin-bottom: 10px;
-    border: 1px solid #bdbdbd;
-  }
-  .login-interface .account-form .item {
-    position: relative;
-    width: 100%;
-    height: 35px;
-    margin-bottom: 25px;
-    border: 1px solid #bdbdbd;
-  }
-  .validcode-img {
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 85px;
-    height: 35px;
-    cursor: pointer;
-  }
-  .login-interface form .item label {
-    width: 35px;
-    height: 35px;
-    float: left;
-    display: inline-block;
-    border-right: 1px solid #bdbdbd;
-  }
-  .login-interface form .item .iconfont {
-    display: block;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    line-height: 35px;
-    color: #666;
-  }
-  .login-interface form .item input {
-    display: inline-block;
-    height: 35px;
-    line-height: 35px;
-    width: 225px;
-    padding-left: 10px;
-  }
-  .login-interface form .forget {
-    height: 18px;
-    margin: 20px 0;
-  }
-  .login-interface form .forget span {
-    float: right;
-  }
-  .login-interface form .form-sub {
-    width: 100%;
-    height: 35px;
-    background: #f42424;
-    text-align: center;
-    margin: 10px 0;
-    cursor: pointer;
-    color: #fff;
+  @import "../assets/styles/color";
+  .login-tab {
+    height: 44px;
+    line-height: 45px;
+    border-bottom: 1px solid #ebeef5;
+    color: #333;
     font-size: 14px;
-    transition: background ease-out .2s;
-  }
-  .login-interface form .form-sub:hover {
-    background: #f65555;
-  }
-  .login-box .login-form .other-login {
-    height: 50px;
-    border-top: 1px solid #f4f4f4;
-    padding: 0 20px;
-    background-color: #fcfcfc;
-  }
-  .login-box .login-form .other-login ul li {
-    float: left;
-    line-height: 50px;
-    a>span {
-      padding: 0 10px;
-      color: #666666;
-      &:hover { color: #f42424 }
+    .tab-item {
+      width: 50%;
+      float: left;
+      text-align: center;
+      &.active span {
+        border-bottom: 2px solid #f23030;
+        padding: 13px 10px;
+      }
     }
   }
-  .login-box .login-form .other-login ul .other-one a>span {
-    border-right: 1px solid #bdbdbd;
+  .login-content {
+    padding: 10px 20px 20px 20px;
+    /deep/ {
+      .van-cell {
+        padding: 10px 0;
+        &::after { left: 0 }
+      }
+      .login-btn {
+        color: #fff;
+        margin-top: 10px;
+        background-color: $color-main;
+        &.van-button--disabled {
+          color: #999;
+          background-color: #e8e8e8;
+          border: 1px solid #e5e5e5;
+        }
+      }
+    }
+    .captcha-img {
+      width: 70px;
+      height: 24px;
+    }
   }
-  .login-box .login-form .other-login ul .other-right {
-    float: right;
+  .login-nav {
+    padding: 0 20px;
+    a + a { float: right }
   }
-  .other-login ul .other-right a {
-    color: #f42424;
-    font-size: 15px;
-  }
-  .send-sms-btn {
-    width: 187px;
-    height: 35px;
-    border: none;
-    color: #ffffff;
-    background-color: #607d8b;
-    transition: background ease-out .2s;
-  }
-  .send-sms-btn:hover {
-    background-color: #516a77;
-  }
-  .send-sms-btn:disabled {
-    background-color: #ccc !important;
-    color: #fff !important;
-    cursor: not-allowed !important;
+  .login-other {
+    margin-top: 80px;
+    height: 85px;
+    position: relative;
+    padding: 24px 10px 0 10px;
+    border-top: 1px solid #d7d7d7;
+    h4 {
+      position: absolute;
+      top: -19px;
+      left: 50%;
+      background-color: white;
+      padding: 5px 15px;
+      color: #bfbfbf;
+      transform: translateX(-50%);
+    }
+    .icons-login-other {
+      width: 100%;
+      text-align: center;
+      i {
+        font-size: 28px;
+        margin-right: 15px;
+      }
+      .ea-icon-qq { color: #68B2FC }
+      .ea-icon-wechat { color: #56C13D }
+      .ea-icon-weibo { color: #E31C34 }
+      .ea-icon-alipay { color: #1AA1E6 }
+    }
+    .agreement-tips {
+      margin-top: 20px;
+      width: 100%;
+      text-align: center;
+      font-size: 14px;
+      line-height: 30px;
+      color: #333
+    }
   }
 </style>
