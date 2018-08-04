2a4924cad68634c6ec1723627bd6820c5d69125f
赖云川 <andste.cc@gmail.com>
2018-08-04 14:30:40 +0800

    style(nuxt)：Vant组件注册

--- buyer/wap/themes/default/plugins/vue-vant.js
+++ buyer/wap/themes/default/plugins/vue-vant.js
@@ -1,29 +1,8 @@
 /**
- * Created by andste.cc@gmail.com on 2018/7/9.
- * 注册常用的Vant组件
+ * Created by Andste on 2018/7/9.
+ * 注册Vant组件
  */
 import Vue from 'vue'
+import Vant from 'vant'
 
-import {
-  Button,
-  Cell,
-  CellGroup,
-  Field,
-  NavBar,
-  NumberKeyboard,
-  Icon,
-  Popup,
-  Tabbar,
-  TabbarItem
-} from 'vant'
-
-Vue.use(Button)
-Vue.use(Cell)
-Vue.use(CellGroup)
-Vue.use(Field)
-Vue.use(NavBar)
-Vue.use(NumberKeyboard)
-Vue.use(Icon)
-Vue.use(Popup)
-Vue.use(Tabbar)
-Vue.use(TabbarItem)
+Vue.use(Vant)
