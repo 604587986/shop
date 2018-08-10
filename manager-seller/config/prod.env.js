8e872519c943c4d4abd2b9eafd65b66aef01209e
赖云川 <andste.cc@gmail.com>
2018-08-10 11:53:14 +0800

    style(api): API适配微服务

--- manager-seller/config/prod.env.js
+++ manager-seller/config/prod.env.js
@@ -2,6 +2,5 @@ const domain = require('../ui-domain')
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-  SELLER_API: `"${domain.api_pro.seller}"`, // api接口地址
-  BASE_API: `"${domain.api_pro.base}"` // baseapi接口地址
+  SELLER_API: `"${domain.api_pro.seller}"` // api接口地址
 }
