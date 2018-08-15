23c3787ea306c78042b2749a3b5ff94620270a90
yangpei <1208812362@qq.com>
2018-08-15 19:50:59 +0800

    后台-分销-个人分销商&提现管理 （部分完成-提交）

--- manager-admin/src/views/distribution/distributor/distributorList.vue
+++ manager-admin/src/views/distribution/distributor/distributorList.vue
@@ -12,16 +12,16 @@
       </div>
       <template slot="table-columns">
         <!--操作-->
-        <el-table-column label="操作" width="150">
-          <template slot-scope="scope">
-            <el-button
-              type="primary"
-              size="mini"
-              icon="el-icon-plus"
-              circle
-              @click="handleOperateSee(scope.row)"></el-button>
-          </template>
-        </el-table-column>
+        <!--<el-table-column label="操作" width="150">-->
+          <!--<template slot-scope="scope">-->
+            <!--<el-button-->
+              <!--type="primary"-->
+              <!--size="mini"-->
+              <!--icon="el-icon-plus"-->
+              <!--circle-->
+              <!--@click="handleOperateSee(scope.row)"></el-button>-->
+          <!--</template>-->
+        <!--</el-table-column>-->
         <!--姓名-->
         <el-table-column prop="sn" label="姓名"/>
         <!--人数-->
@@ -43,19 +43,19 @@
         <!--修改模式-->
         <el-table-column label="修改模式" width="150">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" @click="handleOperateSee(scope.row)">修改模式</el-button>
+            <el-button size="mini" type="primary" @click="handleEditTpl(scope.row)">修改模式</el-button>
           </template>
         </el-table-column>
         <!--营业额统计-->
         <el-table-column label="营业额统计" width="150">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" @click="handleOperateSee(scope.row)">营业额统计</el-button>
+            <el-button size="mini" type="primary" @click="handleTurnoverStatistics(scope.row)">营业额统计</el-button>
           </template>
         </el-table-column>
         <!--利润额统计-->
         <el-table-column label="利润额统计" width="150">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" @click="handleOperateSee(scope.row)">利润额统计</el-button>
+            <el-button size="mini" type="primary" @click="handleProfitStatistics(scope.row)">利润额统计</el-button>
           </template>
         </el-table-column>
       </template>
@@ -91,29 +91,39 @@
         // 列表数据
         tableData: [],
 
+        /** 分页信息 */
         pageData: []
       }
     },
     mounted() {
-      this.GET_AchievementList()
+      this.GET_DistributorList()
     },
     methods: {
       /** 分页大小发生改变 */
       handlePageSizeChange(size) {
         this.params.page_size = size
-        this.GET_AchievementList()
+        this.GET_DistributorList()
       },
 
       /** 分页页数发生改变 */
       handlePageCurrentChange(page) {
         this.params.page_no = page
-        this.GET_AchievementList()
+        this.GET_DistributorList()
+      },
+
+      /** 搜索事件触发 */
+      searchEvent(data) {
+        this.params = {
+          ...this.params,
+          member_name: data
+        }
+        this.GET_DistributorList()
       },
 
       /** 获取个人分销商列表 */
-      GET_AchievementList() {
+      GET_DistributorList() {
         this.loading = true
-        API_distribution.getAchievementList(this.params).then(response => {
+        API_distribution.getDistributorList(this.params).then(response => {
           this.loading = false
           this.tableData = response.data
           this.pageData = {
@@ -124,8 +134,18 @@
         })
       },
 
-      /** 查看 */
-      handleOperateSee() {
+      /** 修改模式 */
+      handleEditTpl() {
+
+      },
+
+      /** 营业额统计 */
+      handleTurnoverStatistics() {
+
+      },
+
+      /** 利润额统计 */
+      handleProfitStatistics() {
 
       }
     }
--- manager-admin/src/views/distribution/distributor/statistics.vue
+++ manager-admin/src/views/distribution/distributor/statistics.vue
@@ -1,13 +1,240 @@
 <template>
-    $END$
+  <div class="bg-shop-summary">
+    <div class="btn-tools">
+      <div class="conditions">
+        <span>日期设置:</span>
+        <en-year-month-picker size="mini" @changed="changeYearMonth"></en-year-month-picker>
+      </div>
+      <div class="conditions">
+        <span>当前会员:</span>
+      </div>
+      <el-button class="btn-opera" size="mini" type="primary" @click="handleSearch">开始搜索</el-button>
+    </div>
+    <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+      <el-tab-pane label="营业额统计" name="first">
+        <div id="turnover" :style="{height: tableHeight + 'px'}"></div>
+      </el-tab-pane>
+      <el-tab-pane label="利润额统计" name="second">
+        <div id="profit" :style="{height: tableHeight + 'px'}"></div>
+      </el-tab-pane>
+      <el-tab-pane label="订单数统计" name="third">
+        <div id="ordernum" :style="{height: tableHeight + 'px'}"></div>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "statistics"
+  import * as API_distribution from '@/api/distribution'
+  export default {
+    name: 'statistic',
+    data() {
+      return {
+        /** 列表loading状态 */
+        loading: false,
+
+        /** 图表参数*/
+        params: {
+          /** 当前选择的日期类型 */
+          cycle_type: 'YEAR',
+
+          /** 年份 */
+          year: '2018',
+
+          /** 月份*/
+          month: '6'
+        },
+
+        tableHeight: document.body.clientHeight / 2
+      }
+    },
+    created() {
+      this.GET_PriceStatistics()
+    },
+    mounted() {
+      window.onresize = this.countTableHeight
+      this.$nextTick(() => {
+        this.priceListChart = this.$echarts.init(document.getElementById('priceList'))
+        this.purchaseTimeChart = this.$echarts.init(document.getElementById('purchaseTime'))
+      })
+    },
+    methods: {
+      /** 窗口缩放时计算table高度 */
+      countTableHeight() {
+        this.tableHeight = document.body.clientHeight / 2
+        /** 图表刷新 */
+        setTimeout(this.priceListChart.resize)
+        setTimeout(this.purchaseTimeChart.resize)
+      },
+
+      /** 改变日期的回调*/
+      changeYearMonth(obj) {
+        this.params = {
+          cycle_type: obj.type,
+
+          year: obj.year
+        }
+        if (obj.type === 'MONTH') {
+          this.params = {
+            cycle_type: obj.type,
+
+            year: obj.year,
+
+            month: parseInt(obj.month)
+          }
+        }
+      },
+
+      /** 价格区间 */
+      changePriceRange(obj) {
+        this.params = {
+          ...this.params,
+          ranges: obj
+        }
+      },
+
+      /** 执行搜索 */
+      handleSearch() {
+        this.GET_PriceStatistics()
+      },
+
+      GET_PriceStatistics() {
+        API_buyAnyalysis.getbuyAnyalysisPeriodList(this.params).then(response => {
+          this.purchaseTimeChart.setOption({
+            title: { text: '购买时段分布', x: 'center', subtext: '当前时间段24小时购买总量分布' },
+            tooltip: { trigger: 'axis' },
+            legend: { show: true, orient: 'vertical', data: [{ name: '下单量' }], bottom: '10px' },
+            color: ['#7CB5EC'],
+            toolbox: {
+              feature: {
+                magicType: { type: ['line', 'bar'] },
+                restore: {},
+                saveAsImage: {}
+              }
+            },
+            xAxis: {
+              name: '时',
+              type: 'category',
+              boundaryGap: false,
+              data: response.xAxis
+            },
+            yAxis: {
+              name: '下单量（次）',
+              type: 'value',
+              axisLabel: {
+                formatter: '{value} 次'
+              }
+            },
+            series: [
+              {
+                type: 'line',
+                data: response.series.data,
+                markPoint: {
+                  data: [
+                    { type: 'max', name: '最大值' },
+                    { type: 'min', name: '最小值' }
+                  ]
+                },
+                markLine: {
+                  data: [
+                    { type: 'average', name: '平均值' }
+                  ]
+                }
+              }
+            ]
+          })
+        })
+        API_buyAnyalysis.getbuyAnyalysisRangesList(this.params).then(response => {
+          this.priceListChart.setOption({
+            title: { text: '客价单分布', x: 'center' },
+            tooltip: { trigger: 'axis' },
+            legend: { show: true, orient: 'vertical', data: [{ name: '下单量' }], bottom: '10px' },
+            color: ['#7CB5EC'],
+            toolbox: {
+              feature: {
+                magicType: { type: ['line', 'bar'] },
+                restore: {},
+                saveAsImage: {}
+              }
+            },
+            xAxis: {
+              name: '价格',
+              type: 'category',
+              boundaryGap: false,
+              data: response.xAxis
+            },
+            yAxis: {
+              name: '下单量（次）',
+              type: 'value',
+              axisLabel: {
+                formatter: '{value} 次'
+              }
+            },
+            series: [
+              {
+                type: 'line',
+                data: response.series.data,
+                markPoint: {
+                  data: [
+                    { type: 'max', name: '最大值' },
+                    { type: 'min', name: '最小值' }
+                  ]
+                },
+                markLine: {
+                  data: [
+                    { type: 'average', name: '平均值' }
+                  ]
+                }
+              }
+            ]
+          })
+        })
+      }
     }
+  }
 </script>
 
-<style scoped>
+<style type="text/scss" lang="scss" scoped>
+  .bg-shop-summary {
+    margin: 15px;
+    padding: 15px;
+    background-color: #fff;
+    border: 1px solid #9a9a9a;
+    p {
+      margin: 15px 0;
+      text-align: left;
+    }
+  }
+  /deep/ .el-table td:not(.is-left) {
+    text-align: center;
+  }
+
+  div.btn-tools {
+    margin-right: 30px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    .conditions {
+      margin-right: 30px;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      justify-content: flex-start;
+      align-items: center;
+      span {
+        display: inline-block;
+        margin-right: 10px;
+        font-size: 14px;
+        color: #606266;
+      }
+    }
+    .btn-opera {
+      margin-left: 10px;
+    }
+  }
 
 </style>
+
+
