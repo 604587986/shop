daf733820e10f22074e849c0400cd3d2f5d0bebd
赖云川 <andste.cc@gmail.com>
2018-08-20 15:47:45 +0800

    style(cart)：购物车适配

---
+++ buyer/wap/themes/default/pages/cart/coupons.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="coupons">
+    <nav-bar title="店铺优惠券"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'coupons'
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+
+</style>
