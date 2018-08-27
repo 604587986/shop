6222252ec8df53d0cb66e30e5d3270846b96270e
赖云川 <andste.cc@gmail.com>
2018-08-27 10:59:04 +0800

    style(home)：首页组件调整

--- buyer/wap/themes/default/pages/-index/index.js
+++ buyer/wap/themes/default/pages/-index/index.js
@@ -2,9 +2,9 @@
  * Created by andste.cc@gmail.com on 2018/6/6.
  */
 
-import IndexBanner from './banner'
-import IndexMenu from './menu'
-import IndexSearchBar from './SearchBar'
+import IndexBanner from './-banner'
+import IndexMenu from './-menu'
+import IndexSearchBar from './-searchBar'
 
 export {
   IndexBanner,
