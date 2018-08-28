98b22515c8ccfe38f7dda648dadd71f5c2006c2b
赖云川 <andste.cc@gmail.com>
2018-08-28 11:03:05 +0800

    style(goods)：商品页限时抢购展示适配

---
+++ buyer/wap/themes/default/pages/goods/-goods-promotions.vue
@@ -0,0 +1,124 @@
+<template>
+  <div v-if="promotions && showPromotion" class="promotions-container">
+    <div class="pro-list promotions-box" id="promotions-box">
+      <div class="pro-title">促销信息</div>
+      <div class="pro-content prom">
+        <template v-for="(prom, index) in promotions">
+          <!--满减-->
+          <template v-if="prom.full_discount_vo">
+            <div :key="index + '-full'" class="prom-item">
+              <em class="hl_red_bg">满减</em>
+              <em class="hl_red">满{{ prom.full_discount_vo.full_money }}元，立减现金 <span class="price">{{ prom.full_discount_vo.minus_value }}元</span></em>
+              <!--<nuxt-link v-if="index === 0" :to="'/shop/'+ prom.full_discount_vo.seller_id + '/promotions'" target="blank"> 详情>></nuxt-link>&nbsp;-->
+            </div>
+            <div v-if="prom.full_discount_vo.full_discount_gift_do" :key="index + '-gift'" class="prom-item">
+              <em class="hl_red_bg">赠礼</em>
+              <em class="hl_red">
+                价值<span class="price">{{ prom.full_discount_vo.full_discount_gift_do.gift_price }}元</span>的
+                <a :href="prom.full_discount_vo.full_discount_gift_do.gift_img" target="_blank">
+                  <img :src="prom.full_discount_vo.full_discount_gift_do.gift_img" class="gift-image">
+                </a>
+              </em>
+            </div>
+            <div v-if="prom.full_discount_vo.coupon_do" :key="index + '-coupon'" class="prom-item">
+              <em class="hl_red_bg">赠券</em>
+              <em class="hl_red">
+                <span class="price">{{ prom.full_discount_vo.coupon_do.coupon_price }}元</span>优惠券
+              </em>
+            </div>
+          </template>
+          <!--单品立减-->
+          <div :key="index" v-if="prom.minus_vo" class="prom-item">
+            <em class="hl_red_bg">单品立减</em>
+            <em class="hl_red">单件立减现金<span class="price">{{ prom.minus_vo.single_reduction_value }}</span>元</em>
+          </div>
+          <!--第二件半价-->
+          <div :key="index" v-if="prom.half_price_vo" class="prom-item">
+            <em class="hl_red_bg">第二件半价</em>
+            <em class="hl_red">第二件半价优惠</em>
+          </div>
+        </template>
+      </div>
+    </div>
+    <div id="promotions-place" class="promotions-place"></div>
+  </div>
+</template>
+
+<script>
+  /**
+   * 商品促销模块
+   * 包含满减满赠、单品立减、第二件半价
+   */
+  import * as API_Promotions from '@/api/promotions'
+  export default {
+    name: 'goods-promotions',
+    props: ['promotions'],
+    data() {
+      return {
+        showPromotion: true
+      }
+    },
+    watch: {
+      promotions: function (newVal) {
+        let flag = false
+        const proms = ['full_discount_vo', 'half_price_vo', 'minus_vo']
+        newVal.forEach(item => {
+          proms.forEach(porm => {
+            if (item[porm]) flag = true
+          })
+        })
+        this.showPromotion = flag
+        if (flag) {
+          this.$nextTick(() => {
+            const $proBox = document.getElementById('promotions-box')
+            const $plaBox = document.getElementById('promotions-place')
+            $plaBox.style.height = $proBox.offsetHeight + 'px'
+          })
+        }
+      }
+    }
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+  @import "../../assets/styles/color";
+  .promotions-container {
+    position: relative;
+  }
+  .promotions-box {
+    background: url("../../assets/images/background-price.png") 0 -12px repeat-x #efefef;
+    padding-bottom: 5px;
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+  .promotions-place {
+    position: relative;
+    z-index: 1;
+  }
+  .pro-content {
+    em.hl_red_bg {
+      padding: 2px 3px;
+      color: $color-main;
+      border: 1px solid $color-main;
+      margin-right: 4px;
+    }
+    .prom-item:not(:first-child) {
+      display: inline-block;
+      .hl_red {
+        display: none;
+      }
+    }
+  }
+  .promotions-box:hover .prom-item {
+    display: block;
+    .hl_red { display: inline-block }
+  }
+  .gift-image {
+    width: 20px;
+    height: 20px;
+    vertical-align: middle;
+  }
+</style>
