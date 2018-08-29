90f33f66c109c9b732a8a37c0dfd33c945ffd1fe
赖云川 <andste.cc@gmail.com>
2018-08-29 14:38:13 +0800

    style(shop)：店铺商品列表适配

--- buyer/wap/themes/default/pages/shop/goods-list.vue
+++ buyer/wap/themes/default/pages/shop/goods-list.vue
@@ -1,61 +1,47 @@
 <template>
-  <div id="shop-goods-list" class="container">
-    <div class="sort-nav">
-      <div class="inner w">
-        <div class="left">
-          <div class="inner-sort">
-            <template v-for="sort in sorts">
-              <div class="item" :key="sort.title" :class="[sort.active && 'active', sort.type === 'asc' ? 'up' : 'down']" @click="handleSortChanged(sort)">
-                {{ sort.title }}<i class="iconfont ea-icon-arrow-down2"></i>
-              </div>
-            </template>
-          </div>
-          <div class="inner-price">
-            ￥
-            <el-input-number size="mini" :controls="false" v-model="params.min_price"/>
-            -
-            <el-input-number size="mini" :controls="false" v-model="params.max_price"/>
-            <el-button size="mini" class="price-btn" @click="handlePriceConfirm">确认</el-button>
-          </div>
-        </div>
-        <div class="inner-search">
-          <el-input placeholder="店内搜索" size="mini" clearable v-model="params.keyword" @keyup.enter.native="handleSearch('shop')">
-            <el-button slot="append" icon="el-icon-search" @click="handleSearch('shop')"></el-button>
-          </el-input>
-          <el-button size="mini" type="danger" class="search-btn" @click="handleSearch('all')">搜全站</el-button>
-        </div>
+  <div id="shop-goods-list" class="container" style="background-color: #f7f7f7">
+    <shop-header :shop="shop"/>
+    <div class="search-box">
+      <form method="get" action="/" @submit.prevent="handleOnSubmit">
+        <input v-model="params.keyword" type="text" maxlength="20" placeholder="想找什么商品？" style="color: rgb(153, 153, 153);">
+        <button type="submit">店内搜</button>
+        <button type="button" @click="handleSearchAll">全站搜</button>
+      </form>
+    </div>
+    <div class="sort-box">
+      <div
+        v-for="(sort, index) in sorts"
+        :key="index"
+        class="item-sort"
+        :class="[sort.active && 'active', sort.type]"
+        @click="handleSortChanged(sort)"
+      >
+        <span>{{ sort.title }}</span>
+        <i class="iconfont ea-icon-arrow-down3"></i>
       </div>
     </div>
-    <div class="goods-container">
-      <div class="inner w">
-        <ul class="goods-list">
-          <li v-for="goods in goods.data" :key="goods.goods_id" class="goods-item">
-            <div class="goods-image">
-              <a :href="'/goods/' + goods.goods_id">
-                <img :src="goods.thumbnail" :alt="goods.name" :title="goods.name">
-              </a>
-            </div>
-            <div class="goods-name">
-              <a :href="'/goods/' + goods.goods_id">{{ goods.name }}</a>
-            </div>
-            <div class="goods-price">
-              <span>RMB：<span class="price">￥<strong>{{ goods.price | unitPrice }}</strong></span></span>
-              <span>已销售：{{ goods.buy_count }}件</span>
-            </div>
-          </li>
-          <span class="clr"></span>
-        </ul>
-        <div v-if="goods" class="goods-pagination">
-          <el-pagination
-            @current-change="handleCurrentChange"
-            :current-page.sync="params.page_no"
-            :page-size="params.page_size"
-            layout="total, prev, pager, next"
-            :total="goods.data_total">
-          </el-pagination>
-        </div>
-      </div>
-    </div>
+    <en-empty v-if="finished && !goodsList.length">暂无商品</en-empty>
+    <van-list
+      v-else
+      v-model="loading"
+      :finished="finished"
+      @load="onLoad"
+      class="goods-container"
+    >
+      <ul>
+        <li v-for="(goods, index) in goodsList" :key="index" class="item-goods">
+          <div class="goods-list-left">
+            <nuxt-link :to="'/goods/' + goods.goods_id">
+              <img :src="goods.thumbnail" :alt="goods.name">
+            </nuxt-link>
+          </div>
+          <div class="goods-list-right">
+            <h3><nuxt-link :to="'/goods/' + goods.goods_id">{{ goods.name }}</nuxt-link></h3>
+            <p>￥{{ goods.price | unitPrice }}</p>
+          </div>
+        </li>
+      </ul>
+    </van-list>
   </div>
 </template>
 
@@ -64,6 +50,7 @@
   import * as API_Shop from '@/api/shop'
   import * as API_Goods from '@/api/goods'
   import { Foundation } from '~/ui-utils'
+  import ShopHeader from '@/pages/shop/-themes/-shop-header'
   export default {
     name: 'shop-goods-list',
     validate({ query }) {
@@ -73,8 +60,11 @@
       const shop = await API_Shop.getShopBaseInfo(query.shop_id)
       return { shop }
     },
+    components: { ShopHeader },
     data() {
       return {
+        loading: false,
+        finished: false,
         sorts: [
           { title: '默认', name: 'def', type: 'asc', active: true },
           { title: '销量', name: 'buynum', type: 'asc', active: false },
@@ -82,21 +72,19 @@
           { title: '评价', name: 'grade', type: 'asc', active: false },
         ],
         shop: '',
-        goods: '',
+        goodsList: [],
         params: {
-          page_no: 1,
-          page_size: 16,
+          page_no: 0,
+          page_size: 10,
+          keyword: '',
           ...this.$route.query
         }
       }
     },
-    mounted() {
-      this.GET_GoodsList()
-    },
     methods: {
-      /** 当前页数发生改变 */
-      handleCurrentChange(page) {
-        this.params.page_no = page
+      /** 加载数据 */
+      onLoad() {
+        this.params.page_no += 1
         this.GET_GoodsList()
       },
       /** 排序发生改变 */
@@ -108,41 +96,42 @@
           item.active = item.name === sort.name
           return item
         }))
+        this.params.page_no = 1
+        this.finished = false
+        this.goodsList = []
         this.params.sort = `${sort.name}_${sort.type}`
         this.GET_GoodsList()
       },
-      /** 价格区间确认 */
-      handlePriceConfirm() {
-        const { min_price, max_price } = this.params
-        if (min_price > max_price) {
-          this.$message.error('价格区间格式有误！')
-          return false
-        }
+      /** 提交表单之拦截 */
+      handleOnSubmit() {
+        delete this.params.category
+        delete this.params.sort
+        this.params.page_no = 1
+        this.finished = false
+        this.goodsList = []
+        if (!this.params.keyword) delete this.params.keyword
         this.GET_GoodsList()
       },
-      /** 商品搜索【店内、全站】 */
-      handleSearch(type) {
-        if (type === 'all') {
-          this.$router.push({ path: '/goods', query: { keyword: this.params.keyword } })
-        } else {
-          this.GET_GoodsList()
-        }
+      /** 搜全站 */
+      handleSearchAll() {
+        const { keyword } = this.params
+        this.$router.push({ name: 'goods', query: { keyword } })
       },
       /** 获取店铺商品列表 */
       GET_GoodsList() {
+        this.loading = true
         const params = JSON.parse(JSON.stringify(this.params))
-        if (params.shop_id) {
-          params.seller_id = params.shop_id
-          delete  params.shop_id
-        }
-        if (params.min_price || params.max_price) {
-          params.price = [params.min_price, params.max_price].join('_')
-          delete params.min_price
-          delete params.max_price
-        }
+        params.seller_id = params.shop_id
+        delete params.shop_id
+        if (!params.keyword) delete params.keyword
         API_Goods.getGoodsList(params).then(response => {
-          this.goods = response
-          this.MixinScrollToTop()
+          this.loading = false
+          const { data } = response
+          if (!data || !data.length) {
+            this.finished = true
+          } else {
+            this.goodsList.push(...data)
+          }
         })
       }
     }
@@ -151,92 +140,113 @@
 
 <style type="text/scss" lang="scss" scoped>
   @import "../../assets/styles/color";
-  img { width: 100%; height: 100% }
-  .container {
-    background-color: #F7F7F7;
+  .search-box {
+    width: 90%;
+    height: 2 * 16px;
+    padding: 8px 5%;
+    background: #fff;
+    input {
+      float: left;
+      width: 55%;
+      border: 1px solid #dedede;
+      height: 1.6 * 16px;
+      line-height: 1.6 * 16px;
+      padding: 1px 5px;
+      box-sizing: border-box;
+    }
+    button {
+      width: 22%;
+      float: left;
+      height: 1.6 * 16px;
+      line-height: 1.6 * 16px;
+      text-align: center;
+      background: #3487ed;
+      color: #fff;
+      font-size: 0.8 * 16px;
+      font-weight: 400;
+      border: 1px solid #3487ed;
+      &:last-child {
+        background: #ff4a00;
+        border: 1px solid #ff4a00;
+      }
+    }
   }
-  .sort-nav {
+  .sort-box {
+    display: flex;
+    justify-content: space-between;
     width: 100%;
-    .inner { border-bottom: 1px solid #D8D8D8 }
-    .left {
-      display: flex;
-      align-items: center;
-      height: 100%
-    }
-    .inner, .inner-sort {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      height: 44px;
-      background-color: #F9FCFA;
-    }
-    .inner-sort .item {
-      display: flex;
-      align-items: center;
-      padding: 0 10px;
-      height: 25px;
-      border-right: 1px solid #ccc;
-      font-size: 14px;
-      cursor: pointer;
-      &.active { color: $color-main }
-      &.down .iconfont { transform: rotate(0) scale(.8) }
-      &:last-child { border-right: 0 }
-      .iconfont {
-        font-size: 12px;
-        margin: 0 5px;
+    overflow: hidden;
+    background-color: #fff;
+    .item-sort {
+      width: 25%;
+      text-align: center;
+      line-height: 25px;
+      color: #53a0be;
+      &.active {
         color: $color-main;
-        transform: rotate(180deg) scale(.8);
       }
-    }
-    .inner-price {
-      margin-left: 35px;
-      /deep/ .el-input-number {
-        width: 80px;
+      &.desc .ea-icon-arrow-down3 {
+        transform: rotate(0);
       }
-      .price-btn { margin-left: 5px }
-    }
-    .inner-search {
-      display: flex;
-      margin-right: 30px;
-      .search-btn { margin-left: 10px }
+      .ea-icon-arrow-down3 {
+        display: inline-block;
+        font-size: 12px;
+        transform: rotate(180deg);
+      }
     }
   }
   .goods-container {
-    .inner {
-      background-color: #fff;
-    }
-  }
-  .goods-pagination {
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    height: 50px;
-  }
-  $s_sep: 30px;
-  $g_width: (1210px - $s_sep * 5) / 4;
-  .goods-list { padding: 10px $s_sep }
-  .goods-item {
-    float: left;
-    width: $g_width;
-    min-height: 300px;
-    margin-right: $s_sep;
-    margin-bottom: 20px;
-    &:nth-child(4n) { margin-right: 0 }
-  }
-  .goods-image {
-    width: $g_width;
-    height: $g_width;
-  }
-  .goods-name {
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
+    padding: 8px 2% 0.8 * 16px 2%;
+    position: relative;
+    border-top: 1px solid #ECECEC;
     overflow: hidden;
-    min-height: 36px;
-  }
-  .goods-price {
-    display: flex;
-    justify-content: space-between;
-    strong { font-size: 16px }
+    margin: 0 0 2.2 * 16px 0;
+    .item-goods {
+      float: left;
+      background: #fff;
+      width: 48%;
+      height: 250px;
+      margin: 0 2% 8px 0;
+      overflow: hidden;
+      position: relative;
+      padding: 0.6 * 16px 0;
+      box-sizing: border-box;
+    }
+    .goods-list-left {
+      width: 90%;
+      margin: 0 auto;
+      max-width: 200px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .goods-list-right {
+      margin: 0 5%;
+      h3 {
+        height: 3 * 16px;
+        line-height: 1.3 * 16px;
+        overflow: hidden;
+        text-align: left;
+        a {
+          font-size: 16px;
+          font-weight: 400;
+          min-height: 32px;
+          color: #53a0be;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+          overflow: hidden;
+        }
+      }
+      p {
+        width: 100%;
+        height: 16px;
+        line-height: 16px;
+        margin: 0.3 * 16px 0;
+        text-align: right;
+        font-size: 16px;
+      }
+    }
   }
 </style>
