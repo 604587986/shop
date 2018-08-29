dfd1636bffed6e08d61879a79ed8b1acf6987ed2
赖云川 <andste.cc@gmail.com>
2018-08-29 11:37:59 +0800

    style(shop)：店铺优惠券适配

--- buyer/wap/themes/default/pages/shop/-themes/-theme1-coupons.vue
+++ buyer/wap/themes/default/pages/shop/-themes/-theme1-coupons.vue
@@ -1,13 +1,11 @@
 <template>
-  <div v-if="coupons && coupons.length" id="theme1-coupons" class="shop-coupons">
-    <div class="shop-coupons-title"></div>
-    <ul class="shop-coupons-list">
-      <li v-for="(coupon, index) in coupons" :key="index" class="coupon-item" @click="handleReceiveCoupon(coupon)">
-        <div class="coupon-price">{{ coupon.coupon_price }}</div>
-        <div class="coupon-name">{{ coupon.title }}</div>
-      </li>
-    </ul>
-  </div>
+  <ul v-if="coupons && coupons.length" class="shop-coupons-list">
+    <li v-for="(coupon, index) in coupons" :key="index" class="coupon-item" @click="handleReceiveCoupon(coupon)">
+      <p><span>￥</span>{{ coupon.coupon_price }}<span>元优惠券</span></p>
+      <b>满{{ coupon.coupon_threshold_price }}元使用</b>
+      <label>点击领取</label>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -19,4 +17,60 @@
 </script>
 
 <style type="text/scss" lang="scss" scoped>
+  .shop-coupons-list {
+    overflow: hidden;
+    margin: 8px 0;
+    background: #fff;
+    padding: 0.5rem;
+    box-shadow: 1px 1px 10px #ccc;
+  }
+  .coupon-item {
+    float: left;
+    background: #fff;
+    width: 47%;
+    margin: 0 2% 8px 0;
+    overflow: hidden;
+    position: relative;
+    padding: 0.6 * 16px 0;
+    border: 1px solid #dedede;
+    height: 4.5 * 16px;
+    text-align: center;
+    p {
+      font-size: 1.2 * 16px;
+      width: 100%;
+      text-align: center;
+      font-weight: 400;
+      span {
+        font-size: 0.8rem;
+        color: #ccc;
+        font-weight: 200;
+        height: 1.5rem;
+        line-height: 1.5rem;
+        &:first-child {
+          margin: 0 0.3 * 16px 0 0;
+        }
+        &:last-child {
+          margin: 0 0 0 0.3 * 16px;
+        }
+      }
+    }
+    b {
+      width: 100%;
+      height: 16px;
+      line-height: 16px;
+      text-align: center;
+      font-size: 0.8 * 16px;
+      color: #ccc;
+      font-weight: 200;
+      display: block;
+      margin: 0.3 * 16px 0;
+    }
+    label {
+      padding: 0 0.3 * 16px;
+      border: 1px solid #dedede;
+      font-size: 0.8 * 16px;
+      color: #ccc;
+      font-weight: 200;
+    }
+  }
 </style>
--- buyer/wap/themes/default/pages/shop/-themes/-theme3-coupons.vue
+++ buyer/wap/themes/default/pages/shop/-themes/-theme3-coupons.vue
@@ -1,25 +1,15 @@
 <template>
-  <div v-if="coupons && coupons.length" id="theme1-coupons" class="shop-coupons">
-    <div class="shop-coupons-title">
-      <h2>领</h2>
-      <h3>
-        <span>券后买更优惠</span>
-        <i>RECEIVE-COUPON</i>
-      </h3>
-    </div>
-    <ul class="shop-coupons-list">
-      <li v-for="(coupon, index) in coupons" :key="index" class="coupon-item" @click="handleReceiveCoupon(coupon)">
-        <h2>{{ coupon.coupon_price }}</h2>
-        <p>{{ coupon.title }}</p>
-        <span>{{ coupon.start_time | unixToDate('yyyy-MM-dd') }} - {{ coupon.end_time | unixToDate('yyyy-MM-dd') }}</span>
-      </li>
-    </ul>
-  </div>
+  <ul v-if="coupons && coupons.length" class="shop-coupons-list">
+    <li v-for="(coupon, index) in coupons" :key="index" class="coupon-item" @click="handleReceiveCoupon(coupon)">
+      <p><span>￥</span>{{ coupon.coupon_price }}<span>元优惠券</span></p>
+      <b>满{{ coupon.coupon_threshold_price }}元使用</b>
+      <label>点击领取</label>
+    </li>
+  </ul>
 </template>
 
 <script>
   import mixin from './themeCouponsMixin'
-
   export default {
     name: 'theme-3-coupons',
     mixins: [mixin]
@@ -27,4 +17,60 @@
 </script>
 
 <style type="text/scss" lang="scss" scoped>
+  .shop-coupons-list {
+    overflow: hidden;
+    margin: 8px 0;
+    background: #fff;
+    padding: 0.5rem;
+    box-shadow: 1px 1px 10px #ccc;
+  }
+  .coupon-item {
+    float: left;
+    background: #fff;
+    width: 47%;
+    margin: 0 2% 8px 0;
+    overflow: hidden;
+    position: relative;
+    padding: 0.6 * 16px 0;
+    border: 1px solid #dedede;
+    height: 4.5 * 16px;
+    text-align: center;
+    p {
+      font-size: 1.2 * 16px;
+      width: 100%;
+      text-align: center;
+      font-weight: 400;
+      span {
+        font-size: 0.8rem;
+        color: #ccc;
+        font-weight: 200;
+        height: 1.5rem;
+        line-height: 1.5rem;
+        &:first-child {
+          margin: 0 0.3 * 16px 0 0;
+        }
+        &:last-child {
+          margin: 0 0 0 0.3 * 16px;
+        }
+      }
+    }
+    b {
+      width: 100%;
+      height: 16px;
+      line-height: 16px;
+      text-align: center;
+      font-size: 0.8 * 16px;
+      color: #ccc;
+      font-weight: 200;
+      display: block;
+      margin: 0.3 * 16px 0;
+    }
+    label {
+      padding: 0 0.3 * 16px;
+      border: 1px solid #dedede;
+      font-size: 0.8 * 16px;
+      color: #ccc;
+      font-weight: 200;
+    }
+  }
 </style>
