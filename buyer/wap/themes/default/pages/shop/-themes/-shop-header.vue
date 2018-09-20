4612fe600e03091abe9522eb2364e9d18c31ab48
赖云川 <andste.cc@gmail.com>
2018-09-20 09:31:16 +0800

    style(shop)：店铺分组修正

--- buyer/wap/themes/default/pages/shop/-themes/-shop-header.vue
+++ buyer/wap/themes/default/pages/shop/-themes/-shop-header.vue
@@ -13,7 +13,7 @@
       <i class="iconfont ea-icon-close" @click="show_cate = false"></i>
       <ul>
         <li v-for="(cate, index) in categories" :key="index">
-          <nuxt-link :to="'/shop/goods-list?shop_id='+ shop.shop_id +'&category='+ cate.shop_cat_id">{{ cate.shop_cat_name }}</nuxt-link>
+          <nuxt-link :to="'/shop/goods-list?shop_id='+ shop.shop_id +'&shop_cat_id='+ cate.shop_cat_id">{{ cate.shop_cat_name }}</nuxt-link>
         </li>
       </ul>
     </div>
