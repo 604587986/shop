939d87544f903b0a164ce3db49f43c8adbad99d5
赖云川 <andste.cc@gmail.com>
2018-09-27 15:46:04 +0800

    style(config): 配置调整

--- manager-admin/config/dev.env.js
+++ manager-admin/config/dev.env.js
@@ -4,7 +4,5 @@ const { distribution } = require('./index')
 module.exports = {
 	NODE_ENV: '"development"',
 	ENV_CONFIG: '"dev"',
-  BASE_API: `"${api_dev.base}"`,
-  ADMIN_API: `"${api_dev.admin}"`,
   DISTRIBUTION: distribution
 }
