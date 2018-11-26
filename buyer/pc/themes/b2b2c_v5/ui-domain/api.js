47916442017bdfeee14867ff029997cc307103de
wangyong <604587986@qq.com>
2018-11-26 20:06:31 +0800

    webpack 生产模式rule

---
+++ buyer/pc/themes/b2b2c_v5/ui-domain/api.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Andste on 2018/7/2.
+ * base    : 基础业务API
+ * buyer   : 买家API
+ * seller  : 商家中心API
+ * admin   ：后台管理API
+ */
+
+module.exports = {
+  // 开发环境
+  dev: {
+    base  : 'https://api-base.taofree.com',
+    buyer : 'https://api-buyer.taofree.com',
+    seller: 'https://api-seller.taofree.com',
+    admin : 'https://api-admin.taofree.com'
+  },
+  // 生产环境
+  pro: {
+    base  : 'https://api-base.taofree.com',
+    buyer : 'https://api-buyer.taofree.com',
+    seller: 'https://api-seller.taofree.com',
+    admin : 'https://api-admin.taofree.com'
+  }
+}
+
---
+++ buyer/pc/themes/b2b2c_v5/ui-domain/domain.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Andste on 2018/7/2.
+ * buyer_pc : 买家PC端
+ * buyer_wap: 买家WAP端
+ * seller   : 商家中心
+ * admin    : 后台管理
+ */
+
+module.exports = {
+  // 开发环境
+  dev: {
+    buyer_pc : 'https://buyer.taofree.com',
+    buyer_wap: 'https://wap.taofree.com',
+    seller   : 'https://seller.taofree.com',
+    admin    : 'https://admin.taofree.com'
+  },
+  // 生产环境
+  pro: {
+    buyer_pc : 'https://buyer.taofree.com',
+    buyer_wap: 'https://wap.taofree.com',
+    seller   : 'https://seller.taofree.com',
+    admin    : 'https://admin.taofree.com'
+  }
+}
