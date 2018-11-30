db7503f045c18569604396d7b65c4ea430e8d5cb
赖云川 <andste.cc@gmail.com>
2018-11-30 17:33:00 +0800

    feat(goods): 添加规格参数无数据占位

--- buyer/pc/themes/b2b2c_v5/pages/goods/-goods-params.vue
+++ buyer/pc/themes/b2b2c_v5/pages/goods/-goods-params.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="goods-params" class="goods-params">
-    <table v-for="params in goodsParams" :key="params.group_id" class="params-table">
+    <div class="empty-params" v-if="!goodsParams || !goodsParams.length">
+      <img alt="" src="../../assets/images/icon-empty-member.png">
+      <p>暂无规格参数</p>
+    </div>
+    <table :key="params.group_id" class="params-table" v-else v-for="params in goodsParams">
       <thead>
       <tr><th colspan="2" style="background-color: #f0f0f0">{{ params.group_name }}</th></tr>
       </thead>
@@ -39,5 +43,10 @@
       }
       td:first-child { width: 150px }
     }
+  }
+  .empty-params {
+    display: block;
+    text-align: center;
+    margin-top: 50px;
   }
 </style>
