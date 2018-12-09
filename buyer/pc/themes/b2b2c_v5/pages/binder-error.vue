7b4618e85f4a66775c6c615ed084c5616cf0f0d1
Andste <andste.cc@gmail.com>
2018-12-09 21:55:06 +0800

    feat(connect): binder-error页增加消息提示

--- buyer/pc/themes/b2b2c_v5/pages/binder-error.vue
+++ buyer/pc/themes/b2b2c_v5/pages/binder-error.vue
@@ -1,13 +1,36 @@
 <template>
-  <div id="binder-error"/>
+  <div id="binder-error" class="binder-error w">
+    <h1>{{ message }}</h1>
+    <div class="btns">
+      <el-button type="danger" size="small" @click="$router.back()">返回上页</el-button>
+      <el-button type="danger" size="small" @click="$router.push({ path: '/' })">去首页</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'binder-error'
-}
+  export default {
+    name: 'binder-error',
+    data() {
+      return {
+        message: this.$route.query.message
+      }
+    }
+  }
 </script>
 
 <style type="text/scss" lang="scss" scoped>
-
+  .binder-error {
+    h1 {
+      font-size: 20px;
+      line-height: 200px;
+      text-align: center;
+    }
+    .btns {
+      text-align: center;
+      button {
+        width: 80px;
+      }
+    }
+  }
 </style>
