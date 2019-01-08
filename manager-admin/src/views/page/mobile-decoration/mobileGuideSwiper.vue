00383cec3d16d14eb74f289924ecb6d2811f1d7d
wangyong <604587986@qq.com>
2019-01-08 14:58:48 +0800

    引导图可以上传视频

--- manager-admin/src/views/page/mobile-decoration/mobileGuideSwiper.vue
+++ manager-admin/src/views/page/mobile-decoration/mobileGuideSwiper.vue
@@ -1,38 +1,37 @@
 <template>
-  <div class="container">
-    <div class="swiper-box">
-      <swiper :options="swiperOption">
-        <swiper-slide v-for="(focus, index) in focusList" :key="index">
-          <div class="focus-item">
-            <img :src="focus.pic_url" class="focus-image">
-            <div class="opt-box">
-              <i class="opt-btn el-icon-edit-outline" title="编辑" @click="handleEditFocus(focus, index)"></i>
-              <i class="opt-btn el-icon-delete" title="删除" @click="handleDeleteFocus(focus, index)"></i>
-            </div>
-          </div>
-        </swiper-slide>
-        <div class="swiper-pagination swiper-pagination-white" slot="pagination"></div>
-        <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
-        <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
-      </swiper>
-      <el-button type="primary" class="add-focus-btn" @click="hanldeAddFocus">新增引导图</el-button>
-    </div>
-    <en-image-picker
-      :show="dialogImageShow"
-      :default-data="defaultImageData"
-      @close="dialogImageShow = false"
-      @confirm="handleImagePickerConfirm"
-      :limit="1"
-    />
-  </div>
+	<div class="container">
+		<div class="swiper-box">
+			<swiper :options="swiperOption">
+				<swiper-slide v-for="(focus, index) in focusList" :key="index">
+					<div class="focus-item">
+						<img v-if="isImg(focus.pic_url)" :src="focus.pic_url" class="focus-image">
+						<video v-else :src="focus.pic_url" controls class="focus-image"></video>
+						<div class="opt-box">
+							<i class="opt-btn el-icon-edit-outline" title="编辑" @click="handleEditFocus(focus, index)"></i>
+							<i class="opt-btn el-icon-delete" title="删除" @click="handleDeleteFocus(focus, index)"></i>
+						</div>
+					</div>
+				</swiper-slide>
+				<div class="swiper-pagination swiper-pagination-white" slot="pagination"></div>
+				<div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+				<div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+			</swiper>
+			<el-button type="primary" class="add-focus-btn" @click="hanldeAddFocus">新增引导图</el-button>
+		</div>
+		<file-picker :show="dialogImageShow" :default-data="defaultImageData" @close="dialogImageShow = false" @confirm="handleImagePickerConfirm" :limit="1"/>
+	</div>
 </template>
 
 <script>
 /* eslint-disable */
   import * as API_Floor from '@/api/floor'
+  import FilePicker from '@/components/filePicker'
 
   export default {
     name: 'mobileGuideSwiper',
+    components:{
+      FilePicker
+    },
     data() {
       return {
         client_type: 'GUIDE',
@@ -68,6 +67,7 @@
         this.defaultImageData = [{
           id: focus.id,
           url: focus.pic_url,
+          name:focus.pic_url,
           opt: {
             opt_type: focus.operation_type,
             opt_value: focus.operation_param
@@ -113,71 +113,100 @@
           this.loading = false
           this.focusList = response
         }).catch(() => { this.loading = false })
+      },
+      //判断文件后缀是否是图片
+      isImg(upFileName){
+        upFileName?upFileName.toLowerCase():"";
+
+        var index1=upFileName.lastIndexOf(".");
+        var index2=upFileName.length;
+        var suffix=upFileName.substring(index1+1,index2);//后缀名
+        if(suffix === '' || suffix === 'jpg' || suffix === 'jpeg' || suffix === 'png' || suffix === 'gif' || suffix === 'bmp'){
+          return true;
+        }else{
+          return false;
+        }
       }
     }
   }
 </script>
 
 <style type="text/scss" lang="scss" scoped>
-  .container {
-    background-color: #fff;
-    padding: 10px;
+.container {
+	background-color: #fff;
+	padding: 10px;
 
-    .swiper-box {
-      position: relative;
-      margin: 100px auto;
-      text-align: center;
-      width: 375px;
-      height: 667px;
-      background-color: #fff;
-      box-shadow: 0 0 15px 1px #ccc;
+	.swiper-box {
+		position: relative;
+		margin: 100px auto;
+		text-align: center;
+		width: 375px;
+		height: 667px;
+		background-color: #fff;
+		box-shadow: 0 0 15px 1px #ccc;
+    .swiper-button-prev{
+      top: 10%;
     }
-    .swiper-container {
-      width: 100%;
-      height: 100%;
+    .swiper-button-next{
+      top: 10%;      
     }
-    .focus-item { position: relative }
-    .focus-item, .focus-image {
-      width: 100%;
-      height: 100%;
-    }
-    .opt-box {
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: 2;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0,0,0,0);
-    }
-    .opt-box, .opt-btn {
-      transition: all .3s ease;
-    }
-    .opt-btn {
-      float: left;
-      display: block;
-      position: relative;
-      width: 50%;
-      height: 100%;
-      opacity: 0;
-      color: #fff;
-      font-size: 25px;
-      line-height: 155px * 1.5;
-    }
-    .focus-item:hover {
-      .opt-box { background-color: rgba(0,0,0,.3) }
-      .opt-btn { opacity: 1 }
-    }
-    .opt-btn:hover {
-      background-color: rgba(0,0,0,.5);
-      &.el-icon-delete { background-color: rgba(255, 0, 0, .8) }
-      cursor: pointer;
-    }
-    .add-focus-btn {
-      position: absolute;
-      left: 50%;
-      bottom: -50px;
-      margin-left: -(92px / 2);
-    }
-  }
+	}
+	.swiper-container {
+		width: 100%;
+		height: 100%;
+	}
+	.focus-item {
+		position: relative;
+	}
+	.focus-item,
+	.focus-image {
+		width: 100%;
+		height: 100%;
+	}
+	.opt-box {
+		position: absolute;
+		top: 0;
+    bottom: 80%;
+		left: 0;
+		z-index: 2;
+		width: 100%;
+		background-color: rgba(0, 0, 0, 0);
+	}
+	.opt-box,
+	.opt-btn {
+		transition: all 0.3s ease;
+	}
+	.opt-btn {
+		float: left;
+		display: block;
+		position: relative;
+		width: 50%;
+		height: 100%;
+		opacity: 0;
+		color: #fff;
+		font-size: 25px;
+		line-height: 80px * 1.5;
+	}
+	.focus-item:hover {
+		.opt-box {
+			background-color: rgba(0, 0, 0, 0.3);
+		}
+		.opt-btn {
+			opacity: 1;
+		}
+	}
+	.opt-btn:hover {
+		background-color: rgba(0, 0, 0, 0.5);
+		&.el-icon-delete {
+			background-color: rgba(255, 0, 0, 0.8);
+		}
+		cursor: pointer;
+	}
+	.add-focus-btn {
+		position: absolute;
+		left: 50%;
+		bottom: -50px;
+		margin-left: -(92px / 2);
+	}
+}
 </style>
