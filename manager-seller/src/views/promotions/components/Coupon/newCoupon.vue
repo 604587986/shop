1248dbdec39d80f3d1aa480a2ed1daa5f23c0a8e
wangyong <604587986@qq.com>
2019-01-09 15:14:21 +0800

    优惠券增加类型

--- manager-seller/src/views/promotions/components/Coupon/newCoupon.vue
+++ manager-seller/src/views/promotions/components/Coupon/newCoupon.vue
@@ -1,5 +1,5 @@
 <template>
-	<el-dialog title="新增优惠券" :visible.sync="couponShow" width="30%">
+	<el-dialog title="新增优惠券" :visible.sync="couponShow" width="30%" @close="closeModal">
 		<el-form :model="couponForm" ref="couponForm" label-position="right" :rules="rules" label-width="120px">
 			<el-form-item label="优惠券名称：" prop="title">
 				<el-input
@@ -39,20 +39,79 @@
 			<el-form-item label="每人限领：" prop="limit_num">
 				<el-input v-model="couponForm.limit_num" maxlength="10" label-width="100"></el-input>
 			</el-form-item>
+			<el-form-item label="商品：" prop>
+				<el-button type="primary" @click="innerVisible = true">选择商品</el-button>
+			</el-form-item>
+			<el-form-item label="已选商品：" prop>
+				<div v-for="item in currentSelectedGoods" :key="item.goods_id" class="selected-goods-list">
+					{{item.goods_name}}
+					<i class="el-icon-close selected-goods-item" @click="delGoods(item.goods_id)"></i>
+				</div>
+			</el-form-item>
 		</el-form>
 		<div slot="footer" class="dialog-footer">
 			<el-button @click="handleCancelCoupon">取 消</el-button>
 			<el-button type="primary" @click="handleReserveCoupon">确 定</el-button>
 		</div>
+		<el-dialog width="60%" title="选择商品" :visible.sync="innerVisible" append-to-body>
+			<div>
+				<el-input placeholder="请填写商品名称" v-model="params.keyword" @keyup.native.enter="search">
+					<el-button slot="append" icon="el-icon-search" @click="search"></el-button>
+				</el-input>
+			</div>
+			<el-table :data="tableData" @row-click="selectGoods" class="my-goods-table" :row-class-name="tableRowClassName" v-loading="loading">
+				<el-table-column label="选择" width="100">
+					<template slot-scope="scope">
+						<i class="el-icon-check" v-if="isSelected(scope.row.goods_id)"></i>
+					</template>
+				</el-table-column>
+				<el-table-column label="图片" width="120">
+					<template slot-scope="scope">
+						<img :src="scope.row.thumbnail" class="goods-image">
+					</template>
+				</el-table-column>
+				<el-table-column label="商品名称">
+					<template slot-scope="scope">
+						<a href="javascript:;" target="_blank" style="color: #00a2d4;">{{ scope.row.goods_name }}</a>
+					</template>
+				</el-table-column>
+				<el-table-column label="价格" width="100">
+					<template slot-scope="scope">{{ scope.row.price | unitPrice('￥') }}</template>
+				</el-table-column>
+				<el-table-column label="库存" width="80">
+					<template slot-scope="scope">{{ scope.row.quantity }}件</template>
+				</el-table-column>
+				<el-table-column label="可用库存" width="80">
+					<template slot-scope="scope">{{ scope.row.enable_quantity }}件</template>
+				</el-table-column>
+				<el-table-column label="创建时间" width="120">
+					<template slot-scope="scope">{{ scope.row.create_time | unixToDate('yyyy-MM-dd hh:mm') }}</template>
+				</el-table-column>
+			</el-table>
+			<el-pagination
+				@size-change="handlePageSizeChange"
+				@current-change="handlePageCurrentChange"
+				:current-page="pageData.page_no"
+				:page-sizes="[10, 20, 50, 100]"
+				:page-size="pageData.page_size"
+				layout="total, sizes, prev, pager, next, jumper"
+				:total="pageData.data_total"
+			></el-pagination>
+			<div style="text-align:right;margin-top:20px">
+				<el-button type="primary" @click="innerVisible = false">确定</el-button>
+			</div>
+		</el-dialog>
 	</el-dialog>
 </template>
 
 <script>
 /* eslint-disable */
 import * as API_coupon from "@/api/coupon";
+import * as API_goods from "@/api/goods";
+
 import { RegExp } from "~/ui-utils";
 export default {
-	name: "Coupon",
+	name: "newCoupon",
 	props: {
 		/** 是否显示优惠券弹框 */
 		couponModelShow: {
@@ -69,39 +128,41 @@ export default {
 		}
 	},
 	watch: {
-		couponModelShow() {
-			this.couponShow = this.couponModelShow;
-			this.$nextTick(() => {
-				this.$refs["couponForm"].resetFields();
-			});
+		couponModelShow: {
+			handler: function(val) {
+				this.couponShow = val;
+			}
 		},
 		couponShow() {
 			!this.couponShow && this.$emit("saveCoupon", false);
 		},
-		currentcouponId() {
-			if (this.currentcouponId) {
-				this.GET_couponDetails();
-			} else {
-				this.couponForm = {
-					/** 优惠券名称*/
-					title: "",
+		currentcouponId: {
+			handler: function() {
+				if (this.currentcouponId) {
+					this.GET_couponDetails();
+				} else {
+					this.couponForm = {
+						/** 优惠券名称*/
+						title: "",
 
-					/** 优惠券面额（元）*/
-					coupon_price: "",
+						/** 优惠券面额（元）*/
+						coupon_price: "",
 
-					/** 使用限制（元）*/
-					coupon_threshold_price: "",
+						/** 使用限制（元）*/
+						coupon_threshold_price: "",
 
-					/** 使用时限 */
-					coupon_time_limit: [],
+						/** 使用时限 */
+						coupon_time_limit: [],
 
-					/** 发行量（个） */
-					create_num: "",
+						/** 发行量（个） */
+						create_num: "",
 
-					/** 每人限领 */
-					limit_num: ""
-				};
-			}
+						/** 每人限领 */
+						limit_num: ""
+					};
+				}
+			},
+			immediate: true
 		}
 	},
 	mounted() {
@@ -128,6 +189,8 @@ export default {
 				limit_num: ""
 			};
 		}
+
+		this.GET_GoodsList();
 	},
 	data() {
 		const checkCouponPrice = (rule, value, callback) => {
@@ -180,6 +243,28 @@ export default {
 		};
 
 		return {
+			// 已选择的商品
+			currentSelectedGoods: [],
+			/** 表格数据 */
+			tableData: [],
+			/** 表格loading */
+
+			loading: false,
+			/** 列表分页数据 */
+
+			pageData: [],
+			/** 列表参数 */
+			params: {
+				page_no: 1,
+				page_size: 10,
+				is_services: 1,
+				//关键字
+				keywrod: ""
+			},
+
+			/** 是否显示选择商品弹框 */
+			innerVisible: false,
+
 			/** 是否显示优惠券弹框 */
 			couponShow: false,
 
@@ -286,6 +371,9 @@ export default {
 							parseInt(response.end_time) * 1000
 						]
 					};
+					this.couponForm.goods
+						? (this.currentSelectedGoods = this.couponForm.goods)
+						: [];
 				}
 			);
 		},
@@ -301,9 +389,14 @@ export default {
 				if (valid) {
 					const _params = {
 						...this.couponForm,
-						type_code: 0,
+						type_code: 1,
 						start_time: this.couponForm.coupon_time_limit[0] / 1000,
-						end_time: this.couponForm.coupon_time_limit[1] / 1000
+						end_time: this.couponForm.coupon_time_limit[1] / 1000,
+						goods_ids: this.currentSelectedGoods
+							.map(i => {
+								return i.goods_id;
+							})
+							.join()
 					};
 					delete _params.coupon_time_limit;
 					this.$confirm(
@@ -330,10 +423,76 @@ export default {
 							}
 						})
 						.catch(() => {});
-				}else {
+				} else {
 					return false;
 				}
 			});
+		},
+		/** 获取商品列表*/
+		GET_GoodsList() {
+			this.loading = true;
+			API_goods.getGoodsList(this.params).then(response => {
+				this.loading = false;
+				this.pageData = {
+					page_no: response.page_no,
+					page_size: response.page_size,
+					data_total: response.data_total
+				};
+				this.tableData = response.data;
+			});
+		},
+		/** 分页大小发生改变 */
+		handlePageSizeChange(size) {
+			this.params.page_size = size;
+			this.GET_GoodsList();
+		},
+		/** 分页页数发生改变 */
+		handlePageCurrentChange(page) {
+			this.params.page_no = page;
+			this.GET_GoodsList();
+		},
+		/** 选择商品 */
+		selectGoods(row, event, column) {
+			if (!this.isSelected(row.goods_id)) {
+				this.currentSelectedGoods.push(row);
+			} else {
+				this.delGoods(row.goods_id);
+			}
+		},
+		/**判断是否选择 */
+		isSelected(id) {
+			if (this.currentSelectedGoods.length) {
+				for (let i in this.currentSelectedGoods) {
+					if (this.currentSelectedGoods[i].goods_id === id) {
+						return true;
+					}
+				}
+
+				return false;
+			}
+		},
+		/**删除列表中的商品 */
+		delGoods(id) {
+			for (let i in this.currentSelectedGoods) {
+				if (this.currentSelectedGoods[i].goods_id === id) {
+					this.currentSelectedGoods.splice(i, 1);
+					return;
+				}
+			}
+		},
+		tableRowClassName({ row, rowIndex }) {
+			if (this.isSelected(row.goods_id)) {
+				return "selected-row";
+			}
+			return "";
+		},
+		closeModal() {
+			this.$refs["couponForm"].resetFields();
+			this.currentSelectedGoods = [];
+		},
+		search() {
+			this.params.page_no = 1;
+			this.GET_GoodsList();
 		}
 	}
 };
@@ -342,5 +501,38 @@ export default {
 <style lang="scss" type="scss" scoped>
 .el-date-editor {
 	width: 100%;
+}
+.selected-goods-list {
+	position: relative;
+	padding-right: 25px;
+	.selected-goods-item {
+		position: absolute;
+		right: 10px;
+		top: 10px;
+		cursor: pointer;
+		&:hover {
+			color: #f56c6c;
+		}
+	}
+}
+</style>
+<style lang="scss">
+.my-goods-table {
+	.selected-row {
+		background: #67c23a;
+		color: #fff;
+		.el-icon-check {
+			font-size: 24px;
+			// color: #67c23a;
+			font-weight: bold;
+		}
+		&:hover {
+			color: inherit;
+		}
+	}
+
+	.goods-image {
+		width: 100%;
+	}
 }
 </style>
--- manager-seller/src/views/promotions/discountManager.vue
+++ manager-seller/src/views/promotions/discountManager.vue
@@ -1,271 +1,284 @@
 <template>
-  <div>
-    <en-table-layout
-      toolbar
-      pagination
-      :tableData="tableData"
-      :loading="loading">
-      <div slot="toolbar" class="inner-toolbar">
-        <div class="toolbar-btns">
-          <el-button type="primary" @click="handleAddCoupon">新增</el-button>
-        </div>
-        <div class="toolbar-search">
-          <en-table-search
-            @search="searchEvent"
-            @advancedSearch="advancedSearchEvent"
-            advancedWidth="465"
-            advanced
-            placeholder="请输入关键字">
-            <template slot="advanced-content">
-              <el-form ref="advancedForm" :model="advancedForm" label-width="80px">
-                <el-form-item label="有效期">
-                  <el-date-picker
-                    v-model="advancedForm.coupon_time_limit"
-                    type="daterange"
-                    align="center"
-                    range-separator="-"
-                    value-format="timestamp"
-                    start-placeholder="开始日期"
-                    end-placeholder="结束日期">
-                  </el-date-picker>
-                </el-form-item>
-              </el-form>
-            </template>
-          </en-table-search>
-        </div>
-      </div>
-      <template slot="table-columns">
-        <el-table-column prop="title" label="优惠券名称" />
-        <el-table-column prop="coupon_price" label="优惠券面额（元）" width="120" />
-        <el-table-column prop="coupon_threshold_price" label="使用限制(元)" width="120" />
-        <el-table-column label="使用期限" width="240">
-          <template slot-scope="scope">
-            {{ scope.row.start_time | unixToDate('yyyy-MM-dd') }}~
-            {{ scope.row.end_time | unixToDate('yyyy-MM-dd') }}
-          </template>
-        </el-table-column>
-        <el-table-column prop="create_num" label="发行量（个）" width="120" />
-        <el-table-column prop="used_num" label="已使用量（个）" width="120"/>
-        <el-table-column label="操作" width="180">
-          <template slot-scope="scope">
-            <el-button
-              type="success"
-              @click="handleEditCoupons(scope.row)">修改
-            </el-button>
-            <el-button
-              type="danger"
-              @click="handleDeleteGoods(scope.row)">删除
-            </el-button>
-          </template>
-        </el-table-column>
-      </template>
-      <el-pagination
-        slot="pagination"
-        v-if="pageData"
-        @size-change="handlePageSizeChange"
-        @current-change="handlePageCurrentChange"
-        :current-page="pageData.page_no"
-        :page-sizes="[10, 20, 50, 100]"
-        :page-size="pageData.page_size"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="pageData.data_total">
-      </el-pagination>
-    </en-table-layout>
-    <!--优惠券组件-->
-    <Coupon
-      :couponModelShow="couponModelShow"
-      :currentcouponId="currentcouponId"
-      @saveCoupon="saveCoupon"
-    ></Coupon>
-  </div>
+	<div>
+		<en-table-layout toolbar pagination :tableData="tableData" :loading="loading">
+			<div slot="toolbar" class="inner-toolbar">
+				<div class="toolbar-btns">
+					<el-button type="primary" @click="handleAddCoupon">新增全品类优惠券</el-button>
+					<el-button type="primary" @click="handleAddNewCoupon">新增单品类优惠券</el-button>
+				</div>
+				<div class="toolbar-search">
+					<en-table-search @search="searchEvent" @advancedSearch="advancedSearchEvent" advancedWidth="465" advanced placeholder="请输入关键字">
+						<template slot="advanced-content">
+							<el-form ref="advancedForm" :model="advancedForm" label-width="80px">
+								<el-form-item label="有效期">
+									<el-date-picker
+										v-model="advancedForm.coupon_time_limit"
+										type="daterange"
+										align="center"
+										range-separator="-"
+										value-format="timestamp"
+										start-placeholder="开始日期"
+										end-placeholder="结束日期"
+									></el-date-picker>
+								</el-form-item>
+							</el-form>
+						</template>
+					</en-table-search>
+				</div>
+			</div>
+			<template slot="table-columns">
+				<el-table-column prop="title" label="优惠券名称"/>
+				<el-table-column label="优惠券类型">
+					<template slot-scope="scope">
+						{{scope.row.type_code === 1?"单品类优惠券":"全品类优惠券"}}
+					</template>
+				</el-table-column>
+				<el-table-column prop="coupon_price" label="优惠券面额（元）" width="120"/>
+				<el-table-column prop="coupon_threshold_price" label="使用限制(元)" width="120"/>
+				<el-table-column label="使用期限" width="240">
+					<template slot-scope="scope">
+						{{ scope.row.start_time | unixToDate('yyyy-MM-dd') }}~
+						{{ scope.row.end_time | unixToDate('yyyy-MM-dd') }}
+					</template>
+				</el-table-column>
+				<el-table-column prop="create_num" label="发行量（个）" width="120"/>
+				<el-table-column prop="used_num" label="已使用量（个）" width="120"/>
+				<el-table-column label="操作" width="180">
+					<template slot-scope="scope">
+						<el-button type="success" @click="handleEditCoupons(scope.row)">修改</el-button>
+						<el-button type="danger" @click="handleDeleteGoods(scope.row)">删除</el-button>
+					</template>
+				</el-table-column>
+			</template>
+			<el-pagination
+				slot="pagination"
+				v-if="pageData"
+				@size-change="handlePageSizeChange"
+				@current-change="handlePageCurrentChange"
+				:current-page="pageData.page_no"
+				:page-sizes="[10, 20, 50, 100]"
+				:page-size="pageData.page_size"
+				layout="total, sizes, prev, pager, next, jumper"
+				:total="pageData.data_total"
+			></el-pagination>
+		</en-table-layout>
+		<!--优惠券组件-->
+		<Coupon :couponModelShow="couponModelShow" :currentcouponId="currentcouponId" @saveCoupon="saveCoupon"></Coupon>
+		<!--新增的针对商品的优惠券组件-->
+		<new-Coupon :couponModelShow="newCouponModelShow" :currentcouponId="currentcouponId" @saveCoupon="saveNewCoupon"></new-Coupon>
+	</div>
 </template>
 
 <script>
-  import * as API_coupon from '@/api/coupon'
-  import { CategoryPicker } from '@/components'
-  import { Coupon } from './components'
-  export default {
-    name: 'discountManager',
-    components: {
-      [CategoryPicker.name]: CategoryPicker,
-      [Coupon.name]: Coupon
-    },
-    data() {
-      return {
-        /** 列表loading状态 */
-        loading: false,
+/* eslint-disable */
+import * as API_coupon from "@/api/coupon";
+import { CategoryPicker } from "@/components";
+import { Coupon } from "./components";
+import NewCoupon from "./components/Coupon/newCoupon";
+export default {
+	name: "discountManager",
+	components: {
+		[CategoryPicker.name]: CategoryPicker,
+		[Coupon.name]: Coupon,
+		NewCoupon
+	},
+	data() {
+		return {
+			/** 列表loading状态 */
+			loading: false,
 
-        /** 列表参数 */
-        params: {
-          page_no: 1,
-          page_size: 10
-        },
+			/** 列表参数 */
+			params: {
+				page_no: 1,
+				page_size: 10
+			},
 
-        /** 列表数据 */
-        tableData: null,
+			/** 列表数据 */
+			tableData: null,
 
-        /** 列表分页数据 */
-        pageData: null,
+			/** 列表分页数据 */
+			pageData: null,
 
-        /** 高级搜索数据 */
-        advancedForm: {
-          coupon_time_limit: []
-        },
+			/** 高级搜索数据 */
+			advancedForm: {
+				coupon_time_limit: []
+			},
 
-        /** 优惠券弹框显示*/
-        couponModelShow: false,
+			/** 优惠券弹框显示*/
+			couponModelShow: false,
 
-        /** 当前优惠券ID*/
-        currentcouponId: '',
+			/** 针对商品类优惠券弹框显示*/
+			newCouponModelShow: false,
 
-        /** 优惠券表单*/
-        couponModelForm: {
-          /** 优惠券名称*/
-          title: '',
+			/** 当前优惠券ID*/
+			currentcouponId: "",
 
-          /** 优惠券面额（元）*/
-          coupon_price: 2,
+			/** 优惠券表单*/
+			couponModelForm: {
+				/** 优惠券名称*/
+				title: "",
 
-          /** 使用限制（元）*/
-          coupon_threshold_price: 2,
+				/** 优惠券面额（元）*/
+				coupon_price: 2,
 
-          /** 使用时限 */
-          coupon_time_limit: '2017-02-25',
+				/** 使用限制（元）*/
+				coupon_threshold_price: 2,
 
-          /** 发行量（个） */
-          create_num: 2,
+				/** 使用时限 */
+				coupon_time_limit: "2017-02-25",
 
-          /** 已使用量（个） */
-          used_num: 2
-        }
-      }
-    },
-    mounted() {
-      this.GET_CouponsList()
-    },
-    methods: {
+				/** 发行量（个） */
+				create_num: 2,
 
-      /** 分页大小发生改变 */
-      handlePageSizeChange(size) {
-        this.params.page_size = size
-        this.GET_CouponsList()
-      },
+				/** 已使用量（个） */
+				used_num: 2
+			}
+		};
+	},
+	mounted() {
+		this.GET_CouponsList();
+	},
+	methods: {
+		/** 分页大小发生改变 */
+		handlePageSizeChange(size) {
+			this.params.page_size = size;
+			this.GET_CouponsList();
+		},
 
-      /** 分页页数发生改变 */
-      handlePageCurrentChange(page) {
-        this.params.page_no = page
-        this.GET_CouponsList()
-      },
+		/** 分页页数发生改变 */
+		handlePageCurrentChange(page) {
+			this.params.page_no = page;
+			this.GET_CouponsList();
+		},
 
-      /** 搜索事件触发 */
-      searchEvent(data) {
-        this.params = {
-          ...this.params,
-          keyword: data
-        }
-        Object.keys(this.advancedForm).forEach(key => delete this.params[key])
-        delete this.params.start_time
-        delete this.params.end_time
-        this.GET_CouponsList()
-      },
+		/** 搜索事件触发 */
+		searchEvent(data) {
+			this.params = {
+				...this.params,
+				keyword: data
+			};
+			Object.keys(this.advancedForm).forEach(
+				key => delete this.params[key]
+			);
+			delete this.params.start_time;
+			delete this.params.end_time;
+			this.GET_CouponsList();
+		},
 
-      /** 高级搜索事件触发 */
-      advancedSearchEvent() {
-        this.params = this.MixinClone(this.params)
-        const { coupon_time_limit } = this.advancedForm
-        if (coupon_time_limit && coupon_time_limit[0]) {
-          this.params.start_time = coupon_time_limit[0] / 1000
-          this.params.end_time = coupon_time_limit[1] / 1000
-        } else {
-          delete this.params.start_time
-          delete this.params.end_time
-        }
-        delete this.params.keyword
-        this.GET_CouponsList()
-      },
+		/** 高级搜索事件触发 */
+		advancedSearchEvent() {
+			this.params = this.MixinClone(this.params);
+			const { coupon_time_limit } = this.advancedForm;
+			if (coupon_time_limit && coupon_time_limit[0]) {
+				this.params.start_time = coupon_time_limit[0] / 1000;
+				this.params.end_time = coupon_time_limit[1] / 1000;
+			} else {
+				delete this.params.start_time;
+				delete this.params.end_time;
+			}
+			delete this.params.keyword;
+			this.GET_CouponsList();
+		},
 
-      GET_CouponsList() {
-        this.loading = true
-        API_coupon.getCouponsList(this.params).then(response => {
-          this.loading = false
-          this.pageData = {
-            page_no: response.page_no,
-            page_size: response.page_size,
-            data_total: response.data_total
-          }
-          this.tableData = response.data
-          /** 已经使用量 */
-          this.tableData.forEach(key => {
-            key.used_num = key.used_num || 0
-          })
-        })
-      },
+		GET_CouponsList() {
+			this.loading = true;
+			API_coupon.getCouponsList(this.params).then(response => {
+				this.loading = false;
+				this.pageData = {
+					page_no: response.page_no,
+					page_size: response.page_size,
+					data_total: response.data_total
+				};
+				this.tableData = response.data;
+				/** 已经使用量 */
+				this.tableData.forEach(key => {
+					key.used_num = key.used_num || 0;
+				});
+			});
+		},
 
-      /** 删除优惠券 */
-      handleDeleteGoods(row) {
-        this.$confirm('确认删除此优惠券, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          API_coupon.deleteCoupons(row.coupon_id, row).then(() => {
-            this.GET_CouponsList()
-            this.$message.success('删除优惠券成功！')
-          })
-        }).catch(() => {
-          this.$message.info({ message: '已取消删除' })
-        })
-      },
+		/** 删除优惠券 */
+		handleDeleteGoods(row) {
+			this.$confirm("确认删除此优惠券, 是否继续?", "提示", {
+				confirmButtonText: "确定",
+				cancelButtonText: "取消",
+				type: "warning"
+			})
+				.then(() => {
+					API_coupon.deleteCoupons(row.coupon_id, row).then(() => {
+						this.GET_CouponsList();
+						this.$message.success("删除优惠券成功！");
+					});
+				})
+				.catch(() => {
+					this.$message.info({ message: "已取消删除" });
+				});
+		},
 
-      /** 修改*/
-      handleEditCoupons(row) {
-        this.couponModelShow = true
-        this.currentcouponId = row.coupon_id
-      },
+		/** 修改*/
+		handleEditCoupons(row) {
+			if (row.type_code === 1) {
+				this.newCouponModelShow = true;
+				this.currentcouponId = row.coupon_id;
+			} else {
+				this.couponModelShow = true;
+				this.currentcouponId = row.coupon_id;
+			}
+		},
 
-      /** 新增优惠券*/
-      handleAddCoupon() {
-        this.couponModelShow = true
-        this.currentcouponId = ''
-      },
+		/** 新增优惠券*/
+		handleAddCoupon() {
+			this.couponModelShow = true;
+			this.currentcouponId = "";
+		},
 
-      /** 优惠券监听 */
-      saveCoupon(target) {
-        this.couponModelShow = false
-        target && this.GET_CouponsList()
-      }
-    }
-  }
+		/** 新增单品类优惠券*/
+		handleAddNewCoupon() {
+			this.newCouponModelShow = true;
+			this.currentcouponId = "";
+		},
+
+		/** 优惠券监听 */
+		saveCoupon(target) {
+			this.currentcouponId = "";
+			this.couponModelShow = false;
+			target && this.GET_CouponsList();
+		},
+		/** 单品类优惠券监听 */
+		saveNewCoupon(target) {
+			this.currentcouponId = "";
+			this.newCouponModelShow = false;
+			target && this.GET_CouponsList();
+		}
+	}
+};
 </script>
 
 <style type="text/scss" lang="scss" scoped>
+/deep/ div.toolbar {
+	height: 70px;
+	padding: 20px 0;
+}
 
-  /deep/ div.toolbar {
-    height: 70px;
-    padding: 20px 0;
-  }
+/deep/ .el-table td:not(.is-left) {
+	text-align: center;
+}
 
-  /deep/ .el-table td:not(.is-left) {
-    text-align: center;
-  }
+.inner-toolbar {
+	display: flex;
+	width: 100%;
+	justify-content: space-between;
+}
 
-  .inner-toolbar {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-  }
+.toolbar-btns {
+}
 
-  .toolbar-btns {
+.toolbar-search {
+	margin-right: 10px;
+}
 
-  }
-
-  .toolbar-search {
-    margin-right: 10px;
-  }
-
-  .goods-image {
-    width: 50px;
-    height: 50px;
-  }
-
+.goods-image {
+	width: 50px;
+	height: 50px;
+}
 </style>
